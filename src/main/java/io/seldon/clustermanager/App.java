ebc6741c677eefaf59394d804cdc611aea568a27
Gurminder Sunner <gsunner2000@gmail.com>
2017-02-21 11:14:14 +0000

    initial checkin

---
+++ src/main/java/io/seldon/clustermanager/App.java
@@ -0,0 +1,13 @@
+package io.seldon.clustermanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
