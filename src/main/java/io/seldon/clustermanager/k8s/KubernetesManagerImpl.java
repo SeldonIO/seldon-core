17d84c915984a16fc84e91e511fd4fb07689e2f8
Gurminder Sunner <gsunner2000@gmail.com>
2017-02-28 15:11:58 +0000

    initial kubernetes impl

--- src/main/java/io/seldon/clustermanager/k8s/KubernetesManagerImpl.java
+++ src/main/java/io/seldon/clustermanager/k8s/KubernetesManagerImpl.java
@@ -1,17 +1,53 @@
 package io.seldon.clustermanager.k8s;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.seldon.clustermanager.component.KubernetesManager;
 
 public class KubernetesManagerImpl implements KubernetesManager {
 
+    private KubernetesClient kubernetesClient = null;
+
     @Override
     public void init() throws Exception {
         System.out.println("KubernetesManager init");
+
+        String master = "http://localhost:8001/";
+        Config config = new ConfigBuilder().withMasterUrl(master).build();
+
+        try {
+            kubernetesClient = new DefaultKubernetesClient(config);
+            getNamespaceList(); // simple check to see if client works
+            System.out.println("KubernetesManager: sucessfully passed namespace check test");
+        } catch (KubernetesClientException e) {
+            throw new Exception(e);
+        }
     }
 
     @Override
     public void cleanup() throws Exception {
         System.out.println("KubernetesManager cleanup");
+        if (kubernetesClient != null) {
+            kubernetesClient.close();
+        }
+    }
+
+    public List<String> getNamespaceList() {
+        List<String> namespace_list = new ArrayList<>();
+        NamespaceList namespaceList = kubernetesClient.namespaces().list();
+        for (Namespace ns : namespaceList.getItems()) {
+            namespace_list.add(ns.getMetadata().getName());
+        }
+
+        return namespace_list;
     }
 
 }
