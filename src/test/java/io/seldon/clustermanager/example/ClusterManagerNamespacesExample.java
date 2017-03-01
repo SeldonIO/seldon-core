e5070f08f49bbb72c26be2c9e4a793141a20c1b4
Gurminder Sunner <gsunner2000@gmail.com>
2017-03-01 11:16:47 +0000

    get namespaces from cluster manager example

---
+++ src/test/java/io/seldon/clustermanager/example/ClusterManagerNamespacesExample.java
@@ -0,0 +1,67 @@
+package io.seldon.clustermanager.example;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import io.seldon.clustermanager.cm.CluserManagerImpl;
+import io.seldon.clustermanager.component.ClusterManager;
+import io.seldon.clustermanager.component.KubernetesManager;
+import io.seldon.clustermanager.component.ZookeeperManager;
+import io.seldon.clustermanager.k8s.KubernetesManagerImpl;
+import io.seldon.clustermanager.pb.ProtoBufUtils;
+import io.seldon.protos.DeploymentProtos.CMResultDef;
+
+public class ClusterManagerNamespacesExample {
+
+    public static class CustomConfig {
+
+        @Bean(initMethod = "init", destroyMethod = "cleanup")
+        @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+        public ZookeeperManager zookeeperManager() {
+            return new ZookeeperManager() {
+
+                @Override
+                public void init() throws Exception {
+                }
+
+                @Override
+                public void cleanup() throws Exception {
+                }
+            };
+        }
+
+        @Bean(initMethod = "init", destroyMethod = "cleanup")
+        @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+        public KubernetesManager kubernetesManager() {
+            return new KubernetesManagerImpl();
+        }
+
+        @Bean(initMethod = "init", destroyMethod = "cleanup")
+        @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+        public ClusterManager clusterManager() {
+            return new CluserManagerImpl();
+        }
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(CustomConfig.class);
+        ctx.refresh();
+
+        ClusterManager clusterManager = ctx.getBean(ClusterManager.class);
+        CMResultDef cmResultDef = clusterManager.getNamespaces();
+        String json = null;
+        try {
+            json = ProtoBufUtils.toJson(cmResultDef);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        System.out.println(json);
+
+        ctx.close();
+    }
+}
