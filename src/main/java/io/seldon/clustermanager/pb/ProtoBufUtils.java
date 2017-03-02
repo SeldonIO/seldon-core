2bcca415c1209dc543ff850e95e49d286259e436
Gurminder Sunner <gsunner2000@gmail.com>
2017-03-02 14:14:04 +0000

    added some javadoc

--- src/main/java/io/seldon/clustermanager/pb/ProtoBufUtils.java
+++ src/main/java/io/seldon/clustermanager/pb/ProtoBufUtils.java
@@ -7,6 +7,17 @@ import com.google.protobuf.util.JsonFormat.Printer;
 
 public class ProtoBufUtils {
 
+    private ProtoBufUtils() {
+    }
+
+    /**
+     * Serialize a protobuf message to JSON, indicating if whitespace needs to be stripped.
+     * 
+     * @param message   The protobuf message to serialize.
+     * @param omittingInsignificantWhitespace   True if needs to be stripped of whitespace.
+     * @return json string
+     * @throws InvalidProtocolBufferException
+     */
     public static String toJson(Message message, boolean omittingInsignificantWhitespace) throws InvalidProtocolBufferException {
         String json = null;
         // json = JsonFormat.printer().includingDefaultValueFields().preservingProtoFieldNames().print(message);
@@ -20,6 +31,13 @@ public class ProtoBufUtils {
         return jsonPrinter.print(message);
     }
 
+    /**
+     * Serialize a protobuf message to JSON, allowing the inclusion of whitespace.
+     * 
+     * @param message   The protobuf message to serialize.
+     * @return json string
+     * @throws InvalidProtocolBufferException
+     */
     public static String toJson(Message message) throws InvalidProtocolBufferException {
         boolean omittingInsignificantWhitespace = false;
         return toJson(message, omittingInsignificantWhitespace);
