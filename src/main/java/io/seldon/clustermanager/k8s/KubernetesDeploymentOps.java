6278614b27aa9b5d1ceb1fdb6f89a8964c4482fc
Gurminder Sunner <gsunner2000@gmail.com>
2017-03-03 16:01:57 +0000

    class moved

---
+++ src/main/java/io/seldon/clustermanager/k8s/KubernetesDeploymentOps.java
@@ -0,0 +1,60 @@
+package io.seldon.clustermanager.k8s;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.fabric8.kubernetes.api.model.extensions.Deployment;
+import io.fabric8.kubernetes.api.model.extensions.DeploymentBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.seldon.protos.DeploymentProtos.ClusterResourcesDef;
+
+class KubernetesDeploymentOps {
+
+    private final static Logger logger = LoggerFactory.getLogger(KubernetesDeploymentOps.class);
+
+    private final String namespace_name;
+    private final String seldonDeploymentId;
+    private final KubernetesClient kubernetesClient;
+
+    public KubernetesDeploymentOps(String seldonDeploymentId, KubernetesClient kubernetesClient, String namespace_name) {
+        this.seldonDeploymentId = seldonDeploymentId;
+        this.kubernetesClient = kubernetesClient;
+        this.namespace_name = namespace_name;
+    }
+
+    public Deployment create(String kubernetesDeploymentId, ClusterResourcesDef clusterResourcesDef) {
+        final int replica_number = clusterResourcesDef.getReplicas();
+        final int container_port = 80; // TODO change this!
+        final String image_name_and_version = clusterResourcesDef.getImage() + ":" + clusterResourcesDef.getVersion();
+
+        //@formatter:off
+        Deployment deployment = new DeploymentBuilder()
+                .withNewMetadata().withName(kubernetesDeploymentId).addToLabels("seldon-deployment-id", seldonDeploymentId).endMetadata()
+                .withNewSpec().withReplicas(replica_number)
+                    .withNewTemplate()
+                        .withNewMetadata().addToLabels("seldon-app", kubernetesDeploymentId).endMetadata()
+                        .withNewSpec().addNewContainer().withName("seldon-container").withImage(image_name_and_version)
+                            .addNewPort().withContainerPort(container_port).endPort().endContainer()
+                        .endSpec()
+                    .endTemplate()
+                .endSpec().build();
+            //@formatter:on
+
+        deployment = kubernetesClient.extensions().deployments().inNamespace(namespace_name).create(deployment);
+        return deployment;
+    }
+
+    public void delete(Deployment deployment) {
+        final String deploymentName = deployment.getMetadata().getName();
+
+        io.fabric8.kubernetes.api.model.extensions.ReplicaSetList rslist = kubernetesClient.extensions().replicaSets().inNamespace(namespace_name)
+                .withLabel("seldon-app", deploymentName).list();
+        kubernetesClient.resource(deployment).delete();
+        logger.info(String.format("Deleted kubernetes delployment [%s]", deploymentName));
+        for (io.fabric8.kubernetes.api.model.extensions.ReplicaSet rs : rslist.getItems()) {
+            kubernetesClient.resource(rs).delete();
+            String rsmsg = (rs != null) ? rs.getMetadata().getName() : null;
+            logger.info(String.format("Deleted kubernetes replicaSet [%s]", rsmsg));
+        }
+    }
+}
--- src/main/java/io/seldon/clustermanager/k8s/KubernetesManagerImpl.java
+++ src/main/java/io/seldon/clustermanager/k8s/KubernetesManagerImpl.java
@@ -26,55 +26,6 @@ import io.seldon.protos.DeploymentProtos.PredictorDef;
 
 public class KubernetesManagerImpl implements KubernetesManager {
 
-    private static class KubernetesDeploymentOps {
-
-        private final String namespace_name;
-        private final String seldonDeploymentId;
-        private final KubernetesClient kubernetesClient;
-
-        public KubernetesDeploymentOps(String seldonDeploymentId, KubernetesClient kubernetesClient, String namespace_name) {
-            this.seldonDeploymentId = seldonDeploymentId;
-            this.kubernetesClient = kubernetesClient;
-            this.namespace_name = namespace_name;
-        }
-
-        public Deployment create(String kubernetesDeploymentId, ClusterResourcesDef clusterResourcesDef) {
-            final int replica_number = clusterResourcesDef.getReplicas();
-            final int container_port = 80; // TODO change this!
-            final String image_name_and_version = clusterResourcesDef.getImage() + ":" + clusterResourcesDef.getVersion();
-
-            //@formatter:off
-            Deployment deployment = new DeploymentBuilder()
-                    .withNewMetadata().withName(kubernetesDeploymentId).addToLabels("seldon-deployment-id", seldonDeploymentId).endMetadata()
-                    .withNewSpec().withReplicas(replica_number)
-                        .withNewTemplate()
-                            .withNewMetadata().addToLabels("seldon-app", kubernetesDeploymentId).endMetadata()
-                            .withNewSpec().addNewContainer().withName("seldon-container").withImage(image_name_and_version)
-                                .addNewPort().withContainerPort(container_port).endPort().endContainer()
-                            .endSpec()
-                        .endTemplate()
-                    .endSpec().build();
-            //@formatter:on
-
-            deployment = kubernetesClient.extensions().deployments().inNamespace(namespace_name).create(deployment);
-            return deployment;
-        }
-
-        public void delete(Deployment deployment) {
-            final String deploymentName = deployment.getMetadata().getName();
-
-            io.fabric8.kubernetes.api.model.extensions.ReplicaSetList rslist = kubernetesClient.extensions().replicaSets().inNamespace(namespace_name)
-                    .withLabel("seldon-app", deploymentName).list();
-            kubernetesClient.resource(deployment).delete();
-            logger.info(String.format("Deleted kubernetes delployment [%s]", deploymentName));
-            for (io.fabric8.kubernetes.api.model.extensions.ReplicaSet rs : rslist.getItems()) {
-                kubernetesClient.resource(rs).delete();
-                String rsmsg = (rs != null) ? rs.getMetadata().getName() : null;
-                logger.info(String.format("Deleted kubernetes replicaSet [%s]", rsmsg));
-            }
-        }
-    }
-
     private final static Logger logger = LoggerFactory.getLogger(KubernetesManagerImpl.class);
 
     private KubernetesClient kubernetesClient = null;
