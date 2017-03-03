7810891e240e33a5d2ee2f54060e78f2fa3e34f2
Gurminder Sunner <gsunner2000@gmail.com>
2017-03-03 15:53:53 +0000

    KubernetesDeploymentOps added

--- src/main/java/io/seldon/clustermanager/k8s/KubernetesManagerImpl.java
+++ src/main/java/io/seldon/clustermanager/k8s/KubernetesManagerImpl.java
@@ -26,6 +26,55 @@ import io.seldon.protos.DeploymentProtos.PredictorDef;
 
 public class KubernetesManagerImpl implements KubernetesManager {
 
+    private static class KubernetesDeploymentOps {
+
+        private final String namespace_name;
+        private final String seldonDeploymentId;
+        private final KubernetesClient kubernetesClient;
+
+        public KubernetesDeploymentOps(String seldonDeploymentId, KubernetesClient kubernetesClient, String namespace_name) {
+            this.seldonDeploymentId = seldonDeploymentId;
+            this.kubernetesClient = kubernetesClient;
+            this.namespace_name = namespace_name;
+        }
+
+        public Deployment create(String kubernetesDeploymentId, ClusterResourcesDef clusterResourcesDef) {
+            final int replica_number = clusterResourcesDef.getReplicas();
+            final int container_port = 80; // TODO change this!
+            final String image_name_and_version = clusterResourcesDef.getImage() + ":" + clusterResourcesDef.getVersion();
+
+            //@formatter:off
+            Deployment deployment = new DeploymentBuilder()
+                    .withNewMetadata().withName(kubernetesDeploymentId).addToLabels("seldon-deployment-id", seldonDeploymentId).endMetadata()
+                    .withNewSpec().withReplicas(replica_number)
+                        .withNewTemplate()
+                            .withNewMetadata().addToLabels("seldon-app", kubernetesDeploymentId).endMetadata()
+                            .withNewSpec().addNewContainer().withName("seldon-container").withImage(image_name_and_version)
+                                .addNewPort().withContainerPort(container_port).endPort().endContainer()
+                            .endSpec()
+                        .endTemplate()
+                    .endSpec().build();
+            //@formatter:on
+
+            deployment = kubernetesClient.extensions().deployments().inNamespace(namespace_name).create(deployment);
+            return deployment;
+        }
+
+        public void delete(Deployment deployment) {
+            final String deploymentName = deployment.getMetadata().getName();
+
+            io.fabric8.kubernetes.api.model.extensions.ReplicaSetList rslist = kubernetesClient.extensions().replicaSets().inNamespace(namespace_name)
+                    .withLabel("seldon-app", deploymentName).list();
+            kubernetesClient.resource(deployment).delete();
+            logger.info(String.format("Deleted kubernetes delployment [%s]", deploymentName));
+            for (io.fabric8.kubernetes.api.model.extensions.ReplicaSet rs : rslist.getItems()) {
+                kubernetesClient.resource(rs).delete();
+                String rsmsg = (rs != null) ? rs.getMetadata().getName() : null;
+                logger.info(String.format("Deleted kubernetes replicaSet [%s]", rsmsg));
+            }
+        }
+    }
+
     private final static Logger logger = LoggerFactory.getLogger(KubernetesManagerImpl.class);
 
     private KubernetesClient kubernetesClient = null;
@@ -80,7 +129,8 @@ public class KubernetesManagerImpl implements KubernetesManager {
             logger.info(String.format("Deploying predictiveUnit[%s] for seldonDeployment id[%s]", predictive_unit_name, seldonDeploymentId));
 
             ClusterResourcesDef clusterResourcesDef = predictiveUnitDef.getClusterResources();
-            Deployment deployment = createKubernetesDeployement(seldonDeploymentId, kubernetesDeploymentId, kubernetesClient, clusterResourcesDef);
+            ////Deployment deployment = createKubernetesDeployement(seldonDeploymentId, kubernetesDeploymentId, kubernetesClient, clusterResourcesDef);
+            Deployment deployment = new KubernetesDeploymentOps(seldonDeploymentId, kubernetesClient, namespace_name).create(kubernetesDeploymentId, clusterResourcesDef);
 
             String msg = (deployment != null) ? deployment.getMetadata().getName() : null;
             logger.info(String.format("Created kubernetes delployment [%s]", msg));
@@ -102,7 +152,8 @@ public class KubernetesManagerImpl implements KubernetesManager {
         DeploymentList deployments = kubernetesClient.extensions().deployments().inNamespace(namespace_name)
                 .withLabel("seldon-deployment-id", seldonDeploymentId).list();
         for (Deployment deployment : deployments.getItems()) {
-            deleteKubernetesDeployment(seldonDeploymentId, deployment, kubernetesClient, namespace_name);
+            ///deleteKubernetesDeployment(seldonDeploymentId, deployment, kubernetesClient, namespace_name);
+            new KubernetesDeploymentOps(seldonDeploymentId, kubernetesClient, namespace_name).delete(deployment);
 
             deleteKubernetesService(kubernetesClient, namespace_name, deployment);
 
