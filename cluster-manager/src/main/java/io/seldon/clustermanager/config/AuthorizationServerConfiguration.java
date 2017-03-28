1cd49803131fe4efd07525d2ded800ad0d1f91e9
Gurminder Sunner <gsunner2000@gmail.com>
2017-03-28 14:55:18 +0100

    cluster-manager - auth secret from env

--- cluster-manager/src/main/java/io/seldon/clustermanager/config/AuthorizationServerConfiguration.java
+++ cluster-manager/src/main/java/io/seldon/clustermanager/config/AuthorizationServerConfiguration.java
@@ -1,5 +1,10 @@
 package io.seldon.clustermanager.config;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +20,9 @@ import org.springframework.security.oauth2.provider.token.store.redis.RedisToken
 @Configuration
 @EnableAuthorizationServer
 class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    private final static Logger logger = LoggerFactory.getLogger(AuthorizationServerConfiguration.class);
+    private final static String SELDON_CLUSTER_MANAGER_CLIENT_SECRET_KEY = "SELDON_CLUSTER_MANAGER_CLIENT_SECRET";
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -35,14 +43,33 @@ class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+        final String client_id = "client";
+        String client_secret = null;
+        { // setup the client_secret using the env vars
+            client_secret = System.getenv().get(SELDON_CLUSTER_MANAGER_CLIENT_SECRET_KEY);
+            if (client_secret == null) {
+                client_secret = generateRandomString();
+                logger.error(String.format("FAILED to find env var [%s]", SELDON_CLUSTER_MANAGER_CLIENT_SECRET_KEY));
+                logger.error(String.format("generating client_secret[%s]", client_secret));
+            } else {
+                logger.info(String.format("using client_secret from env var [%s]", SELDON_CLUSTER_MANAGER_CLIENT_SECRET_KEY));
+            }
+        }
+        logger.info(String.format("setting up auth using client credentials, client_id[%s]", client_id));
+
         // @formatter:off
         clients
                 .inMemory()
-                .withClient("testclient")
+                .withClient(client_id)
                 .authorizedGrantTypes("client_credentials", "password")
                 .authorities("ROLE_CLIENT").scopes("read", "write", "trust").resourceIds("cluster-manger-api")
-                .secret("testsecret");
+                .secret(client_secret);
         // @formatter:on
     }
 
+    private static String generateRandomString() {
+        SecureRandom random = new SecureRandom();
+        return new BigInteger(130, random).toString(32);
+    }
 }
