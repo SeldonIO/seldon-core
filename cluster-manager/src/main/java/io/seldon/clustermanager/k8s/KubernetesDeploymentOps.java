0ac31ec19e355aa5f4499a00dee5981b860cea9a
Gurminder Sunner <gsunner2000@gmail.com>
2017-04-21 16:12:39 +0100

    container extracted

--- cluster-manager/src/main/java/io/seldon/clustermanager/k8s/KubernetesDeploymentOps.java
+++ cluster-manager/src/main/java/io/seldon/clustermanager/k8s/KubernetesDeploymentOps.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.ContainerBuilder;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.EnvVarBuilder;
 import io.fabric8.kubernetes.api.model.LocalObjectReference;
@@ -114,6 +116,18 @@ class KubernetesDeploymentOps {
             }
         }
 
+        Container c = new ContainerBuilder()
+                .withName("seldon-container").withImage(image_name_and_version)
+                .withEnv(envVar_PREDICTIVE_UNIT_PARAMETERS)
+                .addNewPort().withContainerPort(container_port).endPort()
+                .withNewResources()
+                    .addToRequests(resource_requests)
+                .endResources()
+                .build();
+        
+        List<Container> containers = new ArrayList<>();
+        containers.add(c);
+        
         //@formatter:off
         Deployment deployment = new DeploymentBuilder()
             .withNewMetadata().withName(kubernetesDeploymentId).addToLabels("seldon-deployment-id", seldonDeploymentId).endMetadata()
@@ -121,15 +135,8 @@ class KubernetesDeploymentOps {
                 .withNewTemplate()
                     .withNewMetadata().addToLabels("seldon-app", kubernetesDeploymentId).endMetadata()
                     .withNewSpec()
-                        .addNewContainer()
-                            .withName("seldon-container").withImage(image_name_and_version)
-                            .withEnv(envVar_PREDICTIVE_UNIT_PARAMETERS)
-                            .addNewPort().withContainerPort(container_port).endPort()
-                            .withNewResources()
-                                .addToRequests(resource_requests)
-                            .endResources()
-                         .endContainer()
-                         .addAllToImagePullSecrets(imagePullSecrets)
+                        .addAllToContainers(containers)
+                        .addAllToImagePullSecrets(imagePullSecrets)
                     .endSpec()
                 .endTemplate()
             .endSpec().build();
