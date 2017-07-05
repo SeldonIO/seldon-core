daa9444c0e7d3aa512362306765c845dac43cd2f
Gurminder Sunner <gsunner2000@gmail.com>
2017-07-05 17:08:30 +0100

    cluster-manager: test properties changes

--- cluster-manager/src/test/java/io/seldon/clustermanager/example/KM_CreateDeploymentExample.java
+++ cluster-manager/src/test/java/io/seldon/clustermanager/example/KM_CreateDeploymentExample.java
@@ -1,13 +1,16 @@
 package io.seldon.clustermanager.example;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 
+import io.seldon.clustermanager.ClusterManagerProperites;
 import io.seldon.clustermanager.component.KubernetesManager;
 import io.seldon.clustermanager.k8s.KubernetesManagerImpl;
 import io.seldon.clustermanager.pb.ProtoBufUtils;
@@ -15,12 +18,19 @@ import io.seldon.protos.DeploymentProtos.DeploymentDef;
 
 public class KM_CreateDeploymentExample {
 
+    @EnableAutoConfiguration
     public static class AppConfigKubernetes {
 
         @Bean(initMethod = "init", destroyMethod = "cleanup")
         @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
         public KubernetesManager kubernetesManager() {
             return new KubernetesManagerImpl();
+        }
+
+        @ConfigurationProperties(prefix = "io.seldon.clustermanager")
+        @Bean
+        public ClusterManagerProperites clusterManagerProperites() {
+            return new ClusterManagerProperites();
         }
 
     }
