ed926f6e684e07a871edfd8ce9659949deacc576
Clive Cox <cc@seldon.io>
2017-07-14 10:33:35 +0100

    updates

--- engine/src/main/java/io/seldon/engine/predictors/EnginePredictor.java
+++ engine/src/main/java/io/seldon/engine/predictors/EnginePredictor.java
@@ -1,16 +1,17 @@
 package io.seldon.engine.predictors;
 
+import java.util.Base64;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import io.seldon.protos.DeploymentProtos.PredictiveUnitDef;
-import io.seldon.protos.DeploymentProtos.PredictorDef;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat;
 import com.google.protobuf.util.JsonFormat.Printer;
-import java.util.Base64;
+
+import io.seldon.protos.DeploymentProtos.PredictiveUnitDef;
+import io.seldon.protos.DeploymentProtos.PredictorDef;
 
 public class EnginePredictor {
 
@@ -66,8 +67,8 @@ public class EnginePredictor {
             PredictiveUnitDef.Builder predictiveUnitDefBuilder = PredictiveUnitDef.newBuilder()
                     .setId("0")
                     .setName("basic-pu")
-                    .setType("model")
-                    .setSubtype("simpleModel");
+                    .setType(PredictiveUnitDef.PredictiveUnitType.MODEL)
+                    .setSubtype(PredictiveUnitDef.PredictiveUnitSubType.MODEL_SIMPLEMODEL);
             //@formatter:on
 
             predictorDefBuilder.addPredictiveUnits(predictiveUnitDefBuilder);
