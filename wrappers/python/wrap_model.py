3719dfd47c5dc511f4a3f1fecc64c743c22fc2d9
Maxime Fournes <mf@seldon.io>
2017-11-13 15:58:54 +0000

    Added service type parameter in wrapper

--- wrappers/python/wrap_model.py
+++ wrappers/python/wrap_model.py
@@ -7,7 +7,7 @@ def populate_template(template,file_out,**kwargs):
         with open(file_out,'w') as fout:
             fout.write(ftmp.read().format(**kwargs))
 
-def wrap_model(repo,model_folder,model_name,version,REST=True,out_folder=None,force_erase=False):
+def wrap_model(repo,model_folder,model_name,service_type,version,REST=True,out_folder=None,force_erase=False):
     if out_folder is None:
         out_folder = model_folder
     build_folder = out_folder+'/build'
@@ -19,14 +19,15 @@ def wrap_model(repo,model_folder,model_name,version,REST=True,out_folder=None,fo
             shutil.rmtree(build_folder)
     shutil.copytree(model_folder,build_folder)
     shutil.copy2('./microservice.py',build_folder)
-    shutil.copy2('./model_microservice.py',build_folder)
+    shutil.copy2('./{}_microservice.py'.format(service_type.lower()),build_folder)
     shutil.copy2("./seldon_requirements.txt",build_folder)
     shutil.copytree('./proto',build_folder+'/proto')
     populate_template(
         './Dockerfile.tmp',
         build_folder+'/Dockerfile',
         model_name=model_name,
-        api_type="REST" if REST else "GRPC"
+        api_type="REST" if REST else "GRPC",
+        service_type = service_type
     )
     populate_template(
         './Makefile.tmp',
@@ -45,6 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("repo",type=str)
     parser.add_argument("--grpc",action="store_true")
     parser.add_argument("--out-folder",type=str,default=None)
+    parser.add_argument("--service-type",type=str,choices=["MODEL","ROUTER"],default="MODEL")
     parser.add_argument("-f","--force",action="store_true")
 
     args = parser.parse_args()
@@ -53,6 +55,7 @@ if __name__ == "__main__":
         args.repo,
         args.model_folder,
         args.model_name,
+        args.service_type,
         args.version,
         REST=not args.grpc,
         out_folder=args.out_folder,
