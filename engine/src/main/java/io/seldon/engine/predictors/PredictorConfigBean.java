13e8d68947d155c53a0f14b780a258e2ef508b1c
Maxime Fournes <mf@seldon.io>
2017-12-14 17:41:42 +0000

    Added Predictor Config file

---
+++ engine/src/main/java/io/seldon/engine/predictors/PredictorConfigBean.java
@@ -0,0 +1,88 @@
+package io.seldon.engine.predictors;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import io.seldon.protos.DeploymentProtos.PredictiveUnit.PredictiveUnitImplementation;
+import io.seldon.protos.DeploymentProtos.PredictiveUnit.PredictiveUnitMethod;
+import io.seldon.protos.DeploymentProtos.PredictiveUnit.PredictiveUnitType;
+
+@Component
+public class PredictorConfigBean {
+    public final Map<PredictiveUnitType,List<PredictiveUnitMethod>> typeMethodsMap;
+    public final Map<PredictiveUnitImplementation,PredictiveUnitBean> nodeImplementationMap;
+    
+    @Autowired
+	public PredictorConfigBean(
+			@Lazy SimpleModelUnit simpleModelUnit, 
+			@Lazy SimpleRouterUnit simpleRouterUnit,
+			@Lazy AverageCombinerUnit averageCombinerUnit,
+			@Lazy RandomABTestUnit randomABTestUnit) {
+        
+        // ---------------------------
+        // DEFINITION OF DEFAULT TYPES
+    	// ---------------------------
+        typeMethodsMap = new HashMap<PredictiveUnitType, List<PredictiveUnitMethod>>();
+        
+        // MODEL -> TRANSFORM INPUT
+        List<PredictiveUnitMethod> modelMethods = new ArrayList<PredictiveUnitMethod>();
+        modelMethods.add(PredictiveUnitMethod.TRANSFORM_INPUT);
+        typeMethodsMap.put(PredictiveUnitType.MODEL, modelMethods);
+        
+        // TRANSFORMER -> TRANSFORM INPUT
+        List<PredictiveUnitMethod> transformerMethods = new ArrayList<PredictiveUnitMethod>();
+        transformerMethods.add(PredictiveUnitMethod.TRANSFORM_INPUT);
+        typeMethodsMap.put(PredictiveUnitType.TRANSFORMER, transformerMethods);
+        
+        // OUTPUT TRANSFORMER -> TRANSFORM OUTPUT
+        List<PredictiveUnitMethod> outTransformerMethods = new ArrayList<PredictiveUnitMethod>();
+        outTransformerMethods.add(PredictiveUnitMethod.TRANSFORM_OUTPUT);
+        typeMethodsMap.put(PredictiveUnitType.OUTPUT_TRANSFORMER, outTransformerMethods);
+        
+        // ROUTER -> ROUTE, SEND FEEDBACK
+        List<PredictiveUnitMethod> routerMethods = new ArrayList<PredictiveUnitMethod>();
+        routerMethods.add(PredictiveUnitMethod.ROUTE);
+        routerMethods.add(PredictiveUnitMethod.SEND_FEEDBACK);
+        typeMethodsMap.put(PredictiveUnitType.ROUTER, routerMethods);
+        
+        // COMBINER -> AGGREGATE
+        List<PredictiveUnitMethod> combinerMethods = new ArrayList<PredictiveUnitMethod>();
+        combinerMethods.add(PredictiveUnitMethod.AGGREGATE);
+        typeMethodsMap.put(PredictiveUnitType.COMBINER,combinerMethods);
+        
+        // -------------------------
+        // HARDCODED IMPLEMENTATIONS
+        // -------------------------
+        nodeImplementationMap = new HashMap<PredictiveUnitImplementation,PredictiveUnitBean>();
+        nodeImplementationMap.put(PredictiveUnitImplementation.AVERAGE_COMBINER, averageCombinerUnit);
+        nodeImplementationMap.put(PredictiveUnitImplementation.SIMPLE_MODEL, simpleModelUnit);
+        nodeImplementationMap.put(PredictiveUnitImplementation.SIMPLE_ROUTER, simpleRouterUnit);
+        nodeImplementationMap.put(PredictiveUnitImplementation.RANDOM_ABTEST, randomABTestUnit);
+    }
+    
+    public PredictiveUnitBean getImplementation(PredictiveUnitState state){
+    	if (state.implementation != null){
+    		return nodeImplementationMap.get(state.implementation);
+    	}
+    	return null;
+    }
+    
+    public Boolean hasMethod(PredictiveUnitMethod method, PredictiveUnitState state){
+    	if (state.implementation != null){
+    		return false;
+    	}
+    	if (state.type == PredictiveUnitType.UNKNOWN_TYPE) {
+    		return state.methods.contains(method);
+    	}
+    	else
+    	{
+    		return typeMethodsMap.get(state.type).contains(method);
+    	}
+    }
+}
