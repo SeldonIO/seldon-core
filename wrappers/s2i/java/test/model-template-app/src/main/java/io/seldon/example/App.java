10b55ab626145b2ae67dcf7384496aa06d554fcb
Clive Cox <cc@seldon.io>
2018-04-08 16:40:05 +0100

    move java test app

--- wrappers/s2i/java/test/model-template-app/src/main/java/io/seldon/example/App.java
+++ wrappers/s2i/java/test/model-template-app/src/main/java/io/seldon/example/App.java
@@ -3,7 +3,9 @@ package io.seldon.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication(scanBasePackages = {"io.seldon.wrapper","io.seldon.example"})
 @Import({ io.seldon.wrapper.config.AppConfig.class })
 public class App {
