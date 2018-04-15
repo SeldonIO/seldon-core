bf54637a9b09078d8d6587e45f99e3acfebe98ba
Clive Cox <cc@seldon.io>
2018-04-15 18:24:28 +0100

    add r_mnist model and add R caret to R s2i builder image

--- examples/models/r_mnist/install.R
+++ examples/models/r_mnist/install.R
@@ -1,2 +1,2 @@
-install.packages('caret')
+install.packages('pls')
 
--- examples/models/r_mnist/mnist.R
+++ examples/models/r_mnist/mnist.R
@@ -1,6 +1,9 @@
 library(methods)
 
 predict.mnist <- function(mnist,newdata=list()) {
+  cn <- 1:784
+  for (i in seq_along(cn)){cn[i] <- paste("X",cn[i],sep = "")}
+  colnames(newdata) <- cn
   predict(mnist$model, newdata = newdata, type='prob')
 }
 
