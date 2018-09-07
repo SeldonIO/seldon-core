a69f549e49ded2c59f90291be8d3d874daf1a41f
Clive Cox <cc@seldon.io>
2018-09-07 16:38:57 +0100

    updated minikube notebook

---
+++ notebooks/seldon_utils.py
@@ -0,0 +1,66 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from proto import prediction_pb2
+from proto import prediction_pb2_grpc
+import grpc
+import numpy as np
+
+def create_random_data(data_size):
+    shape = [1,data_size]
+    arr = np.random.rand(data_size)
+    return (shape,arr)
+
+def get_token(oauth_key,oauth_secret,endpoint):
+    payload = {'grant_type': 'client_credentials'}
+    response = requests.post(
+                "http://"+endpoint+"/oauth/token",
+                auth=HTTPBasicAuth(oauth_key, oauth_secret),
+                data=payload)
+    print(response.text)
+    token =  response.json()["access_token"]
+    return token
+
+    
+def rest_request_api_gateway(oauth_key,oauth_secret,endpoint="localhost:8002",data_size=5):
+    token = get_token(oauth_key,oauth_secret,endpoint)
+    shape, arr = create_random_data(data_size)
+    headers = {'Authorization': 'Bearer '+token}
+    payload = {"data":{"tensor":{"shape":shape,"values":arr.tolist()}}}
+    response = requests.post(
+                "http://"+endpoint+"/api/v0.1/predictions",
+                headers=headers,
+                json=payload)
+    print(response.text)
+
+def grpc_request_api_gateway(oauth_key,oauth_secret,rest_endpoint="localhost:8002",grpc_endpoint="localhost:8003",data_size=5):
+    token = get_token(oauth_key,oauth_secret,rest_endpoint)
+    shape, arr = create_random_data(data_size)
+    datadef = prediction_pb2.DefaultData(
+            tensor = prediction_pb2.Tensor(
+                shape = shape,
+                values = arr
+                )
+            )
+    request = prediction_pb2.SeldonMessage(data = datadef)
+    channel = grpc.insecure_channel(grpc_endpoint)
+    stub = prediction_pb2_grpc.SeldonStub(channel)
+    metadata = [('oauth_token', token)]
+    response = stub.Predict(request=request,metadata=metadata)
+    print(response)
+    
+def grpc_request_ambassador(deploymentName,data_size,endpoint="localhost:8004"):
+    shape, arr = create_random_data(data_size)
+    datadef = prediction_pb2.DefaultData(
+            tensor = prediction_pb2.Tensor(
+                shape = shape,
+                values = arr
+                )
+            )
+    request = prediction_pb2.SeldonMessage(data = datadef)
+    channel = grpc.insecure_channel(endpoint)
+    stub = prediction_pb2_grpc.SeldonStub(channel)
+    metadata = [('seldon',deploymentName)]
+    response = stub.Predict(request=request,metadata=metadata)
+    print(response)
+
+
