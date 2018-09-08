ddb9d0e18a19bbcc9ce7a5a7869f876eb16ba240
Sachin.Varghese <Sachin.Varghese@mu-sigma.com>
2018-09-08 21:33:46 +0530

    nodejs wrapper v1

--- wrappers/s2i/nodejs/microservice.js
+++ wrappers/s2i/nodejs/microservice.js
@@ -1,8 +1,77 @@
-const tf = require("@tensorflow/tfjs");
-require("@tensorflow/tfjs-node");
-const MyModel = require("./MyModel");
+const argparse = require("argparse");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let x = new MyModel();
-x.init().then(() => {
-  x.predict(tf.randomNormal([1, 10])).print();
+loadModel = async function(model) {
+  // model = "./model/" + model;
+  model = "/media/sachin/Docs/nodejs/test/model-template-app/" + model;
+
+  try {
+    const MyModel = require(model);
+    console.log("Loading Model", model);
+    let x = new MyModel();
+    await x.init();
+    return x.predict.bind(x);
+  } catch (msg) {
+    return null;
+  }
+};
+
+const parser = new argparse.ArgumentParser({
+  description: "Seldon-core nodejs microservice builder",
+  addHelp: true
 });
+parser.addArgument("--model", {
+  type: "string",
+  help: "Name of the model File"
+});
+parser.addArgument("--api", {
+  type: "string",
+  help: "Endpoints to be exposed REST or GRPC"
+});
+parser.addArgument("--service", {
+  type: "string",
+  help: "Service type"
+});
+parser.addArgument("--persistance", {
+  type: "int",
+  defaultValue: 0,
+  help: "Persistance present or not"
+});
+const args = parser.parseArgs();
+
+console.log(args.model, args.api, args.service, args.persistance);
+
+if (args.service === "MODEL" && args.api === "REST") {
+  app.post("/predict", async (req, res) => {
+    try {
+      body = JSON.parse(req.body.json);
+      body = body.data;
+    } catch (msg) {
+      res.status(500).send("Cannot parse predict input json " + req.body.json);
+    }
+    const predict = await loadModel(
+      args.model,
+      args.api,
+      args.service,
+      args.persistance
+    );
+    if (predict) {
+      result = predict(body.tensor, body.names);
+      data = await result.data();
+      console.log(result);
+      result.print();
+      res.status(200).send({
+        data: { names: [], tensor: { values: data, shape: result.shape } }
+      });
+    } else {
+      res.status(500).send(args.model + " Not Found");
+    }
+  });
+}
+
+app.listen(5000, () =>
+  console.log("NodeJs Microservice listening on port 5000!")
+);
