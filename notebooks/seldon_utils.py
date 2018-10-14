fa29ce6d757af4925397990a62d707eaaa1c3ad0
Clive Cox <cc@seldon.io>
2018-10-14 20:56:09 +0100

    update base helm and ksonnet scripts and seldon utils

--- notebooks/seldon_utils.py
+++ notebooks/seldon_utils.py
@@ -20,21 +20,28 @@ def get_token(oauth_key,oauth_secret,endpoint):
     token =  response.json()["access_token"]
     return token
 
-    
-def rest_request_api_gateway(oauth_key,oauth_secret,endpoint="localhost:8002",data_size=5,rows=1):
+def rest_request_api_gateway(oauth_key,oauth_secret,endpoint="localhost:8002",data_size=5,rows=1,data=None):
     token = get_token(oauth_key,oauth_secret,endpoint)
-    shape, arr = create_random_data(data_size,rows)
+    if data is None:
+        shape, arr = create_random_data(data_size,rows)
+    else:
+        shape = data.shape
+        arr = data.flatten()
     headers = {'Authorization': 'Bearer '+token}
     payload = {"data":{"tensor":{"shape":shape,"values":arr.tolist()}}}
     response = requests.post(
                 "http://"+endpoint+"/api/v0.1/predictions",
                 headers=headers,
                 json=payload)
-    print(response.text)
+    return response
 
-def grpc_request_api_gateway(oauth_key,oauth_secret,rest_endpoint="localhost:8002",grpc_endpoint="localhost:8003",data_size=5,rows=1):
+def grpc_request_api_gateway(oauth_key,oauth_secret,rest_endpoint="localhost:8002",grpc_endpoint="localhost:8003",data_size=5,rows=1,data=None):
     token = get_token(oauth_key,oauth_secret,rest_endpoint)
-    shape, arr = create_random_data(data_size,rows)
+    if data is None:
+        shape, arr = create_random_data(data_size,rows)
+    else:
+        shape = data.shape
+        arr = data.flatten()
     datadef = prediction_pb2.DefaultData(
             tensor = prediction_pb2.Tensor(
                 shape = shape,
@@ -46,30 +53,39 @@ def grpc_request_api_gateway(oauth_key,oauth_secret,rest_endpoint="localhost:800
     stub = prediction_pb2_grpc.SeldonStub(channel)
     metadata = [('oauth_token', token)]
     response = stub.Predict(request=request,metadata=metadata)
-    print(response)
+    return response
 
-def rest_request_ambassador(deploymentName,endpoint="localhost:8003",data_size=5,rows=1):
+def rest_request_ambassador(deploymentName,endpoint="localhost:8003",data_size=5,rows=1,data=None):
+    if data is None:
         shape, arr = create_random_data(data_size,rows)
-        payload = {"data":{"names":["a","b"],"tensor":{"shape":shape,"values":arr.tolist()}}}
-        response = requests.post(
-            "http://"+endpoint+"/seldon/"+deploymentName+"/api/v0.1/predictions",
-            json=payload)
-        print(response.status_code)
-        print(response.text)
+    else:
+        shape = data.shape
+        arr = data.flatten()
+    payload = {"data":{"names":["a","b"],"tensor":{"shape":shape,"values":arr.tolist()}}}
+    response = requests.post(
+        "http://"+endpoint+"/seldon/"+deploymentName+"/api/v0.1/predictions",
+        json=payload)
+    return response
 
-def rest_request_ambassador_auth(deploymentName,username,password,endpoint="localhost:8003",data_size=5,rows=1):
-    shape, arr = create_random_data(data_size,rows)    
+def rest_request_ambassador_auth(deploymentName,username,password,endpoint="localhost:8003",data_size=5,rows=1,data=None):
+    if data is None:
+        shape, arr = create_random_data(data_size,rows)
+    else:
+        shape = data.shape
+        arr = data.flatten()        
     payload = {"data":{"names":["a","b"],"tensor":{"shape":shape,"values":arr.tolist()}}}
     response = requests.post(
         "http://"+endpoint+"/seldon/"+deploymentName+"/api/v0.1/predictions",
         json=payload,
         auth=HTTPBasicAuth(username, password))
-    print(response.status_code)
-    print(response.text)
-                    
-        
-def grpc_request_ambassador(deploymentName,endpoint="localhost:8004",data_size=5,rows=1):
-    shape, arr = create_random_data(data_size,rows)
+    return response
+
+def grpc_request_ambassador(deploymentName,endpoint="localhost:8004",data_size=5,rows=1,data=None):
+    if data is None:
+        shape, arr = create_random_data(data_size,rows)
+    else:
+        shape = data.shape
+        arr = data.flatten()    
     datadef = prediction_pb2.DefaultData(
             tensor = prediction_pb2.Tensor(
                 shape = shape,
@@ -81,6 +97,6 @@ def grpc_request_ambassador(deploymentName,endpoint="localhost:8004",data_size=5
     stub = prediction_pb2_grpc.SeldonStub(channel)
     metadata = [('seldon',deploymentName)]
     response = stub.Predict(request=request,metadata=metadata)
-    print(response)
+    return response
 
 
