390f9a56bc08394d94eb441f44019ba9a347278e
Clive Cox <cc@seldon.io>
2018-11-05 06:33:01 +0000

    add new test for rolling update form 1 component to 2 in 2 deployemnts

--- testing/scripts/test_rolling_updates.py
+++ testing/scripts/test_rolling_updates.py
@@ -11,6 +11,7 @@ def wait_for_rollout(deploymentName):
         ret = run("kubectl rollout status deploy/"+deploymentName, shell=True)
 
 # Test updating a model with a new image version as the only change
+@pytest.mark.skip
 def test_rolling_update1():
     run("kubectl delete sdep --all", shell=True, check=True)
     time.sleep(30)
@@ -34,6 +35,7 @@ def test_rolling_update1():
     assert i < 100
 
 # test changing the image version and the name of its container
+@pytest.mark.skip
 def test_rolling_update2():
     run("kubectl delete sdep --all", shell=True, check=True)
     time.sleep(30)
@@ -57,6 +59,7 @@ def test_rolling_update2():
     assert i < 100
 
 # Test updating a model with a new resource request but same image
+@pytest.mark.skip
 def test_rolling_update3():
     run("kubectl delete sdep --all", shell=True, check=True)
     time.sleep(30)
@@ -76,3 +79,28 @@ def test_rolling_update3():
         assert ((res["meta"]["requestPath"]["complex-model"] == "seldonio/fixed-model:0.1" and res["data"]["tensor"]["values"] == [1.0,2.0,3.0,4.0]))
         time.sleep(1)
     assert i == 49
+
+
+# Test updating a model with a multi deployment new model
+def test_rolling_update4():
+    run("kubectl delete sdep --all", shell=True, check=True)
+    time.sleep(30)
+    run("kubectl apply -f ../resources/graph1.json", shell=True, check=True)
+    wait_for_rollout("mymodel-mymodel-svc-orch")
+    wait_for_rollout("mymodel-mymodel-e2eb561")
+    r = rest_request_api_gateway("oauth-key","oauth-secret")        
+    assert r.status_code == 200
+    assert r.json()["data"]["tensor"]["values"] == [1.0,2.0,3.0,4.0]
+    run("kubectl apply -f ../resources/graph5.json", shell=True, check=True)
+    i = 0
+    for i in range(50):
+        r = rest_request_api_gateway("oauth-key","oauth-secret")
+        res = r.json()
+        print(res)
+        assert r.status_code == 200
+        assert (("complex-model" in res["meta"]["requestPath"] and res["meta"]["requestPath"]["complex-model"] == "seldonio/fixed-model:0.1" and res["data"]["tensor"]["values"] == [1.0,2.0,3.0,4.0]) or (res["meta"]["requestPath"]["model1"] == "seldonio/fixed-model:0.1" and res["data"]["tensor"]["values"] == [1.0,2.0,3.0,4.0] and res["meta"]["requestPath"]["model2"] == "seldonio/fixed-model:0.1"))
+        if (not r.status_code == 200) or ("model1" in res["meta"]["requestPath"]):
+            break
+        time.sleep(1)
+    assert i < 100
+    
