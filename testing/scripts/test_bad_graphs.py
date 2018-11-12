17169c74b3f8f08966fb6d3f5c00bf72df4c7cad
Clive Cox <cc@seldon.io>
2018-11-12 13:50:39 +0000

    add more tests for bad graphs

--- testing/scripts/test_bad_graphs.py
+++ testing/scripts/test_bad_graphs.py
@@ -4,7 +4,6 @@ import subprocess
 from subprocess import run
 import json
 
-# Test updating a model with a new image version as the only change
 def test_duplicate_predictor_name():
     run("kubectl apply -f ../resources/bad_duplicate_predictor_name.json", shell=True, check=True)
     time.sleep(10)
@@ -12,5 +11,17 @@ def test_duplicate_predictor_name():
     jStr = completedProcess.stdout
     j = json.loads(jStr)
     assert j["status"]["state"] == "Failed"
+    assert j["status"]["description"] == "Duplicate predictor name: mymodel"
+    run("kubectl delete sdep --all", shell=True)    
+
+# Name in graph and that in PodTemplateSpec don't match
+def test_model_name_mismatch():
+    run("kubectl apply -f ../resources/bad_name_mismatch.json", shell=True, check=True)
+    time.sleep(10)
+    completedProcess = run("kubectl get sdep namemismatch -o json", shell=True, check=True, stdout=subprocess.PIPE)
+    jStr = completedProcess.stdout
+    j = json.loads(jStr)
+    assert j["status"]["state"] == "Failed"
+    assert j["status"]["description"] == "Can't find container for predictive unit with name complex-model_bad_name"
     run("kubectl delete sdep --all", shell=True)    
     
