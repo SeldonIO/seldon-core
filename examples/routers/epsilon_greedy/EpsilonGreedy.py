ff07ff6dd8ed9e480df6c4fdddd1cd058c2daf21
Janis Klaise <jk@seldon.io>
2018-11-12 12:01:57 +0000

    Update to lates e-greedy dev version

--- examples/routers/epsilon_greedy/EpsilonGreedy.py
+++ examples/routers/epsilon_greedy/EpsilonGreedy.py
@@ -11,16 +11,35 @@ def n_success_failures(features,reward):
 
 class EpsilonGreedy(object):
 
-    def __init__(self,n_branches=None,epsilon=0.1,verbose=False):
+    def __init__(self,n_branches=None,epsilon=0.1,best_branch=None,verbose=False,seed=None,history=False):
         print("Starting Epsilon Greedy Microservice, version {}".format(__version__))
+
+        # for reproducibility
+        if seed:
+            random.seed(seed)
+            np.random.seed(seed)
+
         if n_branches is None:
             raise Exception("n_branches parameter must be given")
+
+        self.name = 'epsilon_greedy_' + __version__
         self.verbose = verbose
         self.epsilon = epsilon
-        self.best_branch = 0
-        self.branches_success = [0 for _ in range(n_branches)]
-        self.branches_tries = [0 for _ in range(n_branches)]
-        self.n_branches = n_branches
+        self.history = history
+
+        if best_branch:
+            self.best_branch = best_branch
+        else:
+            self.best_branch = random.choice(range(n_branches))
+
+        self.branch_success = [0 for _ in range(n_branches)]
+        self.branch_tries = [0 for _ in range(n_branches)]
+        self.branch_values = [0 for _ in range(n_branches)]
+
+        if self.history:
+            self.branch_history = []
+            self.value_history = []
+
         if self.verbose:
             print("Router initialised")
             print("# branches:",self.n_branches)
@@ -29,9 +48,13 @@ class EpsilonGreedy(object):
 
     def route(self,features,feature_names):
         x = random.random()
-        best_branch = self.best_branch
-        other_branches = [i for i in range(self.n_branches) if i!=best_branch]
-        selected_branch = best_branch if x>self.epsilon else random.choice(other_branches)
+        other_branches = [i for i in range(self.n_branches) if i != self.best_branch]
+        selected_branch = self.best_branch if x > self.epsilon else random.choice(other_branches)
+
+        if self.history:
+            self.branch_history.append(selected_branch)
+            self.value_history.append(self.branch_values.copy())
+
         if self.verbose:
             print("Routing")
             print("Current best branch:",best_branch)
@@ -45,17 +68,26 @@ class EpsilonGreedy(object):
             print("Prev success #", self.branches_success)
             print("Prev tries #", self.branches_tries)
             print("Prev best branch:", self.best_branch)
-        n_success, n_failures = n_success_failures(features,reward)
-        self.branches_success[routing] += n_success
-        self.branches_tries[routing] += n_success + n_failures
-        perfs = [
-            (self.branches_success[i]+1)/float(self.branches_tries[i]+1)
-            for i
-            in range(self.n_branches)
-        ]
-        self.best_branch = np.argmax(perfs)
+
+        n_success, n_failures = self.n_success_failures(features,reward)
+        self.branch_success[routing] += n_success
+        self.branch_tries[routing] += n_success + n_failures
+        self.branch_values[routing] = self.branch_success[routing]/self.branch_tries[routing]
+
+        # break ties randomly
+        self.best_branch = np.random.choice(np.where(np.array(self.branch_values) == max(self.branch_values))[0])
+
         if self.verbose:
             print("New success #", self.branches_success)
             print("New tries #", self.branches_tries)
             print("New best branch:",self.best_branch)
             print()
+
+    def metrics(self):
+        return [{"type":"GAUGE", "key":"branch_value_{}".format(b), "value":self.branch_values[b]} for b in range(self.n_branches)]
+
+    def n_success_failures(self,features,reward):
+        n_predictions = features.shape[0]
+        n_success = int(reward*n_predictions)
+        n_failures = n_predictions - n_success
+        return n_success, n_failures
