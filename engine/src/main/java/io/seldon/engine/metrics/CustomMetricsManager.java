797749136b66eaa9425e160af40547039cc63b9f
Clive Cox <cc@seldon.io>
2018-11-15 14:23:30 +0000

    Fix storing of Gauge metrics

--- engine/src/main/java/io/seldon/engine/metrics/CustomMetricsManager.java
+++ engine/src/main/java/io/seldon/engine/metrics/CustomMetricsManager.java
@@ -2,12 +2,15 @@ package io.seldon.engine.metrics;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.google.common.util.concurrent.AtomicDouble;
 
+import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.Metrics;
-import io.seldon.engine.predictors.PredictiveUnitState;
+import io.micrometer.core.instrument.Tag;
 import io.seldon.protos.PredictionProtos.Metric;
 
 /**
@@ -19,17 +22,21 @@ import io.seldon.protos.PredictionProtos.Metric;
 @Component
 public class CustomMetricsManager {
 
-	private ConcurrentHashMap<PredictiveUnitState,AtomicDouble> gauges = new ConcurrentHashMap<>();
+	private final static Logger logger = LoggerFactory.getLogger(CustomMetricsManager.class);
+	private ConcurrentHashMap<Meter.Id,AtomicDouble> gauges = new ConcurrentHashMap<>();
 	
-	public AtomicDouble get(PredictiveUnitState state,Metric metric)
+
+	public AtomicDouble get(Iterable<Tag> tags,Metric metric)
 	{
-		if (gauges.containsKey(state))
-			return gauges.get(state);
+		Meter.Id id = Metrics.globalRegistry.createId(metric.getKey(), tags, "");
+		if (gauges.containsKey(id))
+			return gauges.get(id);
 		else
 		{
+			logger.info("Creating new metric Id for {}",metric.toString());
 			AtomicDouble d = new AtomicDouble();
-			gauges.put(state, d);
-			Metrics.globalRegistry.gauge(metric.getKey(), d);
+			gauges.put(id, d);
+			Metrics.globalRegistry.gauge(metric.getKey(), tags, d);
 			return d;
 		}
 	}
