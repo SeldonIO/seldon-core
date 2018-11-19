de43c76f205b92abec04563154f21cb73cd377b6
Clive Cox <cc@seldon.io>
2018-11-19 18:44:41 +0000

    add further tests for grpc channel handler

--- engine/src/test/java/io/seldon/engine/grpc/GrpcChannelHandlerTest.java
+++ engine/src/test/java/io/seldon/engine/grpc/GrpcChannelHandlerTest.java
@@ -41,5 +41,20 @@ public class GrpcChannelHandlerTest {
 		Assert.assertNotEquals(mc1, mc2);
 		Assert.assertEquals(2, ch.size());
 	}
+	
+	@Test
+	public void testDifferenceByHost()
+	{
+		GrpcChannelHandler ch = new GrpcChannelHandler();
+		
+		Endpoint e1 = Endpoint.newBuilder().setServiceHost("hostA").setServicePort(1000).setType(EndpointType.REST).build();
+		Endpoint e2 = Endpoint.newBuilder().setServiceHost("hostB").setServicePort(1000).setType(EndpointType.REST).build();
+		
+		ManagedChannel mc1 = ch.get(e1);
+		ManagedChannel mc2 = ch.get(e2);
+		
+		Assert.assertNotEquals(mc1, mc2);
+		Assert.assertEquals(2, ch.size());
+	}
 
 }
