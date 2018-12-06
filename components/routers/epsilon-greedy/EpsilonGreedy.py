b7b676d53de2488850642bd383e980edd08321e6
Janis Klaise <jk@seldon.io>
2018-12-06 15:01:13 +0000

    Overhaul mab routers, add Thomson Sampling component

---
+++ components/routers/epsilon-greedy/EpsilonGreedy.py
@@ -0,0 +1,150 @@
+import random
+import logging
+import numpy as np
+
+__version__ = "v1.3"
+logger = logging.getLogger(__name__)
+
+
+class EpsilonGreedy(object):
+    """ Multi-armed bandit routing using epsilon-greedy strategy.
+
+    This class implements epsilon-greedy routing. The rewards are assumed to
+    come from a Bernoulli distribution. The reward is assumed to be a single
+    float between 0 and 1 indicating the mean reward for a batch of samples.
+
+    Parameters
+    ----------
+    n_branches : int
+        Number of child components/models the router will route requests to
+    epsilon : float
+        Epsilon parameter specifying the probability of choosing a random branch
+    best_branch : int, optional
+        Specify the starting branch
+    verbose : bool
+        Set the logger level
+    seed : int, optional
+        Set the random seed
+    history : bool
+        Set storing router history
+    branch_names: str, optional
+        A string specifying branch names separated by `:`
+
+    """
+
+    def __init__(self, n_branches=None, epsilon=0.1, best_branch=None, verbose=False,
+                 seed=None, history=False, branch_names=None):
+
+        if verbose:
+            logger.setLevel(10)
+            logger.info("Enabling debug mode")
+
+        logger.info(
+            "Starting %s Microservice, version %s", __name__, __version__)
+
+        # for reproducibility
+        if seed:
+            logger.info("Setting random seed to %s", seed)
+            random.seed(seed)
+            np.random.seed(seed)
+
+        try:
+            n_branches = int(n_branches)
+        except (TypeError, ValueError) as e:
+            logger.exception("n_branches parameter must be given")
+            raise
+
+        self.name = __name__ + __version__
+        self.verbose = verbose
+        self.n_branches = n_branches
+        self.epsilon = epsilon
+        self.history = history
+
+        if best_branch:
+            logger.info("Best branch supplied: %s", best_branch)
+            self.best_branch = best_branch
+        else:
+            self.best_branch = random.choice(range(n_branches))
+            logger.info("Starting with a random branch: %s", self.best_branch)
+
+        self.branch_success = [0 for _ in range(n_branches)]
+        self.branch_tries = [0 for _ in range(n_branches)]
+        self.branch_values = [0 for _ in range(n_branches)]
+
+        if self.history:
+            logger.info("Enabling history")
+            self.branch_history = []
+            self.value_history = []
+
+        if branch_names is not None:
+            self.branch_names = branch_names.split(":")
+            logger.info("Branch names: %s", self.branch_names)
+
+        logger.info("Router initialised, n_branches: %s, epsilon: %s",
+                    self.n_branches, self.epsilon)
+
+    def route(self, features, feature_names):
+        logger.debug("Routing features %s", features)
+
+        x = random.random()
+        other_branches = [i for i in range(
+            self.n_branches) if i != self.best_branch]
+        selected_branch = self.best_branch if x > self.epsilon else random.choice(
+            other_branches)
+
+        if self.history:
+            self.branch_history.append(selected_branch)
+            self.value_history.append(self.branch_values.copy())
+
+        logger.info("Routing to branch %s", selected_branch)
+        logger.debug("Current best branch %s has value %s",
+                     self.best_branch, self.branch_values[self.best_branch])
+        logger.debug("Selected branch %s has value %s",
+                     selected_branch, self.branch_values[selected_branch])
+
+        return selected_branch
+
+    def send_feedback(self, features, feature_names, routing, reward, truth):
+        loger.debug("Sending feedback with reward %s and truth %s",
+                    reward, truth)
+        logger.debug("Prev success # %s", self.branch_success)
+        logger.debug("Prev tries # %s", self.branch_tries)
+        logger.debug("Prev best branch: %s", self.best_branch)
+
+        n_success, n_failures = self.n_success_failures(features, reward)
+        logger.debug("n_success: %s, n_failures: %s", n_success, n_failures)
+
+        self.branch_success[routing] += n_success
+        self.branch_tries[routing] += n_success + n_failures
+        self.branch_values[routing] = self.branch_success[routing] / \
+            self.branch_tries[routing]
+
+        # break ties randomly
+        self.best_branch = np.random.choice(
+            np.where(np.array(self.branch_values) == max(self.branch_values))[0])
+
+        logger.debug("New success # %s", self.branch_success)
+        logger.debug("New tries # %s", self.branch_tries)
+        logger.debug("Branch values %s", self.branch_values)
+        logger.debug("New best branch: %s", self.best_branch)
+
+    def metrics(self):
+        try:
+            tags = self.branch_names
+        except AttributeError:
+            tags = range(self.n_branches)
+
+        branch_values = [{"type": "GAUGE", "key": "branch_value", "value": self.branch_values[b], "tags":{
+            "branch_name": tag}} for (b, tag) in zip(range(self.n_branches), tags)]
+        branch_success = [{"type": "GAUGE", "key": "n_success_total", "value": self.branch_success[b], "tags":{
+            "branch_name": tag}} for (b, tag) in zip(range(self.n_branches), tags)]
+        branch_tries = [{"type": "GAUGE", "key": "n_tries_total", "value": self.branch_tries[b], "tags":{
+            "branch_name": tag}} for (b, tag) in zip(range(self.n_branches), tags)]
+
+        return branch_values + branch_success + branch_tries
+
+    def n_success_failures(self, features, reward):
+        n_predictions = features.shape[0]
+        n_success = int(reward * n_predictions)
+        n_failures = n_predictions - n_success
+        return n_success, n_failures
---
+++ components/routers/thomson-sampling/ThomsonSampling.py
@@ -0,0 +1,130 @@
+import random
+import logging
+import numpy as np
+
+__version__ = "v1.0"
+logger = logging.getLogger(__name__)
+
+
+class ThomsonSampling(object):
+    """ Multi-armed bandit routing using Thomson Sampling strategy.
+
+    This class implements Thomson Sampling for the Beta-Binomial model, i.e.
+    rewards are assumed to come from a Bernoulli distribution for which the
+    conjugate prior is a Beta distribution.
+
+    The reward is assumed to be a single float between 0 and 1 indicating the
+    mean reward for a batch of samples. The prior is a Beta(1,1) distribution
+    (Uniform over the child components).
+
+    Parameters
+    ----------
+    n_branches : int
+        Number of child components/models the router will route requests to
+    verbose : bool
+        Set the logger level
+    seed : int, optional
+        Set the random seed
+    history : bool
+        Set storing router history
+    branch_names: str, optional
+        A string specifying branch names separated by `:`
+
+    """
+
+    def __init__(self, n_branches=None, verbose=False, seed=None, history=False,
+                 branch_names=None):
+
+        if verbose:
+            logger.setLevel(10)
+            logger.info("Enabling debug mode")
+
+        logger.info(
+            "Starting %s Microservice, version %s", __name__, __version__)
+
+        # for reproducibility
+        if seed:
+            logger.info("Setting random seed to %s", seed)
+            random.seed(seed)
+            np.random.seed(seed)
+
+        try:
+            n_branches = int(n_branches)
+        except (TypeError, ValueError) as e:
+            logger.exception("n_branches parameter must be given")
+            raise
+
+        self.name = __name__ + __version__
+        self.n_branches = n_branches
+        self.models_beta_params = [[1, 1] for _ in range(n_branches)]
+        self.verbose = verbose
+        self.history = history
+
+        self.branch_success = [0 for _ in range(n_branches)]
+        self.branch_tries = [0 for _ in range(n_branches)]
+        self.branch_values = [0 for _ in range(n_branches)]
+
+        if self.history:
+            logger.info("Enabling history")
+            self.branch_history = []
+            self.value_history = []
+
+        if branch_names is not None:
+            self.branch_names = branch_names.split(":")
+            logger.info("Branch names: %s", self.branch_names)
+
+        logger.info("Router initialised, n_branches: %s", self.n_branches)
+
+    def route(self, features, feature_names):
+        logger.debug("Routing features %s", features)
+
+        self.branch_values = [np.random.beta(
+            a, b) for a, b in self.models_beta_params]
+        selected_branch = np.argmax(self.branch_values)
+        logger.debug("Sampled branch values: %s", self.branch_values)
+
+        if self.history:
+            self.branch_history.append(selected_branch)
+            self.value_history.append(self.branch_values)
+
+        logger.info("Routing to branch %s", selected_branch)
+        return selected_branch
+
+    def send_feedback(self, features, feature_names, routing, reward, truth):
+        loger.debug("Sending feedback with reward %s and truth %s",
+                    reward, truth)
+        logger.debug("Prev success # %s", self.branch_success)
+        logger.debug("Prev tries # %s", self.branch_tries)
+
+        n_success, n_failures = self.n_success_failures(features, reward)
+        logger.debug("n_success: %s, n_failures: %s", n_success, n_failures)
+
+        self.models_beta_params[routing][0] += n_success
+        self.models_beta_params[routing][1] += n_failures
+
+        self.branch_success[routing] += n_success
+        self.branch_tries[routing] += n_success + n_failures
+
+        logger.debug("New success # %s", self.branch_success)
+        logger.debug("New tries # %s", self.branch_tries)
+
+    def metrics(self):
+        try:
+            tags = self.branch_names
+        except AttributeError:
+            tags = range(self.n_branches)
+
+        branch_values = [{"type": "GAUGE", "key": "branch_value", "value": self.branch_values[b], "tags":{
+            "branch_name": tag}} for (b, tag) in zip(range(self.n_branches), tags)]
+        branch_success = [{"type": "GAUGE", "key": "n_success_total", "value": self.branch_success[b], "tags":{
+            "branch_name": tag}} for (b, tag) in zip(range(self.n_branches), tags)]
+        branch_tries = [{"type": "GAUGE", "key": "n_tries_total", "value": self.branch_tries[b], "tags":{
+            "branch_name": tag}} for (b, tag) in zip(range(self.n_branches), tags)]
+
+        return branch_values + branch_success + branch_tries
+
+    def n_success_failures(self, features, reward):
+        n_predictions = features.shape[0]
+        n_success = int(reward * n_predictions)
+        n_failures = n_predictions - n_success
+        return n_success, n_failures
