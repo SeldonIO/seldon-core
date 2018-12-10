b9d502174849cf4deccefe002de06eafe7633c84
Janis Klaise <jk@seldon.io>
2018-12-10 16:34:34 +0000

    Move metric logic into child nodes/models

--- components/routers/case_study/models/xgb_model/XGBModel.py
+++ components/routers/case_study/models/xgb_model/XGBModel.py
@@ -1,7 +1,7 @@
 from sklearn.externals import joblib
 import logging
 
-__version__ = "v1.0"
+__version__ = "0.1"
 logger = logging.getLogger(__name__)
 
 
@@ -12,6 +12,10 @@ class XGBModel(object):
                     __name__, __version__)
         self.model = joblib.load('XGBModel.sav')
         self.cm = {'tp': 0, 'fp': 0, 'tn': 0, 'fn': 0}
+
+        self.tries = 0
+        self.success = 0
+        self.value = 0
 
     def predict(self, X, features_names):
         return self.model.predict_proba(X)
@@ -31,7 +35,12 @@ class XGBModel(object):
             if truth == 0:
                 self.cm['fp'] += 1
 
+        self.tries += 1
+        self.success = self.success + 1 if reward else self.success
+        self.value = self.success / self.tries
+
         logger.debug(self.cm)
+        logger.debug(self.tries, self.success, self.value)
 
     def metrics(self):
         tp = {"type": "GAUGE", "key": "true_pos_total",
@@ -43,4 +52,11 @@ class XGBModel(object):
         fn = {"type": "GAUGE", "key": "false_neg_total",
               "value": self.cm['fn'], "tags": {"branch_name": "xgb"}}
 
-        return [tp, tn, fp, fn]
+        value = {"type": "GAUGE", "key": "branch_value", "value": self.value,
+                 "tags": {"branch_name": "xgb"}}
+        success = {"type": "GAUGE", "key": "n_success_total", "value": self.success,
+                   "tags": {"branch_name": "xgb"}}
+        tries = {"type": "GAUGE", "key": "n_tries_total", "value": self.tries,
+                 "tags": {"branch_name": "xgb"}}
+
+        return [tp, tn, fp, fn, value, success, tries]
