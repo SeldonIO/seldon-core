15b04188587b9f923d3640173078306e715faf82
Clive Cox <cc@seldon.io>
2018-12-17 20:31:43 +0000

    updated openvino demo

--- examples/models/openvino_imagenet_ensemble/resources/transformer/ImageNetTransformer.py
+++ examples/models/openvino_imagenet_ensemble/resources/transformer/ImageNetTransformer.py
@@ -10,7 +10,9 @@ logger = logging.getLogger(__name__)
 class ImageNetTransformer(object):
     def __init__(self, metrics_ok=True):
         print("Init called")
-
+        f = open('imagenet_classes.json')
+        self.cnames = eval(f.read())
+        
     def transform_input_grpc(self, request):
         logger.info("Transform called")
         X = tf.make_ndarray(request.data.tftensor)
@@ -24,5 +26,15 @@ class ImageNetTransformer(object):
         request = prediction_pb2.SeldonMessage(data = datadef)
         return request
 
-    def transform_output_grpc(self, X, features_names):
-        return X
+    def transform_output_grpc(self, request):
+        print("Transform output called")
+        result = tf.make_ndarray(request.data.tftensor)
+        result = result.reshape(1,1000)
+
+        single_result = result[[0],...]
+        ma = np.argmax(single_result)
+        name = self.cnames[ma]
+
+        response = prediction_pb2.SeldonMessage(strData = name)
+        
+        return response
