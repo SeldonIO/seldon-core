45d92dcf600be66a827e510e51991f527f593a40
Clive Cox <cc@seldon.io>
2018-12-20 15:57:44 +0000

    Fix Async processing not working in engine and allow single Tracing span

--- engine/src/main/java/io/seldon/engine/tracing/TracingProvider.java
+++ engine/src/main/java/io/seldon/engine/tracing/TracingProvider.java
@@ -1,5 +1,7 @@
 package io.seldon.engine.tracing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import io.jaegertracing.Configuration;
@@ -8,6 +10,8 @@ import io.opentracing.Tracer;
 @Component
 public class TracingProvider {
 
+	private final static Logger logger = LoggerFactory.getLogger(TracingProvider.class);
+	
 	private final Tracer tracer;
 	private final boolean active;
 	
@@ -17,11 +21,13 @@ public class TracingProvider {
 		String tracingEnv = System.getenv().get("TRACING");
 		if (tracingEnv != null)
 		{
+			logger.info("Activating tracing");
 			active = true;
 			tracer = Configuration.fromEnv("seldon-svc-orch").getTracer();
 		}
 		else
 		{
+			logger.info("Not activating tracing");
 			active = false;
 			tracer = null;
 		}
