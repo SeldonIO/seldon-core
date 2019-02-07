eda4ba1c5277b1e01ebe90068085066093c3fe95
Clive Cox <cc@seldon.io>
2019-02-07 15:03:15 +0000

    updates from testing notebook

--- examples/models/openvino_imagenet_ensemble/resources/model/Prediction.py
+++ examples/models/openvino_imagenet_ensemble/resources/model/Prediction.py
@@ -29,8 +29,12 @@ def gs_download_file(path):
     parsed_path = urlparse(path)
     bucket_name = parsed_path.netloc
     file_path = parsed_path.path[1:]
-    gs_client = storage.Client()
-    bucket = gs_client.get_bucket(bucket_name)
+    try:
+        gs_client = storage.Client()
+        bucket = gs_client.get_bucket(bucket_name)
+    except:
+        gs_client = storage.Client.create_anonymous_client()
+        bucket = gs_client.bucket(bucket_name, user_project=None)
     blob = bucket.blob(file_path)
     tmp_path = os.path.join('/tmp', file_path.split(os.sep)[-1])
     blob.download_to_filename(tmp_path)
@@ -52,6 +56,7 @@ def s3_download_file(path):
 
 
 def GetLocalPath(requested_path):
+    print("Trying to download ",requested_path)
     parsed_path = urlparse(requested_path)
     if parsed_path.scheme == '':
         return requested_path
