1d2d83b8f879b0eb70a61b986db83551521e7db9
ryandawsonuk <rd@seldon.io>
2019-03-28 18:20:25 +0000

    apply resources to ambassador, note on minikube and retries for grpc calls

--- testing/scripts/seldon_utils.py
+++ testing/scripts/seldon_utils.py
@@ -4,6 +4,8 @@ from seldon_core.proto import prediction_pb2
 from seldon_core.proto import prediction_pb2_grpc
 import grpc
 import numpy as np
+import random
+from retrying import retry
 
 def create_random_data(data_size,rows=1):
     shape = [rows,data_size]
@@ -39,6 +41,7 @@ def rest_request_api_gateway(oauth_key,oauth_secret,namespace,endpoint="localhos
                 json=payload)
     return response
 
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=7)
 def grpc_request_api_gateway(oauth_key,oauth_secret,namespace,rest_endpoint="localhost:8002",grpc_endpoint="localhost:8003",data_size=5,rows=1,data=None):
     token = get_token(oauth_key,oauth_secret,namespace,rest_endpoint)
     if data is None:
@@ -96,6 +99,7 @@ def rest_request_ambassador_auth(deploymentName,namespace,username,password,endp
             auth=HTTPBasicAuth(username, password))
     return response
 
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=7)
 def grpc_request_ambassador(deploymentName,namespace,endpoint="localhost:8004",data_size=5,rows=1,data=None):
     if data is None:
         shape, arr = create_random_data(data_size,rows)
