53958acac39b0b584967ec74a583f64e11a358c3
Alejandro Saucedo <axsauze@gmail.com>
2019-05-24 11:36:23 +0100

    added pipeline

--- examples/kubeflow/nlp_pipeline.py
+++ examples/kubeflow/nlp_pipeline.py
@@ -8,6 +8,10 @@ from kubernetes import client as k8s
   description='A pipeline demonstrating reproducible steps for NLP'
 )
 def nlp_pipeline(
+        csv_url="https://raw.githubusercontent.com/axsauze/reddit-classification-exploration/master/data/reddit_train.csv",
+        csv_encoding="ISO-8859-1",
+        features_column="BODY",
+        labels_colum="REMOVED",
         raw_text_path='/mnt/text.data',
         labels_path='/mnt/labels.data',
         clean_text_path='/mnt/clean.data',
@@ -30,6 +34,21 @@ def nlp_pipeline(
       size="2Gi"
     )
 
+    download_step = dsl.ContainerOp(
+        name='data_downloader',
+        image='data_downloader:0.1',
+        arguments=[
+            "/app/pipeline_step.py",
+            "--labels-path", labels_path,
+            "--features-path", raw_text_path,
+            "--csv-url", csv_url,
+            "--csv-encoding", csv_encoding,
+            "--features-column", features_column,
+            "--labels-column", labels_column
+        ],
+        pvolumes={"/mnt": vop.volume}
+    )
+
     clean_step = dsl.ContainerOp(
         name='clean_text',
         image='clean_text_transformer:0.1',
@@ -38,7 +57,7 @@ def nlp_pipeline(
             "--in-path", raw_text_path,
             "--out-path", clean_text_path,
         ],
-        pvolumes={"/mnt": vop.volume}
+        pvolumes={"/mnt": download_step.volume}
     )
 
     tokenize_step = dsl.ContainerOp(
