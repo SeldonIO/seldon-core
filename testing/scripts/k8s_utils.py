823982c1f88823e0856aa4da01ebc75c292f188c
ryandawsonuk <rd@seldon.io>
2019-05-28 17:32:42 +0100

    find-replace to take latest image

--- testing/scripts/k8s_utils.py
+++ testing/scripts/k8s_utils.py
@@ -56,7 +56,7 @@ def get_seldon_version():
 
 # Not using latest Ambassador due to gRPC issue. https://github.com/datawire/ambassador/issues/504
 def create_ambassador():
-    run("helm install stable/ambassador --name ambassador --set image.tag=0.40.2 --namespace seldon", shell=True)
+    run("helm install stable/ambassador --name ambassador --namespace seldon", shell=True)
     run("kubectl rollout status deployment.apps/ambassador --namespace seldon", shell=True)
 
 
