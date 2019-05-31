1797f7154ff5a7da594e4653c64a55281217e30b
Clive Cox <cc@seldon.io>
2019-05-31 08:01:48 +0100

    remove branch usage in go compile for tests

--- testing/scripts/go_utils.py
+++ testing/scripts/go_utils.py
@@ -7,6 +7,6 @@ import time
 
 
 def build_go_images(version):
-    #run("rm -rf ${PWD}/go && export GOPATH=${PWD}/go && mkdir -p $GOPATH/src/github.com/seldonio/ && cd ./go/src/github.com/seldonio && git clone https://github.com/SeldonIO/seldon-operator.git && cd seldon-operator && make docker-build docker-push-local-private VERSION="+version, shell=True, check=True)
-    run("rm -rf ${PWD}/go && export GOPATH=${PWD}/go && mkdir -p $GOPATH/src/github.com/seldonio/ && cd ./go/src/github.com/seldonio && git clone --single-branch --branch ambassador_update https://github.com/cliveseldon/seldon-operator.git && cd seldon-operator && make docker-build docker-push-local-private VERSION="+version, shell=True, check=True)    
+    run("rm -rf ${PWD}/go && export GOPATH=${PWD}/go && mkdir -p $GOPATH/src/github.com/seldonio/ && cd ./go/src/github.com/seldonio && git clone https://github.com/SeldonIO/seldon-operator.git && cd seldon-operator && make docker-build docker-push-local-private VERSION="+version, shell=True, check=True)
+
 
