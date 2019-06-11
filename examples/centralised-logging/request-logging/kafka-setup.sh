607b9503a77e16528df655eab63a9c4fb1e4db09
ryandawsonuk <rd@seldon.io>
2019-06-11 18:03:06 +0100

    tidy up notes

--- examples/centralised-logging/request-logging/kafka-setup.sh
+++ examples/centralised-logging/request-logging/kafka-setup.sh
@@ -5,19 +5,8 @@ curl -L https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.11
 
 kubectl apply -f https://raw.githubusercontent.com/strimzi/strimzi-kafka-operator/0.11.4/examples/kafka/kafka-persistent-single.yaml -n kafka
 
-kubectl apply -f ./kafka-config.yaml
+kubectl apply -f https://github.com/knative/eventing/releases/download/v0.6.0/kafka.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/200-channelable-manipulator-clusterrole.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/200-controller-clusterrole.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/200-dispatcher-clusterrole.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/200-dispather-service.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/200-serviceaccount.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/200-webhook-clusterrole.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/201-clusterrolebinding.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/300-kafka-channel.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/400-webhook-service.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/500-controller.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/500-dispatcher.yaml
-kubectl apply -f https://raw.githubusercontent.com/knative/eventing/master/contrib/kafka/config/500-webhook.yaml
+kubectl apply -f ./kafka-config.yaml
 
 kubectl apply -f ./kafka-channel.yaml
