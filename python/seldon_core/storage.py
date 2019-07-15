4c9ab2cb67487d398ce4261651ad6cd6228d4ade
Clive Cox <cc@seldon.io>
2019-07-15 06:37:41 +0100

    add note about copied storage.py

--- python/seldon_core/storage.py
+++ python/seldon_core/storage.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# Copied from kfserving project as starter.
+#
+
 import glob
 import logging
 import tempfile
