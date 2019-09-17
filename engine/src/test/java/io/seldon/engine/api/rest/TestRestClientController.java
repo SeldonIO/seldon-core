d8c824a674df0c330655795320732cf1f0560afc
Adrián González Martín <kaseyo23@gmail.com>
2019-09-17 15:56:19 +0100

    Remove spring-security config and dependency

--- engine/src/test/java/io/seldon/engine/api/rest/TestRestClientController.java
+++ engine/src/test/java/io/seldon/engine/api/rest/TestRestClientController.java
@@ -47,7 +47,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 
 import static org.mockito.Mockito.when;
 
