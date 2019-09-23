50a0d2d7348b8822e2be618b7df5108d00959323
Adrian Gonzalez-Martin <agm@seldon.io>
2019-09-23 16:19:20 +0100

    Update import for LocalPort

--- engine/src/test/java/io/seldon/engine/api/rest/TestRandomABTest.java
+++ engine/src/test/java/io/seldon/engine/api/rest/TestRandomABTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
--- engine/src/test/java/io/seldon/engine/api/rest/TestRestClientController.java
+++ engine/src/test/java/io/seldon/engine/api/rest/TestRestClientController.java
@@ -20,7 +20,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
