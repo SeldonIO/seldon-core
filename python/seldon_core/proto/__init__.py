b656936bd8b411e241a50bdc07ed564ac63ba2ed
Adrian Gonzalez-Martin <agm@seldon.io>
2019-10-11 11:00:47 +0100

    Remove extra indentations from notice

--- python/seldon_core/proto/__init__.py
+++ python/seldon_core/proto/__init__.py
@@ -1,14 +1,17 @@
 import logging
+import textwrap
 from seldon_core.tf_helper import _TF_MISSING, _patch_tf_protos
 
+logger = logging.getLogger(__name__)
 
 if _TF_MISSING:
-    logging.info("""
+    notice = textwrap.dedent("""
         Tensorflow is not installed.
         If you want to use `tftensor` and Tensorflow's data types
         install `tensorflow` or install `seldon_core` as
 
             $ pip install seldon_core[tensorflow]
     """)
+    logger.info(notice)
     _patch_tf_protos()
 
