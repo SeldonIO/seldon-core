6a3ebda3d499360bd10b1feda6d0dd5d0018ffa1
cliveseldon <cc@seldon.io>
2019-10-16 18:48:33 +0100

    Allow self-signed certificates in helm chart for operator for all namespaces

--- operator/helm/split_resources.py
+++ operator/helm/split_resources.py
@@ -38,10 +38,16 @@ HELM_VALUES_IMAGE_PULL_POLICY = '{{ .Values.image.pullPolicy }}'
 def helm_value(value: str):
     return '{{ .Values.' + value + ' }}'
 
+def helm_release(value: str):
+    return '{{ .Release.' + value + ' }}'
 
 if __name__ == "__main__":
     exp = args.prefix + "*"
     files = glob.glob(exp)
+    webhookData = '{{- $altNames := list ( printf "seldon-webhook-service.%s" .Release.Namespace ) ( printf "seldon-webhook-service.%s.svc" .Release.Namespace ) -}}\n'
+    webhookData = webhookData + '{{- $ca := genCA "custom-metrics-ca" 365 -}}\n'
+    webhookData = webhookData + '{{- $cert := genSignedCert "seldon-webhook-service" nil $altNames 365 $ca -}}\n'
+
     for file in files:
         with open(file, 'r') as stream:
             res = yaml.safe_load(stream)
@@ -58,7 +64,7 @@ if __name__ == "__main__":
                 res["metadata"]["labels"][
                     "app.kubernetes.io/version"] = '{{ .Chart.Version }}'
 
-            # Update namespace to be helm var
+            # Update namespace to be helm var only if we are deploying into seldon-system
             if "metadata" in res and "namespace" in res["metadata"]:
                 if res["metadata"]["namespace"] == "seldon-system":
                     res["metadata"]["namespace"] = '{{ .Release.Namespace }}'
@@ -80,24 +86,43 @@ if __name__ == "__main__":
             if kind == "serviceaccount" and name == "seldon-manager":
                 res["metadata"]["name"] = helm_value("serviceAccount.name")
 
-            if kind == "clusterrolebinding" and name == "seldon-manager-rolebinding":
-                res["subjects"][0]["name"] = helm_value("serviceAccount.name")
+            # Update cluster role bindings
+            if kind == "clusterrolebinding":
+                if name == "seldon-manager-rolebinding":
+                    res["subjects"][0]["name"] = helm_value("serviceAccount.name")
+                    res["subjects"][0]["namespace"] = helm_release("Namespace")
+                elif name != "seldon-spartakus-volunteer":
+                    res["subjects"][0]["namespace"] = helm_release("Namespace")
+
+            # Update role bindings
+            if kind == "rolebinding":
+                res["subjects"][0]["namespace"] = helm_release("Namespace")
+
+
 
             # Update webhook certificates
             if name == "seldon-webhook-server-cert" and kind == "secret":
-                res["data"]["ca.crt"] = helm_value("webhook.ca.crt")
-                res["data"]["tls.crt"] = helm_value("webhook.tls.crt")
-                res["data"]["tls.key"] = helm_value("webhook.tls.key")
+                res["data"]["ca.crt"] = "{{ $ca.Cert | b64enc }}"
+                res["data"]["tls.crt"] = "{{ $cert.Cert | b64enc }}"
+                res["data"]["tls.key"] = "{{ $cert.Key | b64enc }}"
 
             if kind == "mutatingwebhookconfiguration" or kind == "validatingwebhookconfiguration":
-                res["webhooks"][0]["clientConfig"]["caBundle"] = helm_value("webhook.ca.crt")
+                res["webhooks"][0]["clientConfig"]["caBundle"] = "{{ $ca.Cert | b64enc }}"
+                res["webhooks"][0]["clientConfig"]["service"]["namespace"] = helm_release("Namespace")
+                if "certmanager.k8s.io/inject-ca-from" in res["metadata"]["annotations"]:
+                    res["metadata"]["annotations"]["certmanager.k8s.io/inject-ca-from"] = helm_release("Namespace") + "/seldon-serving-cert"
+
 
             if kind == "certificate":
                 res["spec"]["commonName"] = '{{- printf "seldon-webhook-service.%s.svc" .Release.Namespace -}}'
                 res["spec"]["dnsNames"][0] = '{{- printf "seldon-webhook-service.%s.svc.cluster.local" .Release.Namespace -}}'
 
-            if kind == "customresourcedefinition"and name == "seldondeployments.machinelearning.seldon.io" and "conversion" in res["spec"]:
-                res["spec"]["conversion"]["webhookClientConfig"]["caBundle"] = helm_value("webhook.ca.crt")
+            if kind == "customresourcedefinition"and name == "seldondeployments.machinelearning.seldon.io":
+                # Will only work for cert-manager at present as caBundle would need to be generated in same file as secrets above
+                if "conversion" in res["spec"]:
+                    res["spec"]["conversion"]["webhookClientConfig"]["caBundle"] = "=="
+                if "certmanager.k8s.io/inject-ca-from" in res["metadata"]["annotations"]:
+                    res["metadata"]["annotations"]["certmanager.k8s.io/inject-ca-from"] = helm_release("Namespace") + "/seldon-serving-cert"
 
             fdata = yaml.dump(res, width=1000)
 
@@ -117,5 +142,13 @@ if __name__ == "__main__":
                 fdata = HELM_NOT_CERTMANAGER_IF_START + fdata + HELM_IF_END
 
             if not kind == "namespace":
-                with open(filename, 'w') as outfile:
-                    outfile.write(fdata)
+                if name == "seldon-webhook-server-cert" and kind == "secret" or \
+                        kind == "mutatingwebhookconfiguration" or kind == "validatingwebhookconfiguration":
+                    webhookData = webhookData + "---\n\n" + fdata
+                else:
+                    with open(filename, 'w') as outfile:
+                        outfile.write(fdata)
+    # Write webhook related data in 1 file
+    filename = args.folder + "/" + "webhook.yaml"
+    with open(filename, 'w') as outfile:
+        outfile.write(webhookData)
