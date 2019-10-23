859026aaa3ef387974ea8d69f3ca1e9e8c04deee
Alejandro Saucedo <axsauze@gmail.com>
2019-10-23 15:52:15 +0100

    Removed to see if kindsetup works

--- testing/scripts/kind_test_all.sh
+++ testing/scripts/kind_test_all.sh
@@ -43,13 +43,13 @@ if [[ ${KIND_EXIT_VALUE} -eq 0 ]]; then
     make kind_setup
     SETUP_EXIT_VALUE=$?
 
-    # INSTALL ALL REQUIRED DEPENDENCIES
-    make -C ../../python install-dev
-    INSTALL_EXIT_VALUE=$?
+    ## INSTALL ALL REQUIRED DEPENDENCIES
+    #make -C ../../python install-dev
+    #INSTALL_EXIT_VALUE=$?
 
-    # RUNNING TESTS AND CAPTURING ERROR
-    make test
-    TEST_EXIT_VALUE=$?
+    ## RUNNING TESTS AND CAPTURING ERROR
+    #make test
+    #TEST_EXIT_VALUE=$?
 fi
 
 # DELETE KIND CLUSTER
