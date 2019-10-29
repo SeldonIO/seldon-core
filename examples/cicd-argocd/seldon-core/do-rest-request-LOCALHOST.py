6d94368b1243cf61316c8a891af7f5136ed29f6f
Gurminder Sunner <gsunner2000@gmail.com>
2019-10-29 11:33:37 +0000

    update components for cicd demo

--- examples/cicd-argocd/seldon-core/do-rest-request-LOCALHOST.py
+++ examples/cicd-argocd/seldon-core/do-rest-request-LOCALHOST.py
@@ -4,14 +4,15 @@ import requests
 import json
 
 def rest_request_ambassador(deploymentName, request, endpoint="localhost:8003"):
+    namespace_name="default"
     response = requests.post(
-        "http://" + endpoint + "/seldon/" + deploymentName + "/api/v0.1/predictions",
+        "http://" + endpoint + "/seldon/" + namespace_name + "/" + deploymentName + "/api/v0.1/predictions",
         json=request)
     return response.json()
 
 def rest_request():
     payload = {"data":{"names":["a","b"],"tensor":{"shape":[2,2],"values":[0,0,1,1]}}}
-    response_dict=rest_request_ambassador("seldon-deployment-example", payload, endpoint="localhost:8080")
+    response_dict=rest_request_ambassador("seldon-deployment-example", payload, endpoint="localhost:8003")
     response_json=json.dumps(response_dict, sort_keys=True, indent=4, separators=(',', ': '))
     print(response_json)
 
