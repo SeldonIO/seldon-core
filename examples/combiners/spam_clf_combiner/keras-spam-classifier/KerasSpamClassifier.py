eea2ee9da24e8c0a431efb096b2bf9bd43535eee
Sandhya Sandhya <sandhya.sandhya@ssandhya-mbp.local>
2019-11-04 16:33:47 -0600

    added keras model architecture files along with working python code

--- examples/combiners/spam_clf_combiner/keras-spam-classifier/KerasSpamClassifier.py
+++ examples/combiners/spam_clf_combiner/keras-spam-classifier/KerasSpamClassifier.py
@@ -2,15 +2,17 @@ from pathlib import Path
 import numpy as np
 import pickle
 from sklearn.externals import joblib 
-
+from keras.preprocessing import sequence
+from keras.engine.saving import model_from_json
 
 class KerasSpamClassifier():
 
-    def __init__(self, parent_path = Path('/model')):
+    def __init__(self, model_path = Path('./model')):
         self._architecture_path = model_path.joinpath('architecture.json')
         self._weights_path = model_path.joinpath('weights.h5')
         tokenizer_path = model_path.joinpath('tokenizer.pkl')
 
+        self.max_len = 150
         # loading of trained tokenizer
         with tokenizer_path.open('rb') as handle:
             self.tokenizer = joblib.load(handle)
@@ -26,11 +28,17 @@ class KerasSpamClassifier():
         """
         Predict on a english text you got from translator service. The output returns the probability of text being spam
         """
+        seq= self.tokenizer.texts_to_sequences([text])
+        padded = sequence.pad_sequences(seq, maxlen=self.max_len)
+        prob = self.model.predict(padded)
 
-        probas = self.model.predict([x, np.array(x_char)])[0]
-
-        prob = probas[0][1]
-
-        return np.array([prob, "spam"])
+        return np.array([prob[0][0], "spam"])
 
 
+if __name__== "__main__":
+
+    text = 'please click this link to win the price'
+    #vect_path=Path('/Users/sandhya.sandhya/Desktop/data/doc/data'),
+    sc = KerasSpamClassifier(model_path=Path('./model'))
+    res = sc.predict(text, feature_names=None)
+    print(res)
