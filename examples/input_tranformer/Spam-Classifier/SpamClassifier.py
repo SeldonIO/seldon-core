52aa5b39740a2daf173015437d02ccbb50b0fa51
Sandhya Sandhya <sandhya.sandhya@ssandhya-mbp.local>
2019-11-04 12:28:26 -0600

    working example with correct model file and vectorizer

--- examples/input_tranformer/Spam-Classifier/SpamClassifier.py
+++ examples/input_tranformer/Spam-Classifier/SpamClassifier.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import numpy as np
 import pickle
-from sklearn.externals import joblib 
+from sklearn.externals import joblib
+import sklearn 
 
 
 class SpamClassifier():
@@ -17,12 +18,18 @@ class SpamClassifier():
         """
         Predict on a english text you got from translator service. The output returns the probability of text being spam
         """
-
-        clean_text  = self.preprocess(text)
-        data = self.vectorizer.transform([clean_text]).todense()
+        data = self.vectorizer.transform([text]).todense()
         probas = self.clf.predict_proba(data)
         prob = probas[0][1]
 
         return np.array([prob, "spam"])
 
 
+# if __name__== "__main__":
+#     print('The scikit-learn version is {}.'.format(sklearn.__version__))
+
+#     text = 'click here to win the price'
+#     #vect_path=Path('/Users/sandhya.sandhya/Desktop/data/doc/data'),
+#     sc = SpamClassifier(model_path=Path('./model'))
+#     res = sc.predict(text, feature_names=None)
+#     print(res)
