8131877b4213d0cdef203a37e5afccc057e016dd
Sandhya Sandhya <sandhya.sandhya@ssandhya-mbp.local>
2019-11-03 22:50:37 -0600

    updated Combiner.py

--- examples/combiners/spam_clf_combiner/Combiner/Combiner.py
+++ examples/combiners/spam_clf_combiner/Combiner/Combiner.py
@@ -3,19 +3,19 @@ import numpy as np
 class Combiner(object):
 
     def aggregate(self, Xs, features_names=None):
+    	
+        return np.mean([float(x[0]) for x in Xs])
 
-        return max(Xs, key=lambda item: float(item[0]))
 
 
+# if __name__== "__main__":
+#     clf1_res = np.array(['0.80', 'Spam'])
+#     clf2_res = np.array(['0.9959868467126312e-04', 'Spam'])
 
-if __name__== "__main__":
-    clf1_res = np.array(['0.80', 'Spam'])
-    clf2_res = np.array(['0.9959868467126312e-04', 'Spam'])
-
-    example = np.array([clf1_res, clf2_res])
-    dc = Combiner()
-    res = dc.aggregate(example, features_names=None)
-    print(res)
+#     example = np.array([clf1_res, clf2_res])
+#     dc = Combiner()
+#     res = dc.aggregate(example, features_names=None)
+#     print(res)
 
 
 
