611c19a230b1ddf6082afb5f7e23b92a0ed79274
Alejandro Saucedo <axsauze@gmail.com>
2019-11-11 08:01:35 +0000

    Updated for linter

--- testing/scripts/test_rolling_updates.py
+++ testing/scripts/test_rolling_updates.py
@@ -15,13 +15,13 @@ class TestRollingHttp(object):
     def test_rolling_update1(self):
         namespace = "test-rolling-update-1"
         run(f"kubectl create namespace {namespace}", shell=True, check=True)
-        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}")
         wait_for_rollout("mymodel-mymodel-e2eb561", namespace)
         print("Initial request")
         r = initial_rest_request("mymodel", namespace)
         assert r.status_code == 200
         assert r.json()["data"]["tensor"]["values"] == [1.0, 2.0, 3.0, 4.0]
-        retry_run(f"kubectl apply -f ../resources/graph2.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph2.json -n {namespace}")
         i = 0
         for i in range(100):
             r = rest_request_ambassador("mymodel", namespace, API_AMBASSADOR)
@@ -44,25 +44,21 @@ class TestRollingHttp(object):
             time.sleep(1)
         assert i < 100
         print("Success for test_rolling_update1")
-        run(
-            f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True,
-        )
-        run(
-            f"kubectl delete -f ../resources/graph2.json -n {namespace}", shell=True,
-        )
+        run(f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True)
+        run(f"kubectl delete -f ../resources/graph2.json -n {namespace}", shell=True)
         run(f"kubectl delete namespace {namespace}", shell=True)
 
     # test changing the image version and the name of its container
     def test_rolling_update2(self):
         namespace = "test-rolling-update-2"
         run(f"kubectl create namespace {namespace}", shell=True, check=True)
-        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}")
         wait_for_rollout("mymodel-mymodel-e2eb561", namespace)
         print("Initial request")
         r = initial_rest_request("mymodel", namespace)
         assert r.status_code == 200
         assert r.json()["data"]["tensor"]["values"] == [1.0, 2.0, 3.0, 4.0]
-        retry_run(f"kubectl apply -f ../resources/graph3.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph3.json -n {namespace}")
         i = 0
         for i in range(100):
             r = rest_request_ambassador("mymodel", namespace, API_AMBASSADOR)
@@ -86,25 +82,21 @@ class TestRollingHttp(object):
             time.sleep(1)
         assert i < 100
         print("Success for test_rolling_update2")
-        run(
-            f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True,
-        )
-        run(
-            f"kubectl delete -f ../resources/graph3.json -n {namespace}", shell=True,
-        )
+        run(f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True)
+        run(f"kubectl delete -f ../resources/graph3.json -n {namespace}", shell=True)
         run(f"kubectl delete namespace {namespace}", shell=True)
 
     # Test updating a model with a new resource request but same image
     def test_rolling_update3(self):
         namespace = "test-rolling-updates-3"
         run(f"kubectl create namespace {namespace}", shell=True, check=True)
-        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}")
         wait_for_rollout("mymodel-mymodel-e2eb561", namespace)
         print("Initial request")
         r = initial_rest_request("mymodel", namespace)
         assert r.status_code == 200
         assert r.json()["data"]["tensor"]["values"] == [1.0, 2.0, 3.0, 4.0]
-        retry_run(f"kubectl apply -f ../resources/graph4.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph4.json -n {namespace}")
         i = 0
         for i in range(50):
             r = rest_request_ambassador("mymodel", namespace, API_AMBASSADOR)
@@ -122,25 +114,21 @@ class TestRollingHttp(object):
             time.sleep(1)
         assert i == 49
         print("Success for test_rolling_update3")
-        run(
-            f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True,
-        )
-        run(
-            f"kubectl delete -f ../resources/graph4.json -n {namespace}", shell=True,
-        )
+        run(f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True)
+        run(f"kubectl delete -f ../resources/graph4.json -n {namespace}", shell=True)
         run(f"kubectl delete namespace {namespace}", shell=True)
 
     # Test updating a model with a multi deployment new model
     def test_rolling_update4(self):
         namespace = "test-rolling-update-4"
         run(f"kubectl create namespace {namespace}", shell=True, check=True)
-        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}")
         wait_for_rollout("mymodel-mymodel-e2eb561", namespace)
         print("Initial request")
         r = initial_rest_request("mymodel", namespace)
         assert r.status_code == 200
         assert r.json()["data"]["tensor"]["values"] == [1.0, 2.0, 3.0, 4.0]
-        retry_run(f"kubectl apply -f ../resources/graph5.json -n {namespace}",)
+        retry_run(f"kubectl apply -f ../resources/graph5.json -n {namespace}")
         i = 0
         for i in range(50):
             r = rest_request_ambassador("mymodel", namespace, API_AMBASSADOR)
@@ -162,10 +150,6 @@ class TestRollingHttp(object):
             time.sleep(1)
         assert i < 100
         print("Success for test_rolling_update4")
-        run(
-            f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True,
-        )
-        run(
-            f"kubectl delete -f ../resources/graph5.json -n {namespace}", shell=True,
-        )
+        run(f"kubectl delete -f ../resources/graph1.json -n {namespace}", shell=True)
+        run(f"kubectl delete -f ../resources/graph5.json -n {namespace}", shell=True)
         run(f"kubectl delete namespace {namespace}", shell=True)
