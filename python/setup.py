0f089326c3ba3ada33a35612928826b93c30ebe4
Adrian Gonzalez-Martin <agm@seldon.io>
2019-11-11 14:03:52 +0000

    Add version ceiling to a few packages and remove tornados and future from requirements

--- python/setup.py
+++ python/setup.py
@@ -43,7 +43,7 @@ setup(
         "azure-storage-blob >= 2.0.1, < 3.0.0",
         "setuptools >= 41.0.0",
     ],
-    tests_require=["pytest", "pytest-cov", "Pillow"],
+    tests_require=["pytest<6.0.0", "pytest-cov<3.0.0", "Pillow==6.2.0"],
     extras_require=extras,
     test_suite="tests",
     entry_points={
