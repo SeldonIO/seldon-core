3d0d636d40520ebfa7d29bbbb8f76c226f521145
ryandawsonuk <rd@seldon.io>
2019-11-12 17:05:46 +0000

    shadow traffic going throug

--- operator/controllers/seldondeployment_controller.go
+++ operator/controllers/seldondeployment_controller.go
@@ -168,39 +168,24 @@ func createIstioResources(mlDep *machinelearningv1alpha2.SeldonDeployment,
 		},
 	}
 
-	routesHttp := make([]istio.HTTPRouteDestination, len(mlDep.Spec.Predictors))
-	routesGrpc := make([]istio.HTTPRouteDestination, len(mlDep.Spec.Predictors))
+	// shadows don't get destinations in the vs as a shadow is a mirror instead
+	var shadows int = 0
+	for i := 0; i < len(mlDep.Spec.Predictors); i++ {
+		p := mlDep.Spec.Predictors[i]
+		if p.Shadow == "true" {
+			shadows += 1
+		}
+	}
+
+	routesHttp := make([]istio.HTTPRouteDestination, len(mlDep.Spec.Predictors)-shadows)
+	routesGrpc := make([]istio.HTTPRouteDestination, len(mlDep.Spec.Predictors)-shadows)
+
+	// the shdadow/mirror entry does need a DestinationRule though
 	drules := make([]*istio.DestinationRule, len(mlDep.Spec.Predictors))
 	for i := 0; i < len(mlDep.Spec.Predictors); i++ {
 
 		p := mlDep.Spec.Predictors[i]
 		pSvcName := machinelearningv1alpha2.GetPredictorKey(mlDep, &p)
-
-		if p.Shadow == "true" {
-			//if there's a shadow then add a mirror section to the VirtualService
-
-			httpVsvc.Spec.HTTP[0].Mirror = &istio.Destination{
-				Host:   pSvcName + "." + namespace + ".svc.cluster.local",
-				Subset: p.Name,
-				Port: istio.PortSelector{
-					Number: uint32(ports[i].httpPort),
-				},
-			}
-
-			fmt.Println("VirtualService for " + p.Name)
-			out, _ := json.Marshal(httpVsvc)
-			fmt.Println(string(out))
-
-			grpcVsvc.Spec.HTTP[0].Mirror = &istio.Destination{
-				Host:   pSvcName + "." + namespace + ".svc.cluster.local",
-				Subset: p.Name,
-				Port: istio.PortSelector{
-					Number: uint32(ports[i].grpcPort),
-				},
-			}
-
-			continue
-		}
 
 		drule := &istio.DestinationRule{
 			ObjectMeta: metav1.ObjectMeta{
@@ -225,6 +210,30 @@ func createIstioResources(mlDep *machinelearningv1alpha2.SeldonDeployment,
 			},
 		}
 
+		drules[i] = drule
+
+		if p.Shadow == "true" {
+			//if there's a shadow then add a mirror section to the VirtualService
+
+			httpVsvc.Spec.HTTP[0].Mirror = &istio.Destination{
+				Host:   pSvcName,
+				Subset: p.Name,
+				Port: istio.PortSelector{
+					Number: uint32(ports[i].httpPort),
+				},
+			}
+
+			grpcVsvc.Spec.HTTP[0].Mirror = &istio.Destination{
+				Host:   pSvcName,
+				Subset: p.Name,
+				Port: istio.PortSelector{
+					Number: uint32(ports[i].grpcPort),
+				},
+			}
+
+			continue
+		}
+
 		//we split by adding different routes with their own Weights
 		//so not by tag - different destinations (like https://istio.io/docs/tasks/traffic-management/traffic-shifting/) distinguished by host
 		routesHttp[i] = istio.HTTPRouteDestination{
@@ -247,18 +256,11 @@ func createIstioResources(mlDep *machinelearningv1alpha2.SeldonDeployment,
 			},
 			Weight: int(p.Traffic),
 		}
-		drules[i] = drule
+
 	}
 	httpVsvc.Spec.HTTP[0].Route = routesHttp
 	grpcVsvc.Spec.HTTP[0].Route = routesGrpc
 
-	// TODO: currently this is producing a VirtualService with two destinationRules
-	// the second has an empty host and both have weight zero
-	// like a kind of ghost entry for the shadow?
-
-	fmt.Println("VirtualService for " + mlDep.Name)
-	out, _ := json.Marshal(httpVsvc)
-	fmt.Println(string(out))
 	if httpAllowed && grpcAllowed {
 		vscs := make([]*istio.VirtualService, 2)
 		vscs[0] = httpVsvc
