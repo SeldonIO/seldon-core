db7fcd0c196f8e56ef78e36859acae35663ed8f8
Alejandro Saucedo <axsauze@gmail.com>
2019-11-13 08:20:03 +0000

    Adding explicit run of script on background

--- testing/scripts/conftest.py
+++ testing/scripts/conftest.py
@@ -1,13 +1,21 @@
 import pytest
 from seldon_e2e_utils import get_s2i_python_version
+from subprocess import run
+
+
+@pytest.fixture(scope="session", autouse=True)
+def run_pod_information_in_background(request):
+    # This command runs the pod information and prints it
+    #   in the background every time there's a new update
+    run(
+        "kubectl get pods --all-namespaces -w | awk '{print \"\nPODS UPDATE: \"$0}\n' &",
+        shell=True,
+    )
 
 
 @pytest.fixture(scope="module")
 def s2i_python_version():
     return do_s2i_python_version()
-
-
-#### Implementations below
 
 
 def do_s2i_python_version():
