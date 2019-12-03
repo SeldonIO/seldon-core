aec1826834c6093d20288d22c026a85861fdff5b
ryandawsonuk <rd@seldon.io>
2019-12-03 15:07:50 +0000

    minimize rolling updates to main predictor

--- operator/controllers/seldondeployment_controller.go
+++ operator/controllers/seldondeployment_controller.go
@@ -1038,6 +1038,8 @@ func createDeployments(r *SeldonDeploymentReconciler, components *components, in
 
 				if !reflect.DeepEqual(deploy.Spec.Template.Spec.Containers[0].Resources, found.Spec.Template.Spec.Containers[0].Resources) {
 					log.Info("Containers differ")
+				} else {
+					log.Info("Deployments differ")
 				}
 
 				ready = false
