35e8b03821452db091547bcea63242b0fa456c82
Adrian Gonzalez-Martin <agm@seldon.io>
2019-12-11 17:39:15 +0000

    Add test deploying different apiVersions

---
+++ testing/scripts/test_api_version.py
@@ -0,0 +1,42 @@
+import pytest
+from seldon_e2e_utils import (
+    wait_for_rollout,
+    initial_rest_request,
+    rest_request_ambassador,
+    retry_run,
+    API_AMBASSADOR,
+)
+from subprocess import run
+
+
+@pytest.mark.parametrize(
+    "apiVersion",
+    [
+        "machinelearning.seldon.io/v1alpha2",
+        "machinelearning.seldon.io/v1alpha3",
+        "machinelearning.seldon.io/v1",
+    ],
+)
+def test_api_version(apiVersion):
+    version = apiVersion.split("/")[-1]
+    namespace = f"test-api-version-{version}"
+    retry_run(f"kubectl create namespace {namespace}")
+    command = (
+        "helm install mymodel ../../helm-charts/seldon-single-model "
+        "--set oauth.key=oauth-key "
+        "--set oauth.secret=oauth-secret "
+        f"--set apiVersion={apiVersion} "
+        f"--namespace {namespace}"
+    )
+    run(command, shell=True, check=True)
+
+    wait_for_rollout(f"mymodel-mymodel-7cd068f", namespace)
+    initial_rest_request("mymodel", namespace)
+
+    r = rest_request_ambassador("mymodel", namespace, API_AMBASSADOR)
+
+    assert r.status_code == 200
+    assert len(r.json()["data"]["tensor"]["values"]) == 1
+
+    run(f"helm delete mymodel", shell=True)
+    run(f"kubectl delete namespace {namespace}", shell=True)
