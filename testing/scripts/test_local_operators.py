d221d6ca4356552bc9408f87ce888df105d81667
cliveseldon <cc@seldon.io>
2019-12-16 12:13:43 +0000

    fix lint

--- testing/scripts/test_local_operators.py
+++ testing/scripts/test_local_operators.py
@@ -13,13 +13,13 @@ from seldon_e2e_utils import (
 )
 
 
-
 class TestLocalOperators(object):
-
     def test_namespace_operator(self):
         namespace = "test-namespaced-operator"
         retry_run(f"kubectl create namespace {namespace}")
-        retry_run(f"helm install seldon ../../helm-charts/seldon-core-operator --namespace {namespace} --set istio.enabled=true --set istio.gateway=seldon-gateway --set certManager.enabled=false --set crd.create=false --set singleNamespace=true")
+        retry_run(
+            f"helm install seldon ../../helm-charts/seldon-core-operator --namespace {namespace} --set istio.enabled=true --set istio.gateway=seldon-gateway --set certManager.enabled=false --set crd.create=false --set singleNamespace=true"
+        )
         retry_run(f"kubectl apply -f ../resources/graph1.json -n {namespace}")
         wait_for_rollout("mymodel-mymodel-e2eb561", namespace)
         logging.warning("Initial request")
@@ -33,7 +33,9 @@ class TestLocalOperators(object):
     def test_labelled_operator(self):
         namespace = "test-labelled-operator"
         retry_run(f"kubectl create namespace {namespace}")
-        retry_run(f"helm install seldon ../../helm-charts/seldon-core-operator --namespace {namespace} --set istio.enabled=true --set istio.gateway=seldon-gateway --set certManager.enabled=false --set crd.create=false --set controllerId=seldon-id1")
+        retry_run(
+            f"helm install seldon ../../helm-charts/seldon-core-operator --namespace {namespace} --set istio.enabled=true --set istio.gateway=seldon-gateway --set certManager.enabled=false --set crd.create=false --set controllerId=seldon-id1"
+        )
         retry_run(f"kubectl apply -f ../resources/model_controller_id.yaml -n default")
         wait_for_rollout("test-c1-example-cf749e0", "default")
         logging.warning("Initial request")
@@ -41,5 +43,8 @@ class TestLocalOperators(object):
         assert r.status_code == 200
         assert r.json()["data"]["tensor"]["values"] == [1.0, 2.0, 3.0, 4.0]
         logging.warning("Success for test_labelled_operator")
-        run(f"kubectl delete -f ../resources/model_controller_id.yaml -n default", shell=True)
+        run(
+            f"kubectl delete -f ../resources/model_controller_id.yaml -n default",
+            shell=True,
+        )
         run(f"kubectl delete namespace {namespace}", shell=True)
