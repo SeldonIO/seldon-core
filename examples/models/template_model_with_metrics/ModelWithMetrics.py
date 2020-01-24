519cc3b08e88f7602af93e52c800e3b1a1884416
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2020-01-24 14:31:37 +0000

    add example for custom metrics in feedback

--- examples/models/template_model_with_metrics/ModelWithMetrics.py
+++ examples/models/template_model_with_metrics/ModelWithMetrics.py
@@ -4,14 +4,17 @@ class ModelWithMetrics(object):
     def __init__(self):
         print("Initialising")
 
-    def predict(self,X,features_names):
+    def predict(self, X, features_names):
         print("Predict called")
         return X
 
+    def send_feedback(self, features, feature_names, reward, truth, routing=None):
+        print("Send feedback called")
+        return []
+
     def metrics(self):
         return [
-            {"type":"COUNTER","key":"mycounter","value":1}, # a counter which will increase by the given value
-            {"type":"GAUGE","key":"mygauge","value":100}, # a gauge which will be set to given value
-            {"type":"TIMER","key":"mytimer","value":20.2}, # a timer which will add sum and count metrics - assumed millisecs
-            ]
-    
+            {"type": "COUNTER", "key": "mycounter", "value": 1}, # a counter which will increase by the given value
+            {"type": "GAUGE", "key": "mygauge", "value": 100}, # a gauge which will be set to given value
+            {"type": "TIMER", "key": "mytimer", "value": 20.2}, # a timer which will add sum and count metrics - assumed millisecs
+        ]
