2429329821b5183a8945bf555c0b7464f3eaf08a
Adrian Gonzalez-Martin <agm@seldon.io>
2020-02-19 16:58:13 +0000

    Add some comments on how we fetch traces

--- testing/scripts/jaeger_utils.py
+++ testing/scripts/jaeger_utils.py
@@ -16,6 +16,36 @@ def _is_empty(result):
     retry=retry_if_result(_is_empty),
 )
 def get_traces(pod_name, service, operation):
+    """
+    Fetch traces for a given pod, service and operation.
+
+    We use Jaeger's [**internal** REST
+    API](https://www.jaegertracing.io/docs/1.13/apis/#http-json-internal).
+    Therefore, it may stop working at some point!
+
+    Note that this method will get retried 5 times (with an exponentially
+    growing waiting time) if the traces are empty.
+    This is to give time to Jaeger to collect and process the trace, which is
+    performed asynchronously.
+
+    Parameters
+    ---
+    pod_name : str
+        We currently don't have access to the PUID (see
+        https://github.com/SeldonIO/seldon-core/issues/1460).
+        As a workaround, we filter the traces using the Pod name.
+    service : str
+        Service sending the traces.
+        This will usually be the `'executor'`, since it's the one which creates
+        the trace.
+    operation : str
+        Operation which was traced (e.g. `'predictions'`).
+
+    Returns
+    ---
+    traces : list
+        List of traces, where each trace contains spans, processes, etc.
+    """
     endpoint = f"{JAEGER_QUERY_URL}/api/traces"
     params = {"service": service, "operation": operation, "tag": f"hostname:{pod_name}"}
     response = requests.get(endpoint, params=params)
