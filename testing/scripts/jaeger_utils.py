9537cedc7604edea1286683fbd95933053c5949c
Adrian Gonzalez-Martin <agm@seldon.io>
2020-02-19 15:52:01 +0000

    Filter traces by pod name

--- testing/scripts/jaeger_utils.py
+++ testing/scripts/jaeger_utils.py
@@ -5,9 +5,9 @@ from seldon_e2e_utils import API_AMBASSADOR
 JAEGER_QUERY_URL = f"http://{API_AMBASSADOR}/jaeger"
 
 
-def get_traces(service, operation):
+def get_traces(pod_name, service, operation):
     endpoint = f"{JAEGER_QUERY_URL}/api/traces"
-    params = dict(service=service, operation=operation)
+    params = {"service": service, "operation": operation, "tag": f"hostname:{pod_name}"}
     response = requests.get(endpoint, params=params)
     payload = response.json()
     return payload["data"]
