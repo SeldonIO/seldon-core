4f8c0567d1cd139f224a6533d2515f98a21dcc9f
Adrian Gonzalez-Martin <agm@seldon.io>
2020-02-19 15:53:47 +0000

    Assert traces by operation order (and referencing)

--- testing/scripts/test_tracing.py
+++ testing/scripts/test_tracing.py
@@ -1,10 +1,30 @@
 from seldon_e2e_utils import (
+    get_pod_names,
+    get_deployment_names,
     retry_run,
     wait_for_status,
     wait_for_rollout,
     initial_rest_request,
 )
 from jaeger_utils import get_traces
+
+
+def assert_trace(trace, expected_operations):
+    spans = trace["spans"]
+    assert len(spans) == len(expected_operations)
+
+    # Assert the operations are in the right order
+    for idx, span in enumerate(spans):
+        assert span["operationName"] == expected_operations[idx]
+        if idx > 0:
+            # Assert there is only one ref to the previous span
+            refs = span["references"]
+            assert len(refs) == 1
+
+            prev_span = spans[idx - 1]
+            ref = span["references"][0]
+            assert ref["refType"] == "CHILD_OF"
+            assert ref["spanID"] == prev_span["traceID"]
 
 
 def test_tracing_rest(namespace):
@@ -14,4 +34,17 @@ def test_tracing_rest(namespace):
     wait_for_rollout("mymodel", namespace)
     initial_rest_request("mymodel", namespace)
 
-    traces = get_traces("executor", "predictions")
+    # We need the current pod name to find the right traces
+    deployment_names = get_deployment_names("mymodel", namespace)
+    deployment_name = deployment_names[0]
+    pod_names = get_pod_names(deployment_name, namespace)
+    pod_name = pod_names[0]
+
+    traces = get_traces(pod_name, "executor", "predictions")
+    assert len(traces) == 1
+
+    trace = traces[0]
+    processes = trace["processes"]
+
+    assert len(processes) == 2
+    assert_trace(trace, expected_operations=["predictions", "/predict", "Predict"])
