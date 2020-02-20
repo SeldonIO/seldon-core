f543c01dc2c36a86344441b91f37a4534df756ae
Gurminder Sunner <gsunner2000@gmail.com>
2020-02-20 13:11:04 +0000

    add operator helm chart executor version update in release script

--- release.py
+++ release.py
@@ -103,6 +103,7 @@ def update_operator_values_yaml_file(fpath, seldon_core_version, debug=False):
     d = yaml_to_dict(yaml_data)
     d["image"]["tag"] = seldon_core_version
     d["engine"]["image"]["tag"] = seldon_core_version
+    d["executor"]["image"]["tag"] = seldon_core_version
 
     with open(fpath, "w") as f:
         f.write(dict_to_yaml(d))
