d78868b1a47972b434faaa5e8817ebeb83aef149
Alejandro Saucedo <axsauze@gmail.com>
2020-03-12 08:32:11 +0000

    Update seldondeployment_prepackaged_servers.go

--- operator/controllers/seldondeployment_prepackaged_servers.go
+++ operator/controllers/seldondeployment_prepackaged_servers.go
@@ -18,14 +18,13 @@ package controllers
 
 import (
 	"encoding/json"
-	"strconv"
-	"strings"
-
 	machinelearningv1 "github.com/seldonio/seldon-core/operator/apis/machinelearning/v1"
 	"github.com/seldonio/seldon-core/operator/constants"
 	"github.com/seldonio/seldon-core/operator/utils"
 	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
+	"strconv"
+	"strings"
 )
 
 const (
