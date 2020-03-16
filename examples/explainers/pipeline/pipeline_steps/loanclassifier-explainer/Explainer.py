52bbf12a0ce6449a02c88d2db2e207126e400c24
Alejandro Saucedo <axsauze@gmail.com>
2020-03-16 15:33:32 +0000

    Added changes on alibi

---
+++ examples/explainers/pipeline/pipeline_steps/loanclassifier-explainer/Explainer.py
@@ -0,0 +1,42 @@
+import dill
+import json
+import numpy as np
+
+
+class Explainer:
+    def __init__(self, *args, **kwargs):
+
+        with open("explainer.dill", "rb") as x_f:
+            self.explainer = dill.load(x_f)
+
+    def predict(self, X, feature_names=[]):
+        print("Received: " + str(X))
+        explanation = self.explainer.explain(X)
+        print("Predicted: " + str(explanation))
+        return json.dumps(explanation, cls=NumpyEncoder)
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(
+            obj,
+            (
+                np.int_,
+                np.intc,
+                np.intp,
+                np.int8,
+                np.int16,
+                np.int32,
+                np.int64,
+                np.uint8,
+                np.uint16,
+                np.uint32,
+                np.uint64,
+            ),
+        ):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
