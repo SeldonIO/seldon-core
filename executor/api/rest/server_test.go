0e4750f92924acd9fce4a04aff8bb0338d1cc123
Alejandro Saucedo <axsauze@gmail.com>
2020-03-17 18:07:40 +0000

    Added test for request

--- executor/api/rest/server_test.go
+++ executor/api/rest/server_test.go
@@ -66,7 +66,7 @@ func TestSimpleModel(t *testing.T) {
 	g.Expect(res.Code).To(Equal(200))
 }
 
-func TestHeaderResponsePuuid(t *testing.T) {
+func TestRequestPuuidIsSet(t *testing.T) {
 	t.Logf("Started")
 	g := NewGomegaWithT(t)
 
@@ -96,6 +96,53 @@ func TestHeaderResponsePuuid(t *testing.T) {
 	// Check that the SeldonPUUIDHeader is set
 	g.Expect(len(res.Header().Get(payload.SeldonPUIDHeader))).ShouldNot(BeZero())
 	g.Expect(len(res.Header().Get(payload.SeldonPUIDHeader))).To(Equal(len(guuid.New().String())))
+}
+
+func TestRequestPuuidIsSet(t *testing.T) {
+	t.Logf("Started")
+	g := NewGomegaWithT(t)
+	called := false
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		g.Expect(err).To(BeNil())
+		g.Expect(len(r.Header.Get(payload.SeldonPUIDHeader))).To(Equal(len(guuid.New().String())))
+		called = true
+		w.Write([]byte(bodyBytes))
+	})
+	server := httptest.NewServer(handler)
+	defer server.Close()
+	url, err := url.Parse(server.URL)
+	g.Expect(err).Should(BeNil())
+	urlParts := strings.Split(url.Host, ":")
+	port, err := strconv.Atoi(urlParts[1])
+	g.Expect(err).Should(BeNil())
+
+	model := v1.MODEL
+	p := v1.PredictorSpec{
+		Name: "p",
+		Graph: &v1.PredictiveUnit{
+			Type: &model,
+			Endpoint: &v1.Endpoint{
+				ServiceHost: urlParts[0],
+				ServicePort: int32(port),
+				Type:        v1.REST,
+			},
+		},
+	}
+
+	client, err := NewJSONRestClient(api.ProtocolSeldon, "dep", &p, nil)
+	g.Expect(err).To(BeNil())
+	r := NewServerRestApi(&p, client, false, url, "default", api.ProtocolSeldon, "test", "/metrics")
+	r.Initialise()
+	var data = ` {"data":{"ndarray":[1.1,2.0]}}`
+
+	req, _ := http.NewRequest("POST", "/api/v0.1/predictions", strings.NewReader(data))
+	req.Header = map[string][]string{"Content-Type": []string{"application/json"}}
+	res := httptest.NewRecorder()
+	r.Router.ServeHTTP(res, req)
+	g.Expect(res.Code).To(Equal(200))
+	g.Expect(called).To(Equal(true))
 }
 
 func TestModelWithServer(t *testing.T) {
