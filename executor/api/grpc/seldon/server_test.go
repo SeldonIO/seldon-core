7c3c3b934a36e2e7138c447ee957becd37cb511f
cliveseldon <cc@seldon.io>
2020-03-23 07:45:30 +0000

    Change test client creation

--- executor/api/grpc/seldon/server_test.go
+++ executor/api/grpc/seldon/server_test.go
@@ -28,7 +28,7 @@ func TestPredict(t *testing.T) {
 		},
 	}
 	url, _ := url.Parse("http://localhost")
-	server := NewGrpcSeldonServer(&p, test.NewSeldonMessageTestClient(t, 0, nil, nil), url, "default")
+	server := NewGrpcSeldonServer(&p, test.SeldonMessageTestClient{}, url, "default")
 
 	var sm proto.SeldonMessage
 	var data = ` {"data":{"ndarray":[[1.1,2.0]]}}`
@@ -58,7 +58,7 @@ func TestFeedback(t *testing.T) {
 		},
 	}
 	url, _ := url.Parse("http://localhost")
-	server := NewGrpcSeldonServer(&p, test.NewSeldonMessageTestClient(t, 0, nil, nil), url, "default")
+	server := NewGrpcSeldonServer(&p, &test.SeldonMessageTestClient{}, url, "default")
 
 	var sm proto.Feedback
 	var data = ` {"request":{"data":{"ndarray":[[1.1,2.0]]}}}`
