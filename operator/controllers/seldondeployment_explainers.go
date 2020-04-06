d2123cd80acc26761e6d33f4968298d01f9613fb
Alejandro Saucedo <axsauze@gmail.com>
2020-04-06 20:18:37 +0100

    Updated operator to have aligned requirements on path

--- operator/controllers/seldondeployment_explainers.go
+++ operator/controllers/seldondeployment_explainers.go
@@ -224,7 +224,7 @@ func createExplainerIstioResources(pSvcName string, p *machinelearningv1.Predict
 				{
 					Match: []*istio_networking.HTTPMatchRequest{
 						{
-							Uri: &istio_networking.StringMatch{MatchType: &istio_networking.StringMatch_Prefix{Prefix: "/seldon/" + namespace + "/" + mlDep.Name + "/" + p.Name + constants.ExplainerPathSuffix + "/"}},
+							Uri: &istio_networking.StringMatch{MatchType: &istio_networking.StringMatch_Prefix{Prefix: "/seldon/" + namespace + "/" + mlDep.Name + constants.ExplainerPathSuffix + "/" + p.Name + "/"}},
 						},
 					},
 					Rewrite: &istio_networking.HTTPRewrite{Uri: "/"},
