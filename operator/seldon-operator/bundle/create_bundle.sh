a9816da985516b109cc1fabe237eca5aa6a74de6
Clive Cox <cc@seldon.io>
2020-04-23 16:32:08 +0100

    update mock classifier certified image

--- operator/seldon-operator/bundle/create_bundle.sh
+++ operator/seldon-operator/bundle/create_bundle.sh
@@ -7,6 +7,7 @@ STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 OPERATOR_IMAGE=registry.connect.redhat.com/seldonio/seldon-core-operator
 EXECUTOR_IMAGE=registry.connect.redhat.com/seldonio/seldon-core-executor
 ENGINE_IMAGE=registry.connect.redhat.com/seldonio/seldon-engine
+MOCK_IMAGE=registry.connect.redhat.com/seldonio/mock-classifier
 
 mkdir -p certified
 
@@ -21,7 +22,8 @@ function update_images {
     sed -i 's#\(^.*\)\(- name: RELATED_IMAGE_EXECUTOR$\)#\1\2\n\1  value: '${EXECUTOR_IMAGE}:${VERSION}'#' certified/${VERSION}/seldon-operator.v${VERSION}.clusterserviceversion.yaml
     sed -i 's#\(^.*\)\(- name: RELATED_IMAGE_ENGINE$\)#\1\2\n\1  value: '${ENGINE_IMAGE}:${VERSION}'#' certified/${VERSION}/seldon-operator.v${VERSION}.clusterserviceversion.yaml
     sed -i 's#\(^.*image: \)seldonio/seldon-core-operator:.*$#\1'${OPERATOR_IMAGE}:${VERSION}'#' certified/${VERSION}/seldon-operator.v${VERSION}.clusterserviceversion.yaml
-    sed -i 's#\(^.*containerImage: \)seldonio/seldon-core-operator:.*$#\1'${OPERATOR_IMAGE}:${VERSION}'#' certified/${VERSION}/seldon-operator.v${VERSION}.clusterserviceversion.yaml    
+    sed -i 's#\(^.*containerImage: \)seldonio/seldon-core-operator:.*$#\1'${OPERATOR_IMAGE}:${VERSION}'#' certified/${VERSION}/seldon-operator.v${VERSION}.clusterserviceversion.yaml
+    sed -i 's#seldonio/mock_classifier_rest:1.3#'${MOCK_IMAGE}:${VERSION}'#' certified/${VERSION}/seldon-operator.v${VERSION}.clusterserviceversion.yaml    
 }
 
 function update_package {
