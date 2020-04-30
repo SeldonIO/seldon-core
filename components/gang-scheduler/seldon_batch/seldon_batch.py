115a67b3edc92b383e43750ec72edcc5f5e909ea
Alejandro Saucedo <axsauze@gmail.com>
2020-04-30 10:36:40 +0100

    Added changes to gang scheduler

--- components/gang-scheduler/seldon_batch/seldon_batch.py
+++ components/gang-scheduler/seldon_batch/seldon_batch.py
@@ -1,48 +1,9 @@
-# import json
-# import numpy as np
-# from seldon_core.seldon_client import SeldonClient
-
-
-# def start_batch_processing_loop(
-#    deployment_name,
-#    gateway_type,
-#    namespace,
-#    endpoint,
-#    transport,
-#    payload_type,
-#    parallelism,
-#    retries,
-#    input_data_path,
-#    output_data_path,
-#    method,
-#    log_level,
-# ):
-#    sc = SeldonClient(
-#        deployment_name=deployment_name,
-#        gateway=gateway_type,
-#        namespace=namespace,
-#        gateway_endpoint=endpoint,
-#        transport=transport,
-#        payload_type="ndarray",
-#    )
-#    input_data_file = open(input_data_path, "r")
-#    output_data_file = open(output_data_path, "w")
-#    # TODO: introduce parallelim with Queue
-#    for line in input_data_file:
-#        raw_data = json.loads(line)
-#        data = np.array(raw_data)
-#        output = sc.predict(data=data)
-#        # TODO: Add identifier to track back to input
-#        # TODO: HAndler errors
-#        output_data_file.write(f"{output.response}\n")
-
 from threading import Thread, Lock
 from queue import Queue
 import requests
 import os
 import json
-from geventhttpclient import HTTPClient
-from geventhttpclient.url import URL
+from seldon_core.seldon_client import SeldonClient
 
 
 def start_batch_processing_loop(
@@ -60,40 +21,27 @@ def start_batch_processing_loop(
     log_level,
 ):
 
-    url = URL(
-        f"http://{endpoint}/seldon/{namespace}/{deployment_name}/api/v1.0/{method}"
-    )
-    http = HTTPClient.from_url(url, concurrency=parallelism)
+    url = f"http://{endpoint}/seldon/{namespace}/{deployment_name}/api/v1.0/{method}"
     q_in = Queue(parallelism * 2)
     q_out = Queue(parallelism * 2)
-    # lock = Lock()
-    # file_cursor = os.path.getsize(input_file_path)
 
     def _start_request_worker():
-        while True:
-            line = q_in.get()
-            response = http.post(
-                url.request_uri, body=line, headers={"Content-Type": "application/json"}
-            )
-            response_bytes = response.read()
+        with requests.Session() as session:
+            while True:
+                line = q_in.get()
+                headers = {"Content-Type": "application/json"}
+                # TODO: Use Seldon Client instead after optimising it
+                response = session.post(url, body=line, headers=headers)
 
-            q_out.put(response_bytes.decode())
-            q_in.task_done()
-            ## We add a lock to ensure there is no race condition on setting end of file cursor
-            # with lock:
-            #    # Ensure cursor moves across the file based on length of line
-            #    file_cursor -= len(line)
-            #    q_out.put((response.text, bool(file_cursor)))
+                q_out.put(response.text)
+                q_in.task_done()
 
     def _start_file_worker():
         output_data_file = open(output_data_path, "w")
         while True:
             line = q_out.get()
-            output_data_file.write(f"{line}\n")
+            output_data_file.write(f"{line}")
             q_out.task_done()
-            ## If we've reached the last line then we break
-            # if not file_cursor:
-            #    break
 
     for _ in range(parallelism):
         t = Thread(target=_start_request_worker)
