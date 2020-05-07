fcb75769b776c30cce8c1ac972d4197d210e4e92
glindsell <gl@seldon.io>
2020-05-07 10:58:38 +0100

    Use kubectl version v1.17.0 in test scripts
    Signed-off-by: glindsell <gl@seldon.io>

---
+++ testing/scripts/kubectl_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# kubectl install
+#
+# kubectl Release info:
+# version: v1.17.0
+#
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+set -o noglob
+#set -o xtrace
+
+FILE=tempresources/kubectl
+
+# If it doesn't already exist, install go-swagger binary into tempresources directory
+[ ! -f "$FILE" ] && \
+	mkdir -p tempresources && \
+	curl -o tempresources/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl && \
+	chmod +x tempresources/kubectl
+exit 0
