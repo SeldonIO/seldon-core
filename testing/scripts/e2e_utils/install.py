6f9e01fa9ea783a1e28d7b6f2625e8758e3a8f4a
Adrian Gonzalez-Martin <agm@seldon.io>
2020-05-22 17:03:43 +0100

    Change logic to engine=True / False

--- testing/scripts/e2e_utils/install.py
+++ testing/scripts/e2e_utils/install.py
@@ -13,7 +13,7 @@ SC_NAME = "seldon"
 SC_NAMESPACE = "seldon-system"
 
 
-def install_seldon(name=SC_NAME, namespace=SC_NAMESPACE, executor=True, version=None):
+def install_seldon(name=SC_NAME, namespace=SC_NAMESPACE, engine=False, version=None):
     chart_path = "seldonio/seldon-core-operator"
     if version is None:
         # Use local
@@ -25,7 +25,7 @@ def install_seldon(name=SC_NAME, namespace=SC_NAMESPACE, executor=True, version=
         "certManager.enabled": "false",
     }
 
-    if not executor:
+    if engine:
         values["executor.enabled"] = "false"
 
     helm.install(
