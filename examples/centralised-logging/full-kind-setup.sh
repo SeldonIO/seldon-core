56cf48991759f75ae12ab0d51223c3210dbf7e2b
ryandawsonuk <rd@seldon.io>
2020-05-22 19:09:13 +0100

    make kind setup clearer

--- examples/centralised-logging/full-kind-setup.sh
+++ examples/centralised-logging/full-kind-setup.sh
@@ -1,16 +1,16 @@
- 
-kind delete cluster
+
+#KIND SETUP
+kind delete cluster || true
 #had a problem with 1.15.6 image https://github.com/SeldonIO/seldon-core/pull/1861#issuecomment-632587125
 kind create cluster --config kind_config.yaml --image kindest/node:v1.14.2
 
-kubectl create namespace seldon-system
-
-helm install seldon-core ../../helm-charts/seldon-core-operator/ --namespace seldon-system
-
+#ISTIO
 ./install_istio.sh
+
+#KNATIVE
 ./install_knative.sh
 
-#remove heavier knative monitoring components
+#remove heavier knative monitoring components as this is kind
 kubectl delete statefulset/elasticsearch-logging -n knative-monitoring
 kubectl delete deployment/grafana -n knative-monitoring
 kubectl delete deployment/kibana-logging -n knative-monitoring
@@ -20,24 +20,40 @@ sleep 20
 kubectl rollout status -n knative-eventing deployments/imc-controller
 kubectl rollout status -n knative-eventing deployments/imc-dispatcher
 
-kubectl label namespace default knative-eventing-injection=enabled
+#REQUEST LOGGER
+kubectl create namespace seldon-logs
+kubectl label namespace seldon-logs knative-eventing-injection=enabled
 sleep 3
-kubectl -n default get broker
+kubectl -n seldon-logs get broker
+kubectl apply -f ./trigger.yaml
+
+kubectl apply -f seldon-request-logger.yaml
+
+#EFK
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+helm install elasticsearch elasticsearch --version 7.6.0 --namespace=seldon-logs -f elastic-kind.yaml --repo https://helm.elastic.co --set image=docker.elastic.co/elasticsearch/elasticsearch-oss
+
+helm install fluentd fluentd-elasticsearch --namespace=seldon-logs -f fluentd-values.yaml --repo https://kiwigrid.github.io
+
+helm install kibana kibana --version 7.6.0 --namespace=seldon-logs --set service.type=NodePort --repo https://helm.elastic.co --set image=docker.elastic.co/kibana/kibana-oss
+
+kubectl rollout status -n seldon-logs statefulset/elasticsearch-master
+
+
+#SELDON CORE
+kubectl create namespace seldon-system
+
+helm install seldon-core ../../helm-charts/seldon-core-operator/ --namespace seldon-system
 
 kubectl rollout status -n seldon-system deployment/seldon-controller-manager
 
-helm install seldon-single-model ../../helm-charts/seldon-single-model/ --set model.logger.enabled=true
+#seldon needs short sleep otherwise get webhook failure on installing first model
+sleep 5
 
-kubectl create namespace logs
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
-helm install elasticsearch elasticsearch --version 7.6.0 --namespace=logs -f elastic-kind.yaml --repo https://helm.elastic.co --set image=docker.elastic.co/elasticsearch/elasticsearch-oss
+#EXAMPLE MODEL
+helm install seldon-single-model ../../helm-charts/seldon-single-model/ --set model.logger.enabled=true --set model.logger.url="http://default-broker.seldon-logs"
 
-helm install fluentd fluentd-elasticsearch --namespace=logs -f fluentd-values.yaml --repo https://kiwigrid.github.io
-
-helm install kibana kibana --version 7.6.0 --namespace=logs --set service.type=NodePort --repo https://helm.elastic.co --set image=docker.elastic.co/kibana/kibana-oss
-
-kubectl rollout status -n logs statefulset/elasticsearch-master
-
+#LOADTESTER
 kubectl label nodes kind-worker role=locust --overwrite
 kubectl label nodes $(kubectl get nodes -o jsonpath='{.items[0].metadata.name}') role=locust --overwrite
 
@@ -49,12 +65,6 @@ kubectl label nodes $(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')
 helm install seldon-core-loadtesting ../../helm-charts/seldon-core-loadtesting/ --set locust.host=http://seldon-single-model-default:8000 --set oauth.enabled=false --set oauth.key=oauth-key --set oauth.secret=oauth-secret --set locust.hatchRate=1 --set locust.clients=1 --set loadtest.sendFeedback=0 --set locust.minWait=1000 --set locust.maxWait=1000 --set replicaCount=1
 
 
-#do this at end as otherwise sometimes gets stuck in terminating and then have to reinstall these
-kubectl apply -f ./trigger.yaml
-
-kubectl apply -f seldon-request-logger.yaml
-
-
-
+#LAUNCH KIBANA UI
 xdg-open localhost:5601
-kubectl port-forward svc/kibana-kibana -n logs 5601:5601
+kubectl port-forward svc/kibana-kibana -n seldon-logs 5601:5601
