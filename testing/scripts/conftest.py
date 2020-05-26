22086f5b29767f8f26b093ee6a953e0d4b170102
Adrian Gonzalez-Martin <agm@seldon.io>
2020-05-26 16:14:40 +0100

    Change env var

--- testing/scripts/conftest.py
+++ testing/scripts/conftest.py
@@ -5,7 +5,8 @@ from seldon_e2e_utils import clean_string, retry_run, get_seldon_version
 from e2e_utils.install import install_seldon, delete_seldon
 from subprocess import run
 
-SELDON_E2E_TESTS_USE_ENGINE = os.getenv("SELDON_E2E_TESTS_USE_ENGINE") == "true"
+# By default, SELDON_E2E_TESTS_USE_EXECUTOR should be True
+SELDON_E2E_TESTS_USE_EXECUTOR = os.getenv("SELDON_E2E_TESTS_USE_EXECUTOR") != "false"
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -68,13 +69,13 @@ def seldon_version(request):
 
     # Delete source version cluster-wide and install new one
     delete_seldon()
-    install_seldon(version=seldon_version, engine=SELDON_E2E_TESTS_USE_ENGINE)
+    install_seldon(version=seldon_version, executor=SELDON_E2E_TESTS_USE_EXECUTOR)
 
     yield seldon_version
 
     # Re-install source code version cluster-wide
     delete_seldon()
-    install_seldon(engine=SELDON_E2E_TESTS_USE_ENGINE)
+    install_seldon(executor=SELDON_E2E_TESTS_USE_EXECUTOR)
 
 
 def do_s2i_python_version():
