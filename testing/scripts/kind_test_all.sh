0ac81d885fc5a3525a9b71e20280e7f8886a9153
Adrian Gonzalez-Martin <agm@seldon.io>
2020-06-01 17:02:23 +0100

    Only run parallel tests for engine

--- testing/scripts/kind_test_all.sh
+++ testing/scripts/kind_test_all.sh
@@ -142,6 +142,8 @@ if [[ ${KIND_EXIT_VALUE} -eq 0 ]]; then
             make test_notebooks
         elif [ "$TESTS_TO_RUN" == "base" ]; then
             make test_parallel test_sequential
+        elif [ "$TESTS_TO_RUN" == "parallel" ]; then
+            make test_parallel
         fi
         TEST_EXIT_VALUE=$?
         if [[ $TEST_EXIT_VALUE -gt 0 ]]; then
