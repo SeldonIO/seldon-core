e6e200bb73e91f919516afe31eacb714252206ab
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2020-06-25 13:47:49 +0100

    include grpc test in notebook tests

--- testing/scripts/test_notebooks.py
+++ testing/scripts/test_notebooks.py
@@ -106,6 +106,9 @@ class TestNotebooks(object):
     def test_graph_metadata(self):
         create_and_run_script("../../examples/models/metadata", "graph_metadata")
 
+    def test_grpc_metadata(self):
+        create_and_run_script("../../examples/models/metadata", "metadata_grpc")
+
     def test_payload_logging(self):
         create_and_run_script(
             "../../examples/models/payload_logging", "payload_logging"
