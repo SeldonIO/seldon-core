83e9bdd300b0107963334d220ab7c3e18b082026
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2020-06-26 09:56:27 +0100

    add note why ModelMetadata is in payload package

--- executor/api/payload/model_metadata.go
+++ executor/api/payload/model_metadata.go
@@ -1,5 +1,10 @@
 package payload
 
+// Struct to store ModelMetadata deserialised from other payloads format (REST/GRPC).
+// We need to deserialise this payload in order to build GraphMetadata.
+// As deserialisation is REST/GRPC depenendent it must happens in corresponding client
+// and ModelMetadata is returned by a corresponding method. It is therefore defined
+// here and not in `predictor_process/metadata.go` in order to avoid circular dependency.
 type ModelMetadata struct {
 	Name     string      `json:"name,omitempty"`
 	Platform string      `json:"platform,omitempty"`
