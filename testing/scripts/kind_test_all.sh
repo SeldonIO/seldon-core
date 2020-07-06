ba81f9ee0744a614bb9122ce627ff73593de788e
Alejandro Saucedo <axsauze@gmail.com>
2020-07-06 21:22:33 +0100

    Added sleep

--- testing/scripts/kind_test_all.sh
+++ testing/scripts/kind_test_all.sh
@@ -15,54 +15,56 @@ echo "Test run type is: [$TESTS_TO_RUN]"
 # This set up follows the best practices outlined by d2iq in their setup
 # More at https://d2iq.com/blog/running-kind-inside-a-kubernetes-cluster-for-continuous-integration
 
-# Determine cgroup parent for docker daemon.
-# We need to make sure cgroups created by the docker daemon do not
-# interfere with other cgroups on the host, and do not leak after this
-# container is terminated.
-if [ -f /sys/fs/cgroup/systemd/release_agent ]; then
-  # This means the user has bind mounted host /sys/fs/cgroup to the
-  # same location in the container (e.g., using the following docker
-  # run flags: `-v /sys/fs/cgroup:/sys/fs/cgroup`). In this case, we
-  # need to make sure the docker daemon in the container does not
-  # pollute the host cgroups hierarchy.
-  # Note that `release_agent` file is only created at the root of a
-  # cgroup hierarchy.
-  CGROUP_PARENT="$(grep systemd /proc/self/cgroup | cut -d: -f3)/docker"
-else
-  CGROUP_PARENT="/docker"
+## Determine cgroup parent for docker daemon.
+## We need to make sure cgroups created by the docker daemon do not
+## interfere with other cgroups on the host, and do not leak after this
+## container is terminated.
+#if [ -f /sys/fs/cgroup/systemd/release_agent ]; then
+#  # This means the user has bind mounted host /sys/fs/cgroup to the
+#  # same location in the container (e.g., using the following docker
+#  # run flags: `-v /sys/fs/cgroup:/sys/fs/cgroup`). In this case, we
+#  # need to make sure the docker daemon in the container does not
+#  # pollute the host cgroups hierarchy.
+#  # Note that `release_agent` file is only created at the root of a
+#  # cgroup hierarchy.
+#  CGROUP_PARENT="$(grep systemd /proc/self/cgroup | cut -d: -f3)/docker"
+#else
+#  CGROUP_PARENT="/docker"
+#
+#  # For each cgroup subsystem, Docker does a bind mount from the
+#  # current cgroup to the root of the cgroup subsystem. For instance:
+#  #   /sys/fs/cgroup/memory/docker/<cid> -> /sys/fs/cgroup/memory
+#  #
+#  # This will confuse some system software that manipulate cgroups
+#  # (e.g., kubelet/cadvisor, etc.) sometimes because
+#  # `/proc/<pid>/cgroup` is not affected by the bind mount. The
+#  # following is a workaround to recreate the original cgroup
+#  # environment by doing another bind mount for each subsystem.
+#  CURRENT_CGROUP=$(grep systemd /proc/self/cgroup | cut -d: -f3)
+#  CGROUP_SUBSYSTEMS=$(findmnt -lun -o source,target -t cgroup | grep "${CURRENT_CGROUP}" | awk '{print $2}')
+#
+#  echo "${CGROUP_SUBSYSTEMS}" |
+#  while IFS= read -r SUBSYSTEM; do
+#    mkdir -p "${SUBSYSTEM}${CURRENT_CGROUP}"
+#    mount --bind "${SUBSYSTEM}" "${SUBSYSTEM}${CURRENT_CGROUP}"
+#  done
+#fi
+#
+#setsid dockerd \
+#  --cgroup-parent="${CGROUP_PARENT}" \
+#  --bip="${DOCKERD_BIP:-172.17.1.1/24}" \
+#  --mtu="${DOCKERD_MTU:-1400}" \
+#  --raw-logs \
+#  ${DOCKER_ARGS:-} &
+#
+## Wait until dockerd is ready.
+#until docker ps >/dev/null 2>&1
+#do
+#  echo "Waiting for dockerd..."
+#  sleep 1
+#done
 
-  # For each cgroup subsystem, Docker does a bind mount from the
-  # current cgroup to the root of the cgroup subsystem. For instance:
-  #   /sys/fs/cgroup/memory/docker/<cid> -> /sys/fs/cgroup/memory
-  #
-  # This will confuse some system software that manipulate cgroups
-  # (e.g., kubelet/cadvisor, etc.) sometimes because
-  # `/proc/<pid>/cgroup` is not affected by the bind mount. The
-  # following is a workaround to recreate the original cgroup
-  # environment by doing another bind mount for each subsystem.
-  CURRENT_CGROUP=$(grep systemd /proc/self/cgroup | cut -d: -f3)
-  CGROUP_SUBSYSTEMS=$(findmnt -lun -o source,target -t cgroup | grep "${CURRENT_CGROUP}" | awk '{print $2}')
-
-  echo "${CGROUP_SUBSYSTEMS}" |
-  while IFS= read -r SUBSYSTEM; do
-    mkdir -p "${SUBSYSTEM}${CURRENT_CGROUP}"
-    mount --bind "${SUBSYSTEM}" "${SUBSYSTEM}${CURRENT_CGROUP}"
-  done
-fi
-
-setsid dockerd \
-  --cgroup-parent="${CGROUP_PARENT}" \
-  --bip="${DOCKERD_BIP:-172.17.1.1/24}" \
-  --mtu="${DOCKERD_MTU:-1400}" \
-  --raw-logs \
-  ${DOCKER_ARGS:-} &
-
-# Wait until dockerd is ready.
-until docker ps >/dev/null 2>&1
-do
-  echo "Waiting for dockerd..."
-  sleep 1
-done
+sleep 3600 # Hold to debug
 
 #######################################
 # AVOID EXIT ON ERROR FOR FOLLOWING CMDS
