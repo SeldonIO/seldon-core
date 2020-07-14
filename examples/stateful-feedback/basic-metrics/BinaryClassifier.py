ca2ba2a3bb60a98873a7f31df46f04fdfad7c4d5
Alejandro Saucedo <axsauze@gmail.com>
2020-07-14 06:20:40 +0100

    Updated example to have binary classifier

---
+++ examples/stateful-feedback/basic-metrics/BinaryClassifier.py
@@ -0,0 +1,40 @@
+import joblib
+
+
+class Score:
+    def __init__(self, TP, FP, TN, FN):
+        self.TP = TP
+        self.FP = FP
+        self.TN = TN
+        self.FN = FN
+
+
+class BinaryClassifier:
+    def __init__(self, model_name="binary-lr.joblib"):
+        self.scores = Score(0, 0, 0, 0)
+        self.model = joblib.load(model_name)
+
+    def predict(self, X, features_names=None, meta=None):
+        return self.model.predict(X)
+
+    def send_feedback(self, features, feature_names, reward, truth, routing=""):
+        predicted = self.predict(features)
+        if int(truth[0]) == 1:
+            if int(predicted[0]) == int(truth[0]):
+                self.scores.TP += 1
+            else:
+                self.scores.FN += 1
+        else:
+            if int(predicted[0]) == int(truth[0]):
+                self.scores.TN += 1
+            else:
+                self.scores.TP += 1
+        return []  # Ignore return statement as its not used
+
+    def metrics(self):
+        return [
+            {"type": "GAUGE", "key": "true_pos", "value": self.scores.TP},
+            {"type": "GAUGE", "key": "true_neg", "value": self.scores.FN},
+            {"type": "GAUGE", "key": "false_pos", "value": self.scores.TN},
+            {"type": "GAUGE", "key": "false_neg", "value": self.scores.FP},
+        ]
