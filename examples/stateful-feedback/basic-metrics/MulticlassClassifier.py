e783e448cdef7f53cdbf84518594becb67a0a5d1
Alejandro Saucedo <axsauze@gmail.com>
2020-07-14 09:25:13 +0100

    updated example to include multiclass

--- examples/stateful-feedback/basic-metrics/MulticlassClassifier.py
+++ examples/stateful-feedback/basic-metrics/MulticlassClassifier.py
@@ -1,9 +1,18 @@
 import joblib
 
 
+class Scores:
+    def __init__(self, numclass):
+        self.TP = [0] * numclass
+        self.FP = [0] * numclass
+        self.FN = [0] * numclass
+        # self.TN = [0] * numclass # Most often there won't be true negatives in multiclass although w
+        # We could explore using reward (or a binary param) to define if expected TN, but that seems more custom
+
+
 class MulticlassClassifier:
     def __init__(self, model_name="multiclass-lr.joblib"):
-        self.scores = {"correct": 0, "incorrect": 0}
+        self.scores = Scores(3)
         self.model = joblib.load(model_name)
 
     def predict(self, X, features_names=None, meta=None):
@@ -11,14 +20,25 @@ class MulticlassClassifier:
 
     def send_feedback(self, features, feature_names, reward, truth, routing=""):
         predicted = self.predict(features)
+        print(f"Predicted: {predicted}")
+        print(f"Truth: {truth}")
         if int(predicted[0]) == int(truth[0]):
-            self.scores["correct"] += 1
+            self.scores.TP[int(truth[0])] += 1
         else:
-            self.scores["incorrect"] += 1
+            self.scores.FN[int(truth[0])] += 1
+            self.scores.FP[int(predicted[0])] += 1
         return []  # Ignore return statement as its not used
 
     def metrics(self):
-        return [
-            {"type": "GAUGE", "key": "correct", "value": self.scores["correct"]},
-            {"type": "GAUGE", "key": "incorrect", "value": self.scores["incorrect"]},
-        ]
+        metrics = []
+        for score, arr in vars(self.scores).items():
+            for i, val in enumerate(arr):
+                metric = {
+                    "type": "GAUGE",
+                    "key": f"score_{score}",
+                    "value": val,
+                    "tags": {"class": f"class_{i}"},
+                }
+                print(metric)
+                metrics.append(metric)
+        return metrics
