977adc7bfb1fb1aca5c12669a3bfbd3e6d2ee4dc
Adrian Gonzalez-Martin <agm@seldon.io>
2020-08-25 16:12:07 +0200

    Use String insted of byte[] for REST

--- incubating/wrappers/java/src/main/java/io/seldon/wrapper/api/SeldonPredictionService.java
+++ incubating/wrappers/java/src/main/java/io/seldon/wrapper/api/SeldonPredictionService.java
@@ -1,17 +1,16 @@
 package io.seldon.wrapper.api;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import io.seldon.protos.PredictionProtos.Feedback;
 import io.seldon.protos.PredictionProtos.SeldonMessage;
 import io.seldon.protos.PredictionProtos.SeldonMessageList;
 import io.seldon.wrapper.pb.ProtoBufUtils;
-import java.nio.charset.StandardCharsets;
 
 public interface SeldonPredictionService {
-  public default byte[] predictRawREST(byte[] request) {
+  public default String predictRawREST(String request) throws InvalidProtocolBufferException {
     SeldonMessage.Builder builder = SeldonMessage.newBuilder();
-    String payload = new String(request, StandardCharsets.UTF_8);
 
-    ProtoBufUtils.updateMessageBuilderFromJson(builder, payload);
+    ProtoBufUtils.updateMessageBuilderFromJson(builder, request);
     SeldonMessage input = builder.build();
 
     SeldonMessage prediction = this.predict(input);
@@ -20,7 +19,7 @@ public interface SeldonPredictionService {
     return rawPrediction;
   }
 
-  public default byte[] predictRawGRPC(byte[] request) {
+  public default byte[] predictRawGRPC(byte[] request) throws InvalidProtocolBufferException {
     SeldonMessage input = SeldonMessage.parseFrom(request);
 
     SeldonMessage prediction = this.predict(input);
