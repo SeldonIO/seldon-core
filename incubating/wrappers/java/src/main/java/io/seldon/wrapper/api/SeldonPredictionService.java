8ffbf86b15a66070c97be3d0382e52cc22cb72d9
Adrian Gonzalez-Martin <agm@seldon.io>
2020-08-25 16:02:12 +0200

    Add raw methods to Java wrapper

--- incubating/wrappers/java/src/main/java/io/seldon/wrapper/api/SeldonPredictionService.java
+++ incubating/wrappers/java/src/main/java/io/seldon/wrapper/api/SeldonPredictionService.java
@@ -3,8 +3,32 @@ package io.seldon.wrapper.api;
 import io.seldon.protos.PredictionProtos.Feedback;
 import io.seldon.protos.PredictionProtos.SeldonMessage;
 import io.seldon.protos.PredictionProtos.SeldonMessageList;
+import io.seldon.wrapper.pb.ProtoBufUtils;
+import java.nio.charset.StandardCharsets;
 
 public interface SeldonPredictionService {
+  public default byte[] predictRawREST(byte[] request) {
+    SeldonMessage.Builder builder = SeldonMessage.newBuilder();
+    String payload = new String(request, StandardCharsets.UTF_8);
+
+    ProtoBufUtils.updateMessageBuilderFromJson(builder, payload);
+    SeldonMessage input = builder.build();
+
+    SeldonMessage prediction = this.predict(input);
+
+    String rawPrediction = ProtoBufUtils.toJson(prediction, true);
+    return rawPrediction;
+  }
+
+  public default byte[] predictRawGRPC(byte[] request) {
+    SeldonMessage input = SeldonMessage.parseFrom(request);
+
+    SeldonMessage prediction = this.predict(input);
+
+    byte[] rawPrediction = prediction.toByteArray();
+    return rawPrediction;
+  }
+
   public default SeldonMessage predict(SeldonMessage request) {
     return SeldonMessage.newBuilder().build();
   }
