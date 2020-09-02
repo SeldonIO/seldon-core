bff1c32e141635bbe129fd92891f14c0adc4f1a9
Alejandro Saucedo <axsauze@gmail.com>
2020-09-02 17:04:42 +0100

    Updated kubeflow example to kf 1.0.0

--- examples/kubeflow/pipeline/pipeline_steps/clean_text/Transformer.py
+++ examples/kubeflow/pipeline/pipeline_steps/clean_text/Transformer.py
@@ -1,14 +1,13 @@
-import re 
+import re
 from html.parser import HTMLParser
 import numpy as np
 import logging
 
-class Transformer():
+
+class Transformer:
     __html_parser = HTMLParser()
-    __uplus_pattern = \
-        re.compile("\<[uU]\+(?P<digit>[a-zA-Z0-9]+)\>")
-    __markup_link_pattern = \
-        re.compile("\[(.*)\]\((.*)\)")
+    __uplus_pattern = re.compile("\<[uU]\+(?P<digit>[a-zA-Z0-9]+)\>")
+    __markup_link_pattern = re.compile("\[(.*)\]\((.*)\)")
 
     def predict(self, X, feature_names=[]):
         logging.warning(X)
@@ -19,25 +18,23 @@ class Transformer():
 
     def fit(self, X, y=None, **fit_params):
         return self
-    
+
     @staticmethod
     def transform_clean_text(raw_text):
         try:
             decoded = raw_text.encode("ISO-8859-1").decode("utf-8")
         except:
             decoded = raw_text.encode("ISO-8859-1").decode("cp1252")
-        html_unescaped =Transformer.\
-            __html_parser.unescape(decoded) 
+        html_unescaped = Transformer.__html_parser.unescape(decoded)
         html_unescaped = re.sub(r"\r\n", " ", html_unescaped)
         html_unescaped = re.sub(r"\r\r\n", " ", html_unescaped)
         html_unescaped = re.sub(r"\r", " ", html_unescaped)
         html_unescaped = html_unescaped.replace("&gt;", " > ")
         html_unescaped = html_unescaped.replace("&lt;", " < ")
         html_unescaped = html_unescaped.replace("--", " - ")
-        html_unescaped = Transformer.__uplus_pattern.sub(
-            " U\g<digit> ", html_unescaped)
+        html_unescaped = Transformer.__uplus_pattern.sub(" U\g<digit> ", html_unescaped)
         html_unescaped = Transformer.__markup_link_pattern.sub(
-            " \1 \2 ", html_unescaped)
+            " \1 \2 ", html_unescaped
+        )
         html_unescaped = html_unescaped.replace("\\", "")
         return html_unescaped
-
