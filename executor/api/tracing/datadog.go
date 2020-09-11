43eb5c0fc65ac0889354b7cc64a656b8effa933d
Michael Meredith <mikemeredith63@gmail.com>
2020-09-10 22:26:04 -0400

    spruce up docs, enable sampling in python

--- executor/api/tracing/datadog.go
+++ executor/api/tracing/datadog.go
@@ -26,25 +26,11 @@ func (d *datadogTracer) Close() error {
 
 // TODO: docs/comments
 // initDatadogTracer attempts to create a tracer for using DataDog and statsd if the
-// correct environment variables are present. See https://pkg.go.dev/gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer?tab=doc#StartOption
-// for all Environment variables. Here are the relevant ones:
-// DD_AGENT_HOST --> default localhost
-// DD_DOGSTATSD_PORT --> default 8126
-// DD_ENV
-// DD_SERVICE
-// DD_VERSION
-// DD_TAGS --> "k:v,k2:v2"
-// DD_TRACE_ANALYTICS_ENABLED
-// DD_TRACE_REPORT_HOSTNAME
-// DD_TRACE_STARTUP_LOGS
-// DD_RUNTIME_METRICS_ENABLED
-// DD_TRACE_DEBUG
-// TODO:
-// DD_SAMPLING_RATE
-// DD_ENABLED
-//
-// DD_PROPAGATION_STYLE_INJECT=Datadog,B3
-// DD_PROPAGATION_STYLE_EXTRACT=Datadog,B3
+// correct environment variables are present. See https://docs.datadoghq.com/tracing/setup/go/
+// for all Environment variables.
+// Additional ones sepcific to Seldon:
+// DD_SAMPLE_RATE --> 0-1, rate of sampling
+// DD_ENABLED --> 0,1
 func initDatadogTracer() (io.Closer, error) {
 
 	serviceName := os.Getenv("DD_SERVICE")
