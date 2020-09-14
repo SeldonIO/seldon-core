db1569ec6bcc0dc01c9682ee13d26b836f962bb0
Michael Meredith <mikemeredith63@gmail.com>
2020-09-14 12:54:13 -0400

    remove extra files

--- python/ModelMain.py
+++
@@ -1,5 +0,0 @@
-# TODO: delete me
-class ModelMain(object):
-    def predict(self, X, names):
-        print("predict called")
-        return X
--- python/main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import re
-import sys
-from seldon_core.microservice import main
-#TODO: delete me
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
