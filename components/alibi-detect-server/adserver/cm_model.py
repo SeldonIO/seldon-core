a0b25de8e8c8934d0454d5de1ac0479786e9cd0b
Alejandro Saucedo <axsauze@gmail.com>
2020-09-22 19:07:58 +0100

    Updated adserver to host metrics and custom server

---
+++ components/alibi-detect-server/adserver/cm_model.py
@@ -0,0 +1,105 @@
+import json
+from typing import List, Dict, Optional, Union
+import logging
+import numpy as np
+from adserver.constants import HEADER_RETURN_INSTANCE_SCORE
+from .numpy_encoder import NumpyEncoder
+from alibi_detect.utils.saving import load_detector, Data
+from adserver.base import CEModel
+from seldon_core.user_model import SeldonResponse
+from seldon_core.flask_utils import SeldonMicroserviceException
+
+
+class CustomMetricsModel(CEModel):  # pylint:disable=c-extension-no-member
+    def __init__(self, name: str, storage_uri: str, model: Optional[Data] = None):
+        """
+        Outlier Detection / Concept Drift Model
+
+        Parameters
+        ----------
+        name
+             The name of the model
+        storage_uri
+             The URI location of the model
+        """
+        super().__init__(name)
+        self.name = name
+        self.storage_uri = storage_uri
+        self.ready = False
+        self.model: Data = model
+
+    def load(self):
+        """
+        Load the model from storage
+
+        """
+        self.ready = True
+
+    def process_event(self, inputs: Union[List, Dict], headers: Dict) -> Dict:
+        """
+        Process the event and return Alibi Detect score
+
+        Parameters
+        ----------
+        inputs
+             Input data
+        headers
+             Header options
+
+        Returns
+        -------
+             Alibi Detect response
+
+        """
+        logging.info("PROCESSING EVENT.")
+        logging.info(str(headers))
+        logging.info("----")
+
+        metrics = []
+        output = {}
+
+        if "truth" not in inputs:
+            raise SeldonMicroserviceException(
+                    f"No truth value provided in: {json.dumps(inputs)}",
+                    status_code=400,
+                    reason="NO_TRUTH_VALUE")
+
+        # We automatically add any metrics provided in the truth
+        if "meta" in inputs["truth"]:
+            if "metrics" in inputs["truth"]["meta"]:
+                metrics = inputs["truth"]["meta"]["metrics"]
+
+        # If response is provided then we can perform a comparison
+        # TODO: If Header UUID provided we could fetch from ELK to do the evaluation
+        if "response" in inputs:
+            # TODO: Add the extensions for the comparisons here
+            response = inputs["response"]
+            truth = inputs["truth"]
+
+            if self.name == "BINARY_CLASSIFICATION":
+                response = int(response) if isinstance(response, list) else int(response[0])
+                truth = int(truth) if isinstance(truth, list) else int(truth[0])
+                correct = response == truth
+                if truth:
+                    if correct:
+                        key = "true_positive"
+                    else:
+                        key = "false_negative"
+                else:
+                    if correct:
+                        key = "true_negative"
+                    else:
+                        key = "false_positive"
+
+                metrics = [{"key":key, "type": "COUNTER", "value": 1}]
+
+            elif self.name == "MULTICLASS_CLASSIFICATION_ONE_HOT":
+                pass
+            elif self.name == "MULTICLASS_CLASSIFICATION_NUMERIC":
+                pass
+
+        # TODO: Allow for scores to be used to calculate metrics as well
+        seldon_response = SeldonResponse(output or None, None, metrics)
+
+        return seldon_response
+
