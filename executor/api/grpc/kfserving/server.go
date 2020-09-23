a8237d50b54b79cd9d958d7f3b24e45bd5872124
Clive Cox <cc@seldon.io>
2020-09-23 08:57:15 +0100

    Update to not impletemted message

--- executor/api/grpc/kfserving/server.go
+++ executor/api/grpc/kfserving/server.go
@@ -32,11 +32,11 @@ func NewGrpcKFServingServer(predictor *v1.PredictorSpec, client client.SeldonApi
 }
 
 func (g GrpcKFServingServer) ServerLive(ctx context.Context, request *inference.ServerLiveRequest) (*inference.ServerLiveResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) ServerReady(ctx context.Context, request *inference.ServerReadyRequest) (*inference.ServerReadyResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) ModelReady(ctx context.Context, request *inference.ModelReadyRequest) (*inference.ModelReadyResponse, error) {
@@ -52,7 +52,7 @@ func (g GrpcKFServingServer) ModelReady(ctx context.Context, request *inference.
 }
 
 func (g GrpcKFServingServer) ServerMetadata(ctx context.Context, request *inference.ServerMetadataRequest) (*inference.ServerMetadataResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) ModelMetadata(ctx context.Context, request *inference.ModelMetadataRequest) (*inference.ModelMetadataResponse, error) {
@@ -80,49 +80,49 @@ func (g GrpcKFServingServer) ModelInfer(ctx context.Context, request *inference.
 }
 
 func (g GrpcKFServingServer) ModelStreamInfer(server inference.GRPCInferenceService_ModelStreamInferServer) error {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) ModelConfig(ctx context.Context, request *inference.ModelConfigRequest) (*inference.ModelConfigResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) ModelStatistics(ctx context.Context, request *inference.ModelStatisticsRequest) (*inference.ModelStatisticsResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) RepositoryIndex(ctx context.Context, request *inference.RepositoryIndexRequest) (*inference.RepositoryIndexResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) RepositoryModelLoad(ctx context.Context, request *inference.RepositoryModelLoadRequest) (*inference.RepositoryModelLoadResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) RepositoryModelUnload(ctx context.Context, request *inference.RepositoryModelUnloadRequest) (*inference.RepositoryModelUnloadResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) SystemSharedMemoryStatus(ctx context.Context, request *inference.SystemSharedMemoryStatusRequest) (*inference.SystemSharedMemoryStatusResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) SystemSharedMemoryRegister(ctx context.Context, request *inference.SystemSharedMemoryRegisterRequest) (*inference.SystemSharedMemoryRegisterResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) SystemSharedMemoryUnregister(ctx context.Context, request *inference.SystemSharedMemoryUnregisterRequest) (*inference.SystemSharedMemoryUnregisterResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) CudaSharedMemoryStatus(ctx context.Context, request *inference.CudaSharedMemoryStatusRequest) (*inference.CudaSharedMemoryStatusResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) CudaSharedMemoryRegister(ctx context.Context, request *inference.CudaSharedMemoryRegisterRequest) (*inference.CudaSharedMemoryRegisterResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
 
 func (g GrpcKFServingServer) CudaSharedMemoryUnregister(ctx context.Context, request *inference.CudaSharedMemoryUnregisterRequest) (*inference.CudaSharedMemoryUnregisterResponse, error) {
-	panic("implement me")
+	panic("Not Implemented")
 }
