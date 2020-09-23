32afb6d91e93ec372621aca106300a82c6c1d860
Alejandro Saucedo <axsauze@gmail.com>
2020-09-23 21:10:13 +0100

    Added logic for multiclass classification one hot and numeric

--- components/alibi-detect-server/adserver/cm_model.py
+++ components/alibi-detect-server/adserver/cm_model.py
@@ -66,7 +66,7 @@ class CustomMetricsModel(CEModel):  # pylint:disable=c-extension-no-member
 
         # We automatically add any metrics provided in the truth
         if "metrics" in inputs:
-            metrics = inputs["metrics"]
+            metrics.extend(inputs["metrics"])
 
         # If response is provided then we can perform a comparison
         # TODO: If Header UUID provided we could fetch from ELK to do the evaluation
@@ -76,26 +76,50 @@ class CustomMetricsModel(CEModel):  # pylint:disable=c-extension-no-member
             truth = inputs["truth"]
 
             if self.name == "BINARY_CLASSIFICATION":
-                response = int(response) if isinstance(response, list) else int(response[0])
-                truth = int(truth) if isinstance(truth, list) else int(truth[0])
-                correct = response == truth
-                if truth:
+                response_class = int(response) if isinstance(response, list) else int(response[0])
+                truth_class = int(truth) if isinstance(truth, list) else int(truth[0])
+
+                correct = response_class == truth_class
+
+                if truth_class:
                     if correct:
-                        key = "true_positive"
+                        key = "seldon_metric_true_positive"
                     else:
-                        key = "false_negative"
+                        key = "seldon_metric_false_negative"
                 else:
                     if correct:
-                        key = "true_negative"
+                        key = "seldon_metric_true_negative"
                     else:
-                        key = "false_positive"
+                        key = "seldon_metric_false_positive"
 
-                metrics = [{"key":key, "type": "COUNTER", "value": 1}]
+                metrics.append({"key":key, "type": "COUNTER", "value": 1})
 
-            elif self.name == "MULTICLASS_CLASSIFICATION_ONE_HOT":
-                pass
-            elif self.name == "MULTICLASS_CLASSIFICATION_NUMERIC":
-                pass
+            else:
+                if self.name == "MULTICLASS_CLASSIFICATION_ONE_HOT":
+                    # TODO: Perform check that input is list
+                    response = response if isinstance(response[0], list) else response[0]
+                    truth = truth if isinstance(truth[0], list) else truth[0]
+                    response_class = max(enumerate(response),key=lambda x: x[1])[0]
+                    truth_class = max(enumerate(truth),key=lambda x: x[1])[0]
+
+                elif self.name == "MULTICLASS_CLASSIFICATION_NUMERIC":
+                    response_class = response if isinstance(response, list) else response[0]
+                    truth_class = truth if isinstance(truth, list) else truth[0]
+
+                correct = response_class == truth_class
+
+                if correct:
+                    metrics.append({"key":"seldon_metric_true_positive",
+                                     "type": "COUNTER", "value": 1,
+                                     "tags": { "class": f"CLASS_{truth_class}" }})
+                else:
+                    metrics.append({"key":"seldon_metric_false_negative",
+                                     "type": "COUNTER", "value": 1,
+                                     "tags": { "class": f"CLASS_{truth_class}" }})
+                    metrics.append({"key":"seldon_metric_false_positive",
+                                     "type": "COUNTER", "value": 1,
+                                     "tags": { "class": f"CLASS_{response_class}" }})
+
 
         # TODO: Allow for scores to be used to calculate metrics as well
         seldon_response = SeldonResponse(output or None, None, metrics)
