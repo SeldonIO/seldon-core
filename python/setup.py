47ff9462943c0c48cf4610379b662a0c675987f8
seldondev <devext@seldon.io>
2020-09-28 15:00:39 +0000

    Added changes for branch

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ setup(
     name="seldon-core",
     author="Seldon Technologies Ltd.",
     author_email="hello@seldon.io",
-    version="1.3.0",
+    version="1.3.0_20200928150014",
     description="Seldon Core client and microservice wrapper",
     url="https://github.com/SeldonIO/seldon-core",
     license="Apache 2.0",
