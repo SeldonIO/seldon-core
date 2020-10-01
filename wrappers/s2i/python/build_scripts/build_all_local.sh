490651c9c088010ec563b9097510cde17e3ecbaf
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2020-10-01 14:43:11 +0200

    fix build_all_local.sh script to use correct gpu-related scripts

--- wrappers/s2i/python/build_scripts/build_all_local.sh
+++ wrappers/s2i/python/build_scripts/build_all_local.sh
@@ -1,5 +1,5 @@
 ./build_local_python3.6.sh
 ./build_local_python3.7.sh
-./build_local_python3.6_gpu.sh
-./build_local_python3.7_gpu.sh
+./build_python3.6_gpu.sh
+./build_python3.7_gpu.sh
 ./build_redhat.sh
