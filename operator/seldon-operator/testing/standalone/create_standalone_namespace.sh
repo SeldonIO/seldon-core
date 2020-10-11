472c5975134e277426dd53ba79f3d540e2bd968d
Clive Cox <cc@seldon.io>
2020-10-11 18:30:50 +0100

    remove old seldon-operator openshift folder

--- operator/seldon-operator/testing/standalone/create_standalone_namespace.sh
+++
@@ -1,9 +0,0 @@
-DEPLOY=../../deploy
-kubectl create -f ${DEPLOY}/cluster_role.yaml 
-kubectl create -f ${DEPLOY}/role_binding.yaml
-kubectl create -f ${DEPLOY}/namespace_role.yaml
-kubectl create -f ${DEPLOY}/namespace_role_binding.yaml
-kubectl create -f ${DEPLOY}/service_account.yaml
-kubectl create -f ${DEPLOY}/crds/machinelearning.seldon.io_seldondeployment_crd.yaml
-cat ${DEPLOY}/operator.yaml | tr '\n' '\f' | sed -e "s/        - name: WATCH_NAMESPACE\f          value: ''/        - name: WATCH_NAMESPACE\f          value: 'default'"/  | tr '\f' '\n' | kubectl create -f -
-
--- operator/seldon-operator/testing/standalone/delete_standalone.sh
+++
@@ -1,9 +0,0 @@
-DEPLOY=../../deploy
-kubectl delete -f ${DEPLOY}/cluster_role.yaml
-kubectl delete -f ${DEPLOY}/role_binding.yaml
-kubectl delete -f ${DEPLOY}/namespace_role.yaml
-kubectl delete -f ${DEPLOY}/namespace_role_binding.yaml
-kubectl delete -f ${DEPLOY}/service_account.yaml
-kubectl delete -f ${DEPLOY}/crds/machinelearning.seldon.io_seldondeployment_crd.yaml
-kubectl delete -f ${DEPLOY}/operator.yaml
-
