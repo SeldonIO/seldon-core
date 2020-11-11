cbaedca47273a2a2505cf026e49515f909484baf
Adrian Gonzalez-Martin <agm@seldon.io>
2020-11-11 14:49:56 +0000

    Use Istio instead of Ambassador

--- testing/scripts/e2e_utils/v2_protocol.py
+++ testing/scripts/e2e_utils/v2_protocol.py
@@ -1,10 +1,10 @@
 import requests
 
-from seldon_e2e_utils import API_AMBASSADOR
+from seldon_e2e_utils import API_ISTIO_GATEWAY
 
 
 def inference_request(
-    model_name: str, namespace: str, payload: dict, host: str = API_AMBASSADOR
+    model_name: str, namespace: str, payload: dict, host: str = API_ISTIO_GATEWAY
 ):
     endpoint = (
         f"http://{host}/seldon/{namespace}/{model_name}/v2/models/{model_name}/infer"
