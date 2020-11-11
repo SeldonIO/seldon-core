d49ec55a78415f4a01737c4d03a3a6a03d4ad06a
Adrian Gonzalez-Martin <agm@seldon.io>
2020-11-11 11:43:01 +0000

    Update regex

--- hack/update_python_version.sh
+++ hack/update_python_version.sh
@@ -14,9 +14,10 @@ fi
 NEW_VERSION=$1
 
 cd ${STARTUP_DIR}/.. \
-  && find . \
-      -type f \( -path './examples/*.ipynb' -or -path './doc/*.md'  -or -path './examples/*Makefile' \) \
-      -exec grep -El 'seldon-core-s2i-python3[67]?:[^\$\n ]+' \{\} \; | xargs -n1 -r sed -Ei "s/(seldon-core-s2i-python3)([67]?:)([^\$\n ]+)/\1\2${NEW_VERSION}/g"
+    && find . \
+        -type f \( -path './examples/*.ipynb' -or -path './doc/*.md'  -or -path './examples/*Makefile' \) \
+        -exec grep -El 'seldon-core-s2i-python3[67]?:[^\$ ]+' \{\} \; \
+            | xargs -n1 -r sed -Ei "s/(seldon-core-s2i-python3)([67]?:)([0-9]+\.[0-9]+\.[0-9]+(-dev)?)/\1\2${NEW_VERSION}/g"
 
 
 
