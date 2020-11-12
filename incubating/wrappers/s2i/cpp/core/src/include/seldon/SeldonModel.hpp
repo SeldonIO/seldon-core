4002f36d590e91e098905d3828a4ac122b4a560b
Alejandro Saucedo <axsauze@gmail.com>
2020-11-12 08:16:54 +0000

    Added tensorflow protos

--- incubating/wrappers/s2i/cpp/core/src/include/seldon/SeldonModel.hpp
+++ incubating/wrappers/s2i/cpp/core/src/include/seldon/SeldonModel.hpp
@@ -2,17 +2,19 @@
 
 #include <pybind11/pybind11.h>
 
-#include "SeldonProto.hpp"
+#include "seldon/proto/prediction.pb.h"
 
 namespace py = pybind11;
 
-namespace sc {
+namespace seldon {
 
 template<typename ProtoClass = seldon::protos::SeldonMessage>
 class SeldonModel 
 {
 public:
     SeldonModel();
+    
+    virtual ~SeldonModel();
 
     py::bytes& predictRaw(py::bytes &data);
 
@@ -21,5 +23,19 @@ public:
 private:
 
 };
+
+}
+
+namespace seldon {
+
+template<typename ProtoClass>
+SeldonModel<ProtoClass>::SeldonModel() {
+
+}
+
+template<typename ProtoClass>
+py::bytes& SeldonModel<ProtoClass>::predictRaw(py::bytes &data) {
+
+}
 
 }
