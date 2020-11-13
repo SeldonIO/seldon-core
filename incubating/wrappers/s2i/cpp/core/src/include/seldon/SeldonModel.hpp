a72bdd92d6d774362d1e082739f2ac21d7bb090f
Alejandro Saucedo <axsauze@gmail.com>
2020-11-13 18:34:13 +0000

    Base build with explicit library references

--- incubating/wrappers/s2i/cpp/core/src/include/seldon/SeldonModel.hpp
+++ incubating/wrappers/s2i/cpp/core/src/include/seldon/SeldonModel.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pybind11/pybind11.h>
+#include <google/protobuf/util/json_util.h>
 
 #include "prediction.pb.h"
 
@@ -11,15 +12,32 @@ namespace seldon {
 class SeldonModel 
 {
 public:
-    SeldonModel();
+    SeldonModel() {}
     
-    virtual ~SeldonModel();
+    virtual ~SeldonModel() {}
 
-    py::bytes& predictRaw(py::bytes &data);
+    // TODO: Return without copy
+    py::bytes predictRaw(py::bytes &data) {
+
+        py::buffer_info info(py::buffer(data).request());
+        const char *charData = reinterpret_cast<const char *>(info.ptr);
+        size_t charLength = static_cast<size_t>(info.size);
+
+        std::string strData(charData, charLength);
+
+        seldon::protos::SeldonMessage input;
+        google::protobuf::util::JsonStringToMessage(strData, &input);
+
+        seldon::protos::SeldonMessage output = this->predict(output);
+
+        std::string outString;
+        google::protobuf::util::MessageToJsonString(output, &outString);
+
+        return outString;
+    }
+
 
     virtual seldon::protos::SeldonMessage& predict(seldon::protos::SeldonMessage &data) = 0;
-
-private:
 
 };
 
