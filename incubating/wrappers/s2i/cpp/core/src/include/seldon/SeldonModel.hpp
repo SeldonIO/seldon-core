1b988a78fa21243a71164690dcf0395094547440
Alejandro Saucedo <axsauze@gmail.com>
2020-11-13 19:13:26 +0000

    Working seldonmessage

--- incubating/wrappers/s2i/cpp/core/src/include/seldon/SeldonModel.hpp
+++ incubating/wrappers/s2i/cpp/core/src/include/seldon/SeldonModel.hpp
@@ -3,6 +3,8 @@
 #include <pybind11/pybind11.h>
 #include <google/protobuf/util/json_util.h>
 
+#include <iostream>
+
 #include "prediction.pb.h"
 
 namespace py = pybind11;
@@ -24,20 +26,25 @@ public:
         size_t charLength = static_cast<size_t>(info.size);
 
         std::string strData(charData, charLength);
+        std::cout << "starting function with str: " << strData << std::endl;
 
         seldon::protos::SeldonMessage input;
         google::protobuf::util::JsonStringToMessage(strData, &input);
+        std::cout << "Converted to jsonmessage" << std::endl;
 
         seldon::protos::SeldonMessage output = this->predict(output);
+
+        std::cout << "Converting output" << std::endl;
 
         std::string outString;
         google::protobuf::util::MessageToJsonString(output, &outString);
 
+        std::cout << "Returning output" << std::endl;
         return outString;
     }
 
 
-    virtual seldon::protos::SeldonMessage& predict(seldon::protos::SeldonMessage &data) = 0;
+    virtual seldon::protos::SeldonMessage predict(seldon::protos::SeldonMessage &data) = 0;
 
 };
 
