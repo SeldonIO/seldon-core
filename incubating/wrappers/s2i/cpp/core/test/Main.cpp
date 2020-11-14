e2262bdbcff8d558b3bd263a3afdc6eb7f92e46d
Alejandro Saucedo <axsauze@gmail.com>
2020-11-14 08:21:10 +0000

    Added base test

--- incubating/wrappers/s2i/cpp/core/test/Main.cpp
+++ incubating/wrappers/s2i/cpp/core/test/Main.cpp
@@ -10,10 +10,6 @@
 class TestModel : public seldon::SeldonModel {
 
     seldon::protos::SeldonMessage predict(seldon::protos::SeldonMessage &data) override {
-        //data.has_data()
-        //google::protobuf::ListValue dataList = data.data().ndarray();
-        //std::vector<google::protobuf::Value> dataVec(dataList.values().cbegin(), dataList.values().cend());
-        ////std::cout << "DATA IS: " << dataVec[0].string_value() << "." << std::endl;
         std::cout << "DATA IS: " << data.strdata() << "." << std::endl;
         std::cout << "done fund." << std::endl;
         return data;
