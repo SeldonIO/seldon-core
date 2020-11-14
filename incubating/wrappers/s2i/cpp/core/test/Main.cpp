fe7685a050651e9d7305a81b42c3caa3f7a17f42
Alejandro Saucedo <axsauze@gmail.com>
2020-11-14 07:34:35 +0000

    Working build

--- incubating/wrappers/s2i/cpp/core/test/Main.cpp
+++ incubating/wrappers/s2i/cpp/core/test/Main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
 
 #include "seldon/SeldonModel.hpp"
 
@@ -10,9 +11,9 @@ class TestModel : public seldon::SeldonModel {
 
     seldon::protos::SeldonMessage predict(seldon::protos::SeldonMessage &data) override {
         //data.has_data()
-        google::protobuf::ListValue dataList = data.data().ndarray();
-        std::vector<google::protobuf::Value> dataVec(dataList.values().cbegin(), dataList.values().cend());
-        //std::cout << "DATA IS: " << dataVec[0].string_value() << "." << std::endl;
+        //google::protobuf::ListValue dataList = data.data().ndarray();
+        //std::vector<google::protobuf::Value> dataVec(dataList.values().cbegin(), dataList.values().cend());
+        ////std::cout << "DATA IS: " << dataVec[0].string_value() << "." << std::endl;
         std::cout << "DATA IS: " << data.strdata() << "." << std::endl;
         std::cout << "done fund." << std::endl;
         return data;
