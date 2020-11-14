62e0891c8ee980a4330361ec5a8d030de4d8e877
Alejandro Saucedo <axsauze@gmail.com>
2020-11-14 18:47:26 +0000

    Updated to support multiple proto messages

--- incubating/wrappers/s2i/cpp/src/include/seldon/SeldonModel.hpp
+++ incubating/wrappers/s2i/cpp/src/include/seldon/SeldonModel.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pybind11/pybind11.h>
+#include <google/protobuf/util/json_util.h>
 
 #include "prediction.pb.h"
 
@@ -8,20 +9,45 @@ namespace py = pybind11;
 
 namespace seldon {
 
+template <typename ProtoMessage = protos::SeldonMessage>
 class SeldonModel
 {
 public:
-    SeldonModel();
+    SeldonModel() { }
 
-    virtual ~SeldonModel();
-
-    // TODO: Return without copy
-    virtual protos::SeldonMessage predict(protos::SeldonMessage &data) = 0;
+    virtual ~SeldonModel() { }
 
     // TODO: Return without copy
-    py::bytes predictRaw(py::bytes &data);
+    virtual ProtoMessage predict(ProtoMessage &data) = 0;
+
+    // TODO: Return without copy
+    virtual py::bytes predictRaw(py::bytes &data) {
+
+        py::buffer_info info(py::buffer(data).request());
+        const char *charData = reinterpret_cast<const char *>(info.ptr);
+        size_t charLength = static_cast<size_t>(info.size);
+
+        std::string strData(charData, charLength);
+        std::cout << "starting function with str: " << strData << std::endl;
+
+        ProtoMessage input;
+        google::protobuf::util::JsonStringToMessage(strData, &input);
+        std::cout << "Converted to jsonmessage" << std::endl;
+
+        ProtoMessage output = this->predict(input);
+
+        std::cout << "Converting output" << std::endl;
+
+        std::string outString;
+        google::protobuf::util::MessageToJsonString(output, &outString);
+
+        std::cout << "Returning output" << std::endl;
+        return outString;
+    }
 
 };
+
+using SeldonModelBase = SeldonModel<protos::SeldonMessage>;
 
 }
 
