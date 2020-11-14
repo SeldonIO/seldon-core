5d98921413865f9bad714af911cf3752fd93add2
Alejandro Saucedo <axsauze@gmail.com>
2020-11-14 19:02:24 +0000

    Added example for buildsystem-override and simple-cpp

---
+++ examples/models/cpp/simple-cpp/SeldonPackage.cpp
@@ -0,0 +1,19 @@
+#include <pybind11/pybind11.h>
+
+#include "seldon/SeldonModel.hpp"
+
+namespace py = pybind11;
+
+class ModelClass : public seldon::SeldonModelBase {
+
+    seldon::protos::SeldonMessage predict(seldon::protos::SeldonMessage &data) override {
+        return data;
+    }
+};
+
+PYBIND11_MODULE(SeldonPackage, m) {
+    py::class_<ModelClass>(m, "ModelClass")
+        .def(py::init())
+        .def("predict_raw", &ModelClass::predictRaw);
+}
+
