3fa1b54d0b5b734173c07c6b6f2e43c74d027548
Alejandro Saucedo <axsauze@gmail.com>
2020-11-14 17:09:15 +0000

    Added support for shared and static libs and fixed ccls for editor checking

--- incubating/wrappers/s2i/cpp/src/include/seldon/SeldonModel.hpp
+++ incubating/wrappers/s2i/cpp/src/include/seldon/SeldonModel.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
 #include <pybind11/pybind11.h>
-#include <google/protobuf/util/json_util.h>
-
-#include <iostream>
-#include <memory>
 
 #include "prediction.pb.h"
 
@@ -12,40 +8,18 @@ namespace py = pybind11;
 
 namespace seldon {
 
-class SeldonModel 
+class SeldonModel
 {
 public:
-    SeldonModel() {}
-    
-    virtual ~SeldonModel() {}
+    SeldonModel();
+
+    virtual ~SeldonModel();
 
     // TODO: Return without copy
-    py::bytes predictRaw(py::bytes &data) {
-
-        py::buffer_info info(py::buffer(data).request());
-        const char *charData = reinterpret_cast<const char *>(info.ptr);
-        size_t charLength = static_cast<size_t>(info.size);
-
-        std::string strData(charData, charLength);
-        std::cout << "starting function with str: " << strData << std::endl;
-
-        seldon::protos::SeldonMessage input;
-        google::protobuf::util::JsonStringToMessage(strData, &input);
-        std::cout << "Converted to jsonmessage" << std::endl;
-
-        seldon::protos::SeldonMessage output = this->predict(input);
-
-        std::cout << "Converting output" << std::endl;
-
-        std::string outString;
-        google::protobuf::util::MessageToJsonString(output, &outString);
-
-        std::cout << "Returning output" << std::endl;
-        return outString;
-    }
+    virtual protos::SeldonMessage predict(protos::SeldonMessage &data) = 0;
 
     // TODO: Return without copy
-    virtual seldon::protos::SeldonMessage predict(seldon::protos::SeldonMessage &data) = 0;
+    py::bytes predictRaw(py::bytes &data);
 
 };
 
