207ad72c464acb6960466553a80bba28c4634548
Adrian Gonzalez-Martin <agm@seldon.io>
2020-11-16 14:47:49 +0000

    Add retries

--- testing/scripts/e2e_utils/v2_protocol.py
+++ testing/scripts/e2e_utils/v2_protocol.py
@@ -1,8 +1,18 @@
 import requests
 
+from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_result
 from seldon_e2e_utils import API_AMBASSADOR
 
 
+def _is_404(res):
+    return res.status_code == 404
+
+
+@retry(
+    wait=wait_exponential(multiplier=1),
+    stop=stop_after_attempt(3),
+    retry=retry_if_result(_is_404),
+)
 def inference_request(
     model_name: str, namespace: str, payload: dict, host: str = API_AMBASSADOR
 ):
