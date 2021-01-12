484f4d4f16734eae2d5e4cb1ad07bd324e3021a1
Clive Cox <cc@seldon.io>
2021-01-12 13:28:17 +0000

    Add V2 data plane support for alibi detect server

--- components/alibi-detect-server/adserver/protocols/__init__.py
+++ components/alibi-detect-server/adserver/protocols/__init__.py
@@ -5,6 +5,7 @@ class Protocol(Enum):
     tensorflow_http = "tensorflow.http"
     seldon_http = "seldon.http"
     seldonfeedback_http = "seldonfeedback.http"
+    v2_http = "v2.http"
 
     def __str__(self):
         return self.value
---
+++ components/drift-detection/nvidia-triton-cifar10/README.md
@@ -0,0 +1,4 @@
+# CIFAR10 Drift Detector with NVIDIA Triton 
+
+Run the [notebook demo](cifar10_drift.ipynb) to test.
+
