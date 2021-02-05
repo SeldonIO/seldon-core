444a8830bd8c2478fb1ba0f24dec6b310b1cd2e5
zyxue <alfred532008@gmail.com>
2021-02-05 07:48:56 -0800

    revert isort changes in seldon_core/proto/tensorflow/core/framework/__init__.py

--- python/seldon_core/proto/tensorflow/core/framework/__init__.py
+++ python/seldon_core/proto/tensorflow/core/framework/__init__.py
@@ -2,4 +2,4 @@ from seldon_core.imports_helper import _TF_PRESENT
 
 if _TF_PRESENT:
     # Let tensorflow shadow these imports if present
-    from tensorflow.core.framework import tensor_pb2, tensor_shape_pb2, types_pb2
+    from tensorflow.core.framework import tensor_pb2, types_pb2, tensor_shape_pb2
