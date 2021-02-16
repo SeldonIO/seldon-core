bf7e2f8ddcfe41e5b9ff1d96106317df65295fb2
Alejandro Saucedo <axsauze@gmail.com>
2021-02-16 13:17:23 +0000

    Addressing base python image cves

--- python/setup.py
+++ python/setup.py
@@ -26,6 +26,8 @@ setup(
         "Flask-cors<4.0.0",
         "redis<4.0.0",
         "requests<3.0.0",
+        # Addresses CVE CVE-2019-11236 and CVE-2020-26137
+        "urllib3 >= 1.25.9, < 1.26"
         "numpy<2.0.0",
         "flatbuffers<2.0.0",
         "protobuf<4.0.0",
@@ -35,10 +37,13 @@ setup(
         "jaeger-client >= 4.1.0, < 4.5.0",
         "grpcio-opentracing >= 1.1.4, < 1.2.0",
         "grpcio-reflection < 1.35.0",
-        "PyYAML<5.5",
+        # Addresses CVE SNYK-PYTHON-PYYAML-590151
+        "PyYAML >= 5.4, < 5.5",
         "gunicorn >= 19.9.0, < 20.1.0",
         "setuptools >= 41.0.0",
         "prometheus_client >= 0.7.1, < 0.9.0",
+        # Addresses CVE PRISMA-2021-0020
+        "click >= 8.0.0a1, < 8.1"
     ],
     extras_require=extras,
     entry_points={
