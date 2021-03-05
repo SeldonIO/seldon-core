53fd4558aa705c8704a1fca39c8209c315191f64
Adrian Gonzalez-Martin <agm@seldon.io>
2021-03-05 09:23:07 +0000

    Add test for clearing metrics

---
+++ python/tests/test_gunicorn_utils.py
@@ -0,0 +1,51 @@
+import pytest
+import requests
+import signal
+import os
+import time
+
+from typing import Set
+from prometheus_client.parser import text_string_to_metric_families
+
+
+def _get_workers(
+    metrics_endpoint: str = "http://127.0.0.1:6005/metrics-endpoint",
+) -> Set[str]:
+    workers = set()
+
+    res = requests.get("http://127.0.0.1:6005/metrics-endpoint")
+    families = text_string_to_metric_families(res.content.decode())
+    for fam in families:
+        for sample in fam.samples:
+            worker_id = sample.labels["worker_id"]
+            workers.add(worker_id)
+
+    return workers
+
+
+@pytest.mark.parametrize(
+    "microservice", [{"app_name": "custom-metrics-model"}], indirect=True
+)
+def test_worker_exit(microservice):
+    # Warm up the custom metrics
+    for _ in range(5):
+        res = requests.post(
+            "http://127.0.0.1:9000/api/v1.0/predictions",
+            json={"data": {"ndarray": [[1, 2, 3]]}},
+        )
+        res.raise_for_status()
+
+    # Fetch metrics and get current list of workers
+    workers = _get_workers(metrics_endpoint="http://127.0.0.1:6005/metrics-endpoint")
+    assert len(workers) != 0
+
+    # Ask Gunicorn to restart all workers (through a HUP)
+    server_pid = microservice.p.pid
+    os.kill(server_pid, signal.SIGHUP)
+    time.sleep(1)
+
+    # Metrics should now be empty
+    empty_workers = _get_workers(
+        metrics_endpoint="http://127.0.0.1:6005/metrics-endpoint"
+    )
+    assert len(empty_workers) == 0
