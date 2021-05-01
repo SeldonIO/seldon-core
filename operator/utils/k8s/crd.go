caa2708c44c5b5a83c40842738f9b1f8cebacdc0
Clive Cox <cc@seldon.io>
2021-05-01 11:15:41 +0100

    create v1 or vebeta1 CRD

--- operator/utils/k8s/crd.go
+++ operator/utils/k8s/crd.go
@@ -2,57 +2,124 @@ package k8s
 
 import (
 	"context"
+	"github.com/Masterminds/semver"
 	"github.com/ghodss/yaml"
 	"github.com/go-logr/logr"
+	extensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/discovery"
 )
 
 type CrdCreator struct {
-	clientset apiextensionsclient.Interface
-	logger    logr.Logger
-	ctx       context.Context
+	apiExtensionsClient apiextensionsclient.Interface
+	discoveryClient     discovery.DiscoveryInterface
+	logger              logr.Logger
+	ctx                 context.Context
 }
 
-func NewCrdCreator(ctx context.Context, clientset apiextensionsclient.Interface, logger logr.Logger) *CrdCreator {
+func NewCrdCreator(ctx context.Context, apiExtensionsClient apiextensionsclient.Interface, discoveryClient discovery.DiscoveryInterface, logger logr.Logger) *CrdCreator {
 	return &CrdCreator{
-		clientset: clientset,
-		logger:    logger,
-		ctx:       ctx,
+		apiExtensionsClient: apiExtensionsClient,
+		discoveryClient:     discoveryClient,
+		logger:              logger,
+		ctx:                 ctx,
 	}
 }
 
-func (cc *CrdCreator) findCRD() (*v1beta1.CustomResourceDefinition, error) {
-	client := cc.clientset.ApiextensionsV1beta1().CustomResourceDefinitions()
+func (cc *CrdCreator) findCRDv1beta1() (*v1beta1.CustomResourceDefinition, error) {
+	client := cc.apiExtensionsClient.ApiextensionsV1beta1().CustomResourceDefinitions()
 	return client.Get(cc.ctx, CRDName, v1.GetOptions{})
 }
 
-func (cc *CrdCreator) createCRD(rawYaml []byte) (*v1beta1.CustomResourceDefinition, error) {
+func (cc *CrdCreator) findCRDv1() (*extensionsv1.CustomResourceDefinition, error) {
+	client := cc.apiExtensionsClient.ApiextensionsV1().CustomResourceDefinitions()
+	return client.Get(cc.ctx, CRDName, v1.GetOptions{})
+}
+
+func (cc *CrdCreator) createCRDV1beta1(rawYaml []byte) (*v1beta1.CustomResourceDefinition, error) {
 	crd := v1beta1.CustomResourceDefinition{}
 	err := yaml.Unmarshal(rawYaml, &crd)
 	if err != nil {
 		return nil, err
 	}
-	client := cc.clientset.ApiextensionsV1beta1().CustomResourceDefinitions()
+	client := cc.apiExtensionsClient.ApiextensionsV1beta1().CustomResourceDefinitions()
 	return client.Create(cc.ctx, &crd, v1.CreateOptions{})
 }
-func (cc *CrdCreator) findOrCreateCRD(rawYaml []byte) (*v1beta1.CustomResourceDefinition, error) {
+
+func (cc *CrdCreator) createCRDV1(rawYaml []byte) (*extensionsv1.CustomResourceDefinition, error) {
+	crd := extensionsv1.CustomResourceDefinition{}
+	err := yaml.Unmarshal(rawYaml, &crd)
+	if err != nil {
+		return nil, err
+	}
+	client := cc.apiExtensionsClient.ApiextensionsV1().CustomResourceDefinitions()
+	return client.Create(cc.ctx, &crd, v1.CreateOptions{})
+}
+
+func (cc *CrdCreator) findOrCreateCRDV1beta1(rawYaml []byte) (v1.Object, error) {
 	//Find or create CRD
-	crd, err := cc.findCRD()
+	crd, err := cc.findCRDv1beta1()
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// create CRD
-			cc.logger.Info("CRD not found - trying to create")
-			crd, err = cc.createCRD(rawYaml)
+			cc.logger.Info("CRD v1beta1 not found - trying to create")
+			crd, err = cc.createCRDV1beta1(rawYaml)
 			if err != nil {
 				return nil, err
 			}
-			cc.logger.Info("CRD created")
+			cc.logger.Info("CRD v1beta1 created")
 		} else {
 			return nil, err
 		}
+	} else {
+		cc.logger.Info("CRD v1beta1 already exists")
 	}
 	return crd, nil
+}
+
+func (cc *CrdCreator) findOrCreateCRDV1(rawYaml []byte) (*extensionsv1.CustomResourceDefinition, error) {
+	//Find or create CRD
+	crd, err := cc.findCRDv1()
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// create CRD
+			cc.logger.Info("CRD V1 not found - trying to create")
+			crd, err = cc.createCRDV1(rawYaml)
+			if err != nil {
+				return nil, err
+			}
+			cc.logger.Info("CRD V1 created")
+		} else {
+			return nil, err
+		}
+	} else {
+		cc.logger.Info("CRD v1 already exists")
+	}
+	return crd, nil
+}
+
+func (cc *CrdCreator) findOrCreateCRD(rawYamlv1 []byte, rawYamlv1beta1 []byte) (v1.Object, error) {
+	sv, err := cc.discoveryClient.ServerVersion()
+	if err != nil {
+		return nil, err
+	}
+	v, err := semver.NewVersion(sv.String())
+	if err != nil {
+		return nil, err
+	}
+	c, err := semver.NewConstraint(">= 1.18.0")
+	if err != nil {
+		return nil, err
+	}
+	check := c.Check(v)
+	if check {
+		cc.logger.Info("Creating V1 CRD for K8s", "version", sv.String())
+		return cc.findOrCreateCRDV1(rawYamlv1)
+	} else {
+		cc.logger.Info("Creating V1Beta1 CRD for K8s", "version", sv.String())
+		return cc.findOrCreateCRDV1beta1(rawYamlv1beta1)
+	}
 }
--- operator/utils/k8s/crd_test.go
+++ operator/utils/k8s/crd_test.go
@@ -4,17 +4,71 @@ import (
 	"context"
 	. "github.com/onsi/gomega"
 	apiextensionsfake "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
+	"k8s.io/apimachinery/pkg/version"
+	discovery "k8s.io/client-go/discovery/fake"
+	coretesting "k8s.io/client-go/testing"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"testing"
 )
 
-func TestCRDCreate(t *testing.T) {
+func TestCRDCreateV1beta1(t *testing.T) {
 	g := NewGomegaWithT(t)
 	apiExtensionsFake := apiextensionsfake.NewSimpleClientset()
-	crdCreator := NewCrdCreator(context.TODO(), apiExtensionsFake, ctrl.Log)
-	bytes, err := LoadBytesFromFile("testdata", "crd.yaml")
+	discoveryFake := &discovery.FakeDiscovery{Fake: &coretesting.Fake{}}
+	crdCreator := NewCrdCreator(context.TODO(), apiExtensionsFake, discoveryFake, ctrl.Log)
+	bytes, err := LoadBytesFromFile("testdata", "crd-v1beta1.yaml")
 	g.Expect(err).To(BeNil())
-	crd, err := crdCreator.findOrCreateCRD(bytes)
+	crd, err := crdCreator.findOrCreateCRDV1beta1(bytes)
+	g.Expect(err).To(BeNil())
+	g.Expect(crd).NotTo(BeNil())
+}
+
+func TestCRDCreateV1(t *testing.T) {
+	g := NewGomegaWithT(t)
+	apiExtensionsFake := apiextensionsfake.NewSimpleClientset()
+	discoveryFake := &discovery.FakeDiscovery{Fake: &coretesting.Fake{},
+		FakedServerVersion: &version.Info{
+			Major: "1",
+			Minor: "18",
+		}}
+	crdCreator := NewCrdCreator(context.TODO(), apiExtensionsFake, discoveryFake, ctrl.Log)
+	bytes, err := LoadBytesFromFile("testdata", "crd-v1.yaml")
+	g.Expect(err).To(BeNil())
+	crd, err := crdCreator.findOrCreateCRDV1(bytes)
+	g.Expect(err).To(BeNil())
+	g.Expect(crd).NotTo(BeNil())
+}
+
+func TestCRDFindCreateV1(t *testing.T) {
+	g := NewGomegaWithT(t)
+	apiExtensionsFake := apiextensionsfake.NewSimpleClientset()
+	discoveryFake := &discovery.FakeDiscovery{Fake: &coretesting.Fake{},
+		FakedServerVersion: &version.Info{
+			GitVersion: "v1.19.0",
+		}}
+	crdCreator := NewCrdCreator(context.TODO(), apiExtensionsFake, discoveryFake, ctrl.Log)
+	bytesV1, err := LoadBytesFromFile("testdata", "crd-v1.yaml")
+	g.Expect(err).To(BeNil())
+	bytesV1beta1, err := LoadBytesFromFile("testdata", "crd-v1beta1.yaml")
+	g.Expect(err).To(BeNil())
+	crd, err := crdCreator.findOrCreateCRD(bytesV1, bytesV1beta1)
+	g.Expect(err).To(BeNil())
+	g.Expect(crd).NotTo(BeNil())
+}
+
+func TestCRDFindCreateV1Beta1(t *testing.T) {
+	g := NewGomegaWithT(t)
+	apiExtensionsFake := apiextensionsfake.NewSimpleClientset()
+	discoveryFake := &discovery.FakeDiscovery{Fake: &coretesting.Fake{},
+		FakedServerVersion: &version.Info{
+			GitVersion: "v1.16.0",
+		}}
+	crdCreator := NewCrdCreator(context.TODO(), apiExtensionsFake, discoveryFake, ctrl.Log)
+	bytesV1, err := LoadBytesFromFile("testdata", "crd-v1.yaml")
+	g.Expect(err).To(BeNil())
+	bytesV1beta1, err := LoadBytesFromFile("testdata", "crd-v1beta1.yaml")
+	g.Expect(err).To(BeNil())
+	crd, err := crdCreator.findOrCreateCRD(bytesV1, bytesV1beta1)
 	g.Expect(err).To(BeNil())
 	g.Expect(crd).NotTo(BeNil())
 }
