38e62e4c8483b359982b3dfbbec0ba43f8ad272d
Clive Cox <cc@seldon.io>
2021-05-17 10:17:47 +0100

    wait longer on adserver tests

--- testing/scripts/test_alibi_detect_server.py
+++ testing/scripts/test_alibi_detect_server.py
@@ -4,7 +4,6 @@ import time
 import uuid
 from subprocess import run
 
-import pytest
 import requests
 from tenacity import RetryError, Retrying, stop_after_attempt, wait_fixed
 
@@ -34,7 +33,6 @@ class TestADServer:
         "ce-specversion": "1.0",
     }
 
-    @pytest.mark.flaky(max_runs=5)
     def test_alibi_detect_cifar10(self, namespace):
         spec = "../resources/adserver-cifar10-od.yaml"
         name = "cifar10-od-server"
@@ -49,7 +47,7 @@ class TestADServer:
         with open(self.truck_json) as f:
             data = json.load(f)
 
-        for attempt in Retrying(wait=wait_fixed(4), stop=stop_after_attempt(3)):
+        for attempt in Retrying(wait=wait_fixed(10), stop=stop_after_attempt(4)):
             with attempt:
                 r = requests.post(
                     f"http://localhost:8004/{vs_prefix}/",
@@ -78,7 +76,6 @@ class TestADServer:
 
         run(f"kubectl delete -f {spec} -n {namespace}", shell=True)
 
-    @pytest.mark.flaky(max_runs=5)
     def test_alibi_detect_cifar10_rclone(self, namespace):
         spec = "../resources/adserver-cifar10-od-rclone.yaml"
         name = "cifar10-od-server-rclone"
@@ -93,7 +90,7 @@ class TestADServer:
         with open(self.truck_json) as f:
             data = json.load(f)
 
-        for attempt in Retrying(wait=wait_fixed(4), stop=stop_after_attempt(3)):
+        for attempt in Retrying(wait=wait_fixed(10), stop=stop_after_attempt(4)):
             with attempt:
                 r = requests.post(
                     f"http://localhost:8004/{vs_prefix}/",
