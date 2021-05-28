016a0ce6b7e3abe5040584a298a104e26301852d
Clive Cox <cc@seldon.io>
2021-05-28 16:01:47 +0100

    Add istio host annotation

--- operator/controllers/istio.go
+++ operator/controllers/istio.go
@@ -7,4 +7,5 @@ const (
 	ANNOTATION_ISTIO_GATEWAY         = "seldon.io/istio-gateway"
 	ANNOTATION_ISTIO_RETRIES         = "seldon.io/istio-retries"
 	ANNOTATION_ISTIO_RETRIES_TIMEOUT = "seldon.io/istio-retries-timeout"
+	ANNOTATION_ISTIO_HOST            = "seldon.io/istio-host"
 )
