10d98235654b14bab6f17c9df5a7429678de7834
Clive Cox <cc@seldon.io>
2021-06-04 08:24:04 +0100

    Clean up openapi

--- openapi/create_openapis.py
+++ openapi/create_openapis.py
@@ -1,5 +1,4 @@
 import json
-import sys
 
 if __name__ == "__main__":
 
@@ -7,8 +6,6 @@ if __name__ == "__main__":
         base = json.load(f)
     with open('paths-ambassador.json') as f:
         paths_ambassador = json.load(f)
-    with open('paths-apife.json') as f:
-        paths_apife = json.load(f)
     with open('components.json') as f:
         components = json.load(f)
     with open('paths-internal.json') as f:
@@ -18,36 +15,22 @@ if __name__ == "__main__":
 
     #
     # Create engine.oas3.json
-    #        
+    #
     base["paths"] = paths_ambassador["paths"]
     base["components"] = components["components"]
 
     with open('engine.oas3.json', 'w') as outfile:
             json.dump(base, outfile, indent=4)
 
-            
+
     #
     # Create component.swagger.json
     #
     with open('base.json') as f:
         base = json.load(f)
-        
+
     base["paths"] = paths_internal["paths"]
-    base["components"] = components["components"]    
+    base["components"] = components["components"]
 
     with open('wrapper.oas3.json', 'w') as outfile:
-            json.dump(base, outfile, indent=4)
-
-    #
-    # Create apife.swagger.json
-    #
-    with open('base.json') as f:
-        base = json.load(f)
-        
-    base["paths"] = paths_apife["paths"]
-    base["components"] = components["components"]    
-    base["components"]["securitySchemes"] = security["securitySchemes"]
-    base["security"] = security["security"]    
-
-    with open('apife.oas3.json', 'w') as outfile:
             json.dump(base, outfile, indent=4)
