d809886d0fec943c9722f0fb739a80021d4491fe
Alejandro Saucedo <axsauze@gmail.com>
2021-06-26 19:54:57 +0100

    Updated test to cover sequences

--- testing/scripts/test_benchmark.py
+++ testing/scripts/test_benchmark.py
@@ -4,6 +4,5 @@ from seldon_e2e_utils import create_and_run_script
 
 
 @pytest.mark.benchmark
-@pytest.mark.usefixtures("ensure_argo_workflows")
 def test_benchmark_overall(namespace):
     create_and_run_script("../../examples/batch/benchmarking-argo-workflows", "README")
