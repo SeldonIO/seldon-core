ad4a9d1c6a47904f77601644b3a6f23702eaf836
Alejandro Saucedo <axsauze@gmail.com>
2021-06-27 10:03:10 +0100

    Updated example

--- testing/scripts/test_benchmark.py
+++ testing/scripts/test_benchmark.py
@@ -4,5 +4,5 @@ from seldon_e2e_utils import create_and_run_script
 
 
 @pytest.mark.benchmark
-def test_benchmark_overall(namespace):
+def test_benchmark_overall():
     create_and_run_script("../../examples/batch/benchmarking-argo-workflows", "README")
