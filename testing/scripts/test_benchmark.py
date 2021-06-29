ad4a27ede11211bf77d7039d45a7d9690f327ae2
Alejandro Saucedo <axsauze@gmail.com>
2021-06-29 22:28:51 +0100

    Extending benchmark testing

--- testing/scripts/test_benchmark.py
+++ testing/scripts/test_benchmark.py
@@ -17,29 +17,46 @@ def test_service_orchestrator():
     )
 
     result_body = "# Benchmark results\n\n"
-    result_body += str(df.to_markdown())
-    post_comment_in_pr(result_body)
 
-    # Ensure all mean performance latency below 7 ms
-    assert all(df["mean"] < 5)
+    # Ensure all mean performance latency below 5 ms
+    latency_mean = all(df["mean"] < 5)
+    result_body += f"* All mean performance latency under 5ms: {latency_mean}"
     # Ensure 99th percentiles are not spiking above 15ms
-    assert all(df["mean"] < 10)
+    latency_nth = all(df["99th"] < 10)
+    result_body += f"* All 99th performance latenc under 10ms: {latency_nth}"
     # Ensure throughput is above 200 rps for REST
-    assert all(df[df["apiType"] == "rest"]["throughputAchieved"] > 200)
+    rps_rest = all(df[df["apiType"] == "rest"]["throughputAchieved"] > 200)
+    result_body += f"* REST throughput above 200rps: {rps_rest}"
     # Ensure throughput is above 250 rps for GRPC
-    assert all(df[df["apiType"] == "grpc"]["throughputAchieved"] > 250)
+    rps_grpc = all(df[df["apiType"] == "grpc"]["throughputAchieved"] > 250)
+    result_body += f"* GRPC throughput above 250rps: {rps_grpc}"
     # Validate latenc added by adding service orchestrator is lower than 4ms
-    assert all(
+    orch_mean = all(
         (
             df[df["disableOrchestrator"] == "true"]["mean"].values
             - df[df["disableOrchestrator"] == "false"]["mean"].values
         )
         < 2
     )
-    assert all(
+    result_body += f"* Orch added mean latency under 2ms: {orch_mean}"
+    orch_nth = all(
         (
             df[df["disableOrchestrator"] == "true"]["99th"].values
             - df[df["disableOrchestrator"] == "false"]["99th"].values
         )
         < 2
     )
+    result_body += f"* Orch added 99th latency under 2ms: {orch_nth}"
+
+    result_body += "\n### Results table\n\n"
+    result_body += str(df.to_markdown())
+    post_comment_in_pr(result_body)
+
+    assert latency_mean
+    assert latency_nth
+    assert rps_rest
+    assert rps_grpc
+    assert orch_mean
+    assert orch_nth
+
+
