0ad71cb51ca85eed28284b691435da64eaf9d260
Sherif Akoush <sa@seldon.io>
2021-07-19 14:55:04 +0100

    move other environment variables to utils.py

--- python/seldon_core/utils.py
+++ python/seldon_core/utils.py
@@ -26,11 +26,32 @@ if _TF_PRESENT:
 
 logger = logging.getLogger(__name__)
 
-
+"""
+Environment variables
+"""
 ENV_MODEL_NAME = "PREDICTIVE_UNIT_ID"
 ENV_MODEL_IMAGE = "PREDICTIVE_UNIT_IMAGE"
+ENV_SELDON_DEPLOYMENT_NAME = "SELDON_DEPLOYMENT_ID"
+ENV_PREDICTOR_NAME = "PREDICTOR_ID"
+ENV_PREDICTOR_LABELS = "PREDICTOR_LABELS"
+
 NONIMPLEMENTED_MSG = "NOT_IMPLEMENTED"
 NONIMPLEMENTED_IMAGE_MSG = f"{NONIMPLEMENTED_MSG}:{NONIMPLEMENTED_MSG}"
+
+
+def get_predictior_version() -> str:
+    return json.loads(os.environ.get(ENV_PREDICTOR_LABELS, "{}")).get(
+        "version", f"{NONIMPLEMENTED_MSG}")
+
+
+def get_predictor_name() -> str:
+    return os.environ.get(ENV_PREDICTOR_NAME, f"{NONIMPLEMENTED_MSG}")
+
+
+def get_deployment_name() -> str:
+    return os.environ.get(
+        ENV_SELDON_DEPLOYMENT_NAME, f"{NONIMPLEMENTED_MSG}"
+    )
 
 
 def get_model_name() -> str:
