d353bb31daed39e769af5f791e358067f801af2c
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2021-08-09 11:00:27 +0300

    do not build 3.8 gpu for now

--- wrappers/s2i/python/build_scripts/build_all.sh
+++ wrappers/s2i/python/build_scripts/build_all.sh
@@ -4,7 +4,7 @@ make -C ../ build_conda_base
 # Build standard wrapper images
 make -C ../ build PYTHON_VERSION=3.6
 make -C ../ build PYTHON_VERSION=3.7.10
-make -C ../ build PYTHON_VERSION=3.8.11
+make -C ../ build PYTHON_VERSION=3.8.10
 
 # Tag the default image
 make -C ../ tag_base_python PYTHON_VERSION=3.7.10
@@ -12,4 +12,4 @@ make -C ../ tag_base_python PYTHON_VERSION=3.7.10
 # Build GPU images
 make -C ../ build_gpu PYTHON_VERSION=3.6
 make -C ../ build_gpu PYTHON_VERSION=3.7.10
-make -C ../ build_gpu PYTHON_VERSION=3.8.11
+make -C ../ build_gpu PYTHON_VERSION=3.8.10
--- wrappers/s2i/python/build_scripts/push_all.sh
+++ wrappers/s2i/python/build_scripts/push_all.sh
@@ -4,7 +4,7 @@ make -C ../ push_to_dockerhub_conda_base
 # Push standard wrapper images
 make -C ../ push_to_dockerhub PYTHON_VERSION=3.6
 make -C ../ push_to_dockerhub PYTHON_VERSION=3.7.10
-make -C ../ push_to_dockerhub PYTHON_VERSION=3.8.11
+make -C ../ push_to_dockerhub PYTHON_VERSION=3.8.10
 
 # Push default tag image
 make -C ../ push_to_dockerhub_base_python PYTHON_VERSION=3.7.10
@@ -12,4 +12,4 @@ make -C ../ push_to_dockerhub_base_python PYTHON_VERSION=3.7.10
 # Push GPU images
 make -C ../ push_gpu_to_dockerhub PYTHON_VERSION=3.6
 make -C ../ push_gpu_to_dockerhub PYTHON_VERSION=3.7.10
-make -C ../ push_gpu_to_dockerhub PYTHON_VERSION=3.8.11
+make -C ../ push_gpu_to_dockerhub PYTHON_VERSION=3.8.10
