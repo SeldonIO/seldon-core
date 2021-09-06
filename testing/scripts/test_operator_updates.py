c60ec6c7c46f95c660a8b7955506059c63c540cb
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2021-09-06 18:24:07 +0100

    extend update tests

--- testing/scripts/test_operator_updates.py
+++ testing/scripts/test_operator_updates.py
@@ -8,7 +8,16 @@ from seldon_e2e_utils import (
     wait_for_status,
 )
 
-SELDON_VERSIONS_TO_TEST = ["1.3.0", "1.4.0", "1.5.0", "1.6.0", "1.7.0"]
+SELDON_VERSIONS_TO_TEST = [
+    "1.3.0",
+    "1.4.0",
+    "1.5.0",
+    "1.6.0",
+    "1.7.0",
+    "1.8.0",
+    "1.9.0",
+    "1.10.0",
+]
 
 
 @pytest.mark.sequential
