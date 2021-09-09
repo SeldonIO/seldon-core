7661f4d21988b34fc719121c0dde0e6c50fd7c0e
Clive Cox <cc@seldon.io>
2021-09-09 09:45:30 +0100

    Add missing utils files

---
+++ operator/utils/k8s/utils.go
@@ -0,0 +1,36 @@
+package k8s
+
+import (
+	"fmt"
+	"github.com/go-logr/logr"
+	"k8s.io/client-go/discovery"
+	"strconv"
+	"strings"
+)
+
+func GetServerVersion(client discovery.DiscoveryInterface, logger logr.Logger) (string, error) {
+	serverVersion, err := client.ServerVersion()
+	if err != nil {
+		return "", err
+	}
+	logger.Info("Server version", "Major", serverVersion.Major, "Minor", serverVersion.Minor)
+	majorVersion, err := strconv.Atoi(serverVersion.Major)
+	if err != nil {
+		logger.Error(err, "Failed to parse majorVersion defaulting to 1")
+		majorVersion = 1
+	}
+	minorVersion, err := strconv.Atoi(serverVersion.Minor)
+	if err != nil {
+		if strings.HasSuffix(serverVersion.Minor, "+") {
+			minorVersion, err = strconv.Atoi(serverVersion.Minor[0 : len(serverVersion.Minor)-1])
+			if err != nil {
+				logger.Error(err, "Failed to parse minorVersion defaulting to 12")
+				minorVersion = 12
+			}
+		} else {
+			logger.Error(err, "Failed to parse minorVersion defaulting to 12")
+			minorVersion = 12
+		}
+	}
+	return fmt.Sprintf("%d.%d", majorVersion, minorVersion), nil
+}
