95485d900c558f284d1b6840cbfef30d58fb6274
Michael Meredith <mikemeredith63@gmail.com>
2021-11-09 21:31:24 -0500

    update split_resources python file

--- operator/helm/create_templates.sh
+++ operator/helm/create_templates.sh
@@ -6,7 +6,7 @@ rm -rf tmp
 mkdir tmp
 cd tmp
 kustomize build ../../config/$1 > tt.yaml
-csplit --suppress-matched   tt.yaml "/^---/" "{*}"
+gcsplit --suppress-matched   tt.yaml "/^---/" "{*}"
 # manually add secret file from cert kustomize option
 cp ../../config/cert/secret.yaml xxSecret
-python ../split_resources.py --folder ../../../helm-charts/seldon-core-operator/templates
+python3 ../split_resources.py --folder ../../../helm-charts/seldon-core-operator/templates
