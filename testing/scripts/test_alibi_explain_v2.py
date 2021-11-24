7f46879d236006142f70383916cb08778692af69
Sherif Akoush <sa@seldon.io>
2021-11-24 14:36:23 +0000

    add a test for tf/triton v2 explain

--- testing/scripts/test_alibi_explain_v2.py
+++ testing/scripts/test_alibi_explain_v2.py
@@ -11,11 +11,19 @@ from seldon_e2e_utils import retry_run, wait_for_deployment
 
 # NOTE:
 # to recreate the artifacts for these test:
+# anchor tabular:
 # 1. python components/alibi-explain-server/tests/make_test_models.py
 # --model anchor_tabular
 # --model_dir <dir_name>
 # 2. upload <dir_name> to gs
 # 3. patch ../resources/iris_anchor_tabular_explainer_v2.yaml to reflect change
+#
+# anchor image:
+# 1. python components/alibi-explain-server/tests/make_test_models.py
+# --model anchor_image
+# --model_dir <dir_name>
+# 2. upload <dir_name> to gs
+# 3. patch ../resources/tf_cifar_anchor_image_explainer_v2.yaml to reflect change
 
 AFTER_WAIT_SLEEP = 20
 TENACITY_WAIT = 10
@@ -65,6 +73,52 @@ class TestExplainV2Server:
                 explanation = json.loads(r.json()["outputs"][0]["data"])
 
         assert explanation["meta"]["name"] == "AnchorTabular"
+        assert "anchor" in explanation["data"]
+        assert "precision" in explanation["data"]
+        assert "coverage" in explanation["data"]
+
+        run(f"kubectl delete -f {spec} -n {namespace}", shell=True)
+
+    @pytest.mark.sequential
+    def test_alibi_explain_anchor_image_triton(self, namespace):
+        spec = "../resources/tf_cifar_anchor_image_explainer_v2.yaml"
+        name = "cifar10-default-explainer"
+        vs_prefix = (
+            f"seldon/{namespace}/cifar10-explainer/default/v2/models/"
+            f"cifar10-default-explainer/infer"
+        )
+
+        test_data = np.random.randn(32, 32, 3)
+        inference_request = {
+            "parameters": {"content_type": "np"},
+            "inputs": [
+                {
+                    "name": "explain",
+                    "shape": test_data.shape,
+                    "datatype": "FP32",
+                    "data": test_data.tolist(),
+                    "parameters": {"content_type": "np"},
+                },
+            ],
+        }
+
+        retry_run(f"kubectl apply -f {spec} -n {namespace}")
+
+        wait_for_deployment(name, namespace)
+
+        for attempt in Retrying(
+            wait=wait_fixed(TENACITY_WAIT),
+            stop=stop_after_attempt(TENACITY_STOP_AFTER_ATTEMPT),
+        ):
+            with attempt:
+                r = requests.post(
+                    f"http://localhost:8004/{vs_prefix}",
+                    json=inference_request,
+                )
+                # note: explanation will come back in v2 as a nested json dictionary
+                explanation = json.loads(r.json()["outputs"][0]["data"])
+
+        assert explanation["meta"]["name"] == "AnchorImage"
         assert "anchor" in explanation["data"]
         assert "precision" in explanation["data"]
         assert "coverage" in explanation["data"]
