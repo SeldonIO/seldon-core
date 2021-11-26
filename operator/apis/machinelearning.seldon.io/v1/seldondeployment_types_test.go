658f3bd41e7cb7b3f9b8c81f849a1a9990850162
Clive Cox <cc@seldon.io>
2021-11-26 17:03:40 +0000

    Allow deployment name as prefix when default name to long

--- operator/apis/machinelearning.seldon.io/v1/seldondeployment_types_test.go
+++ operator/apis/machinelearning.seldon.io/v1/seldondeployment_types_test.go
@@ -92,3 +92,88 @@ func TestGetDeploymentNameLong(t *testing.T) {
 	g.Expect(len(parts)).To(Equal(2))
 	g.Expect(parts[0]).To(Equal(DeploymentNamePrefix))
 }
+
+func TestGetDeploymentNames(t *testing.T) {
+	g := NewGomegaWithT(t)
+	type test struct {
+		name                      string
+		envDeploymentNameAsPrefix string
+		sdepName                  string
+		predictorName             string
+		modelName                 string
+		expected                  string
+	}
+
+	tests := []test{
+		{
+			name:                      "simple",
+			envDeploymentNameAsPrefix: "false",
+			sdepName:                  "s",
+			predictorName:             "p",
+			modelName:                 "m",
+			expected:                  "s-p-0-m",
+		},
+		{
+			name:                      "depAsPrefixSimple",
+			envDeploymentNameAsPrefix: "true",
+			sdepName:                  "s",
+			predictorName:             "p",
+			modelName:                 "m",
+			expected:                  "s-p-0-m",
+		},
+		{
+			name:                      "longName",
+			envDeploymentNameAsPrefix: "false",
+			sdepName:                  "s",
+			predictorName:             "C12345678912345678901234567890123456789012345678901234567890",
+			modelName:                 "m",
+			expected:                  "seldon-10eb0f02cdc303e74043e57c1d8147ab",
+		},
+		{
+			name:                      "longName",
+			envDeploymentNameAsPrefix: "true",
+			sdepName:                  "s",
+			predictorName:             "C12345678912345678901234567890123456789012345678901234567890",
+			modelName:                 "m",
+			expected:                  "s-10eb0f02cdc303e74043e57c1d8147ab",
+		},
+	}
+	resetState := func() { envDeploymentNameAsPrefix = "false" }
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Cleanup(resetState)
+			envDeploymentNameAsPrefix = test.envDeploymentNameAsPrefix
+			mlDep := &SeldonDeployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      test.sdepName,
+					Namespace: "default",
+				},
+				Spec: SeldonDeploymentSpec{
+					Protocol: "abc",
+					Predictors: []PredictorSpec{
+						{
+							Name: test.predictorName,
+							ComponentSpecs: []*SeldonPodSpec{
+								{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											{
+												Image: "seldonio/mock_classifier:1.0",
+												Name:  test.modelName,
+											},
+										},
+									},
+								},
+							},
+							Graph: PredictiveUnit{
+								Name: test.modelName,
+							},
+						},
+					},
+				},
+			}
+			name := GetDeploymentName(mlDep, mlDep.Spec.Predictors[0], mlDep.Spec.Predictors[0].ComponentSpecs[0], 0)
+			g.Expect(name).To(Equal(test.expected))
+		})
+	}
+}
