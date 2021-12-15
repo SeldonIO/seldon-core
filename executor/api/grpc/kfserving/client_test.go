aab0aa8682e23aa082ce79188c7484a34ad324fd
Adrian Gonzalez-Martin <agm@seldon.io>
2021-12-15 10:21:39 +0100

    Add model_name when chaining requests

---
+++ executor/api/grpc/kfserving/client_test.go
@@ -0,0 +1,95 @@
+package kfserving
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/seldonio/seldon-core/executor/api/grpc/kfserving/inference"
+	"github.com/seldonio/seldon-core/executor/api/payload"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func TestChain(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	var prevModelName = "foo"
+	var nextModelName = "bar"
+
+	tests := []struct {
+		name     string
+		msg      payload.SeldonPayload
+		expected payload.SeldonPayload
+	}{
+		{
+			name: "request should be returned as-is",
+			msg: &payload.ProtoPayload{
+				Msg: &inference.ModelInferRequest{
+					ModelName: prevModelName,
+					Inputs: []*inference.ModelInferRequest_InferInputTensor{
+						{
+							Name:     "input-1",
+							Datatype: "INT32",
+							Shape:    []int64{1},
+							Contents: &inference.InferTensorContents{IntContents: []int32{1}},
+						},
+					},
+				},
+			},
+			expected: &payload.ProtoPayload{
+				Msg: &inference.ModelInferRequest{
+					ModelName: prevModelName,
+					Inputs: []*inference.ModelInferRequest_InferInputTensor{
+						{
+							Name:     "input-1",
+							Datatype: "INT32",
+							Shape:    []int64{1},
+							Contents: &inference.InferTensorContents{IntContents: []int32{1}},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "response should be chained",
+			msg: &payload.ProtoPayload{
+				Msg: &inference.ModelInferResponse{
+					ModelName: prevModelName,
+					Outputs: []*inference.ModelInferResponse_InferOutputTensor{
+						{
+							Name:     "input-1",
+							Datatype: "INT32",
+							Shape:    []int64{1},
+							Contents: &inference.InferTensorContents{IntContents: []int32{1}},
+						},
+					},
+				},
+			},
+			expected: &payload.ProtoPayload{
+				Msg: &inference.ModelInferRequest{
+					ModelName: nextModelName,
+					Inputs: []*inference.ModelInferRequest_InferInputTensor{
+						{
+							Name:     "input-1",
+							Datatype: "INT32",
+							Shape:    []int64{1},
+							Contents: &inference.InferTensorContents{IntContents: []int32{1}},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client := &KFServingGrpcClient{Log: logf.Log.WithName("SeldonGrpcClient")}
+			ctx := context.Background()
+
+			chained, err := client.Chain(ctx, nextModelName, tt.msg)
+			g.Expect(err).Should(BeNil())
+			g.Expect(chained).Should(Equal(tt.expected))
+		})
+	}
+}
