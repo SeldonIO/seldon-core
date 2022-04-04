0ba7b94c4637658aad3d3f9db2892e43d86a08de
Alejandro Saucedo <axsauze@gmail.com>
2022-04-04 12:21:12 +0100

    Fixed Flask breaking version by werkzeug dependency limit to 2.1 (#4018)
    * Fixed Flask breaking version by werkzeug dependency limit to 2.1
    
    * Added update to fix broken alibi detect tests

--- python/setup.py
+++ python/setup.py
@@ -37,6 +37,8 @@ setup(
         "gunicorn >= 19.9.0, < 20.2.0",
         "setuptools >= 41.0.0",
         "prometheus_client >= 0.7.1, < 0.9.0",
+        # Addresses https://github.com/SeldonIO/seldon-core/issues/4017
+        "werkzeug < 2.1",
         # Addresses CVE-2020-1971
         "cryptography >= 3.4, < 3.5",
         # Addresses CVE SNYK-PYTHON-PYYAML-590151
