e02664fe17bb234efa8446577056b2cc7229058a
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2022-04-07 18:04:49 +0100

    finish off new example

--- examples/models/metrics/get-metrics.sh
+++ examples/models/metrics/get-metrics.sh
@@ -1,4 +1,4 @@
-QUERY='query=seldon_api_executor_client_requests_seconds_count{deployment_name=~"echo",namespace=~"seldon",method=~"unary"}'
+QUERY='query=mygauge{deployment_name=~"echo",namespace=~"seldon"}'
 QUERY_URL=http://seldon-monitoring-prometheus.seldon-system.svc.cluster.local:9090/api/v1/query
 
 kubectl run --quiet=true -it --rm curlmetrics --image=radial/busyboxplus:curl --restart=Never -- \
