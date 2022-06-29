124d68b69358f75e52c6e29da7063136fa7c1815
Zhuyi Xue <zhuyi.xue@alum.utoronto.ca>
2022-06-29 16:44:32 -0700

    typo fix in logging number of gRPC workers

--- python/seldon_core/microservice.py
+++ python/seldon_core/microservice.py
@@ -530,7 +530,7 @@ def main():
         with _reserve_grpc_port() as bind_port:
             bind_address = "0.0.0.0:{}".format(bind_port)
             logger.info(
-                f"GRPC Server Binding to '%s' {bind_address} with {args.workers} processes"
+                f"GRPC Server Binding to '%s' {bind_address} with {args.grpc_workers} processes."
             )
             sys.stdout.flush()
             workers = []
