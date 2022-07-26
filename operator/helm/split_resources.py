fa5bcebd8280ab5329fec2b154953274e85250b8
Alejandro Saucedo <axsauze@gmail.com>
2022-07-26 14:33:30 +0100

    Fixed trailing dash created from helm split resources

--- operator/helm/split_resources.py
+++ operator/helm/split_resources.py
@@ -18,9 +18,9 @@ HELM_CONTROLLERID_IF_START = "{{- if .Values.controllerId }}\n"
 HELM_NOT_CONTROLLERID_IF_START = "{{- if not .Values.controllerId }}\n"
 HELM_RBAC_IF_START = "{{- if .Values.rbac.create }}\n"
 HELM_RBAC_CSS_IF_START = "{{- if .Values.rbac.configmap.create }}\n"
-HELM_SA_IF_START = "{{- if .Values.serviceAccount.create -}}\n"
-HELM_CERTMANAGER_IF_START = "{{- if .Values.certManager.enabled -}}\n"
-HELM_NOT_CERTMANAGER_IF_START = "{{- if not .Values.certManager.enabled -}}\n"
+HELM_SA_IF_START = "{{- if .Values.serviceAccount.create }}\n"
+HELM_CERTMANAGER_IF_START = "{{- if .Values.certManager.enabled }}\n"
+HELM_NOT_CERTMANAGER_IF_START = "{{- if not .Values.certManager.enabled }}\n"
 HELM_VERSION_IF_START = (
     '{{- if semverCompare ">=1.15.0" .Capabilities.KubeVersion.GitVersion }}\n'
 )
