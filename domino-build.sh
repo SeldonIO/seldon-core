3ba40dc67b09943b58fa2ba1564894fc9b21f4b9
David McWhorter <david.mcwhorter@dominodatalab.com>
2022-08-09 15:26:50 +0000

    add -n flag to allow option to disable push to quay

--- domino-build.sh
+++ domino-build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -o nounset -o errexit -o pipefail
 
+nopush_flag=''
+while getopts 'n' flag; do
+  case "${flag}" in
+    n) nopush_flag='true' ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+readonly nopush_flag
+
 SELDON_REPO=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cd "${SELDON_REPO}"
@@ -24,20 +33,21 @@ docker tag "seldonio/seldon-core-operator:${SOURCE_IMAGE_TAG}" "quay.io/domino/s
 echo -e "\n  Tagging executor...\n"
 docker tag "seldonio/seldon-core-executor:${SOURCE_IMAGE_TAG}" "quay.io/domino/seldon-core-executor:${TARGET_IMAGE_TAG}"
 
+if [ "${nopush_flag}" == "" ]; then
 
+  if [ "$(cat ~/.docker/config.json | jq '.auths | has("quay.io")')" = "true" ]; then
+    echo -e "[Docker is already logged into quay.io, using existing credentials.]"
+  elif [ "${QUAY_USER:-missing}" != "missing" ] && [ "${QUAY_PASSWORD:-missing}" != "missing" ]; then
+    echo "$QUAY_PASSWORD" | docker login -u "$QUAY_USER" --password-stdin quay.io
+  else
+    echo -e "[Push to quay.io requires docker login, either run 'docker login quay.io' or set QUAY_USER and QUAY_PASSWORD before running this script.]"
+    exit 1
+  fi
 
+  echo -e "\n  Pushing operator...\n"
+  docker push "quay.io/domino/seldon-core-operator:${TARGET_IMAGE_TAG}"
 
-if [ "$(cat ~/.docker/config.json | jq '.auths | has("quay.io")')" = "true" ]; then
-  echo -e "[Docker is already logged into quay.io, using existing credentials.]"
-elif [ "${QUAY_USER:-missing}" != "missing" ] && [ "${QUAY_PASSWORD:-missing}" != "missing" ]; then
-  echo "$QUAY_PASSWORD" | docker login -u "$QUAY_USER" --password-stdin quay.io
-else
-  echo -e "[Push to quay.io requires docker login, either run 'docker login quay.io' or set QUAY_USER and QUAY_PASSWORD before running this script.]"
-  exit 1
+  echo -e "\n  Pushing executor...\n"
+  docker push "quay.io/domino/seldon-core-executor:${TARGET_IMAGE_TAG}"
+
 fi
-
-echo -e "\n  Pushing operator...\n"
-docker push "quay.io/domino/seldon-core-operator:${TARGET_IMAGE_TAG}"
-
-echo -e "\n  Pushing executor...\n"
-docker push "quay.io/domino/seldon-core-executor:${TARGET_IMAGE_TAG}"
