1db82bfd04850300aef5e40c691a9a8967ce3bba
Yiqiu Huang <yiqiu.huang@me.com>
2022-10-24 23:32:16 -0400

    Fixed explainer to handle istio hosts annotation

--- operator/controllers/seldondeployment_explainers.go
+++ operator/controllers/seldondeployment_explainers.go
@@ -324,7 +324,7 @@ func createExplainerIstioResources(pSvcName string, p *machinelearningv1.Predict
 			Namespace: namespace,
 		},
 		Spec: istio_networking.VirtualService{
-			Hosts:    []string{"*"},
+			Hosts:    []string{utils2.GetAnnotation(mlDep, ANNOTATION_ISTIO_HOST, "*")},
 			Gateways: []string{utils2.GetAnnotation(mlDep, ANNOTATION_ISTIO_GATEWAY, istio_gateway)},
 			Http: []*istio_networking.HTTPRoute{
 				{
@@ -345,7 +345,7 @@ func createExplainerIstioResources(pSvcName string, p *machinelearningv1.Predict
 			Namespace: namespace,
 		},
 		Spec: istio_networking.VirtualService{
-			Hosts:    []string{"*"},
+			Hosts:    []string{utils2.GetAnnotation(mlDep, ANNOTATION_ISTIO_HOST, "*")},
 			Gateways: []string{utils2.GetAnnotation(mlDep, ANNOTATION_ISTIO_GATEWAY, istio_gateway)},
 			Http: []*istio_networking.HTTPRoute{
 				{
