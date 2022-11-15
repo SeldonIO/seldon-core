a7c16a63855e14ac276c37400819380ca78e488e
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-11-15 00:42:33 +0000

    Add shell script to run Markdown transformations via sed

---
+++ samples/markdown-transforms.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Single pass with multiple transformations for efficiency
+cat $1 \
+  | ./markdown-transforms/format-bash-blocks.sed \
+  | ./markdown-transforms/format-output-blocks.sed \
+  | ./markdown-transforms/coalesce-blank-lines.sed \
+  > $1.tmp
+
+mv $1.tmp $1
