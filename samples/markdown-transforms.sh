f24fa0fca3a66a999eba936a0b2ee629c663ce24
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-11-16 00:33:43 +0000

    Use trailing whitespace removal script in Markdown transforms

--- samples/markdown-transforms.sh
+++ samples/markdown-transforms.sh
@@ -5,6 +5,7 @@ cat $1 \
   | ./markdown-transforms/format-bash-blocks.sed \
   | ./markdown-transforms/format-output-blocks.sed \
   | ./markdown-transforms/coalesce-blank-lines.sed \
+  | ./markdown-transforms/remove-trailing-whitespace.sed \
   > $1.tmp
 
 mv $1.tmp $1
