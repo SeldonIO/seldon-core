30f19403c9fb6494861303ab60f6a843b10a78bc
Adrian Gonzalez-Martin <agm@seldon.io>
2022-11-23 18:42:55 +0000

    Add some debug info

--- testing/scripts/test_alibi_explain_v2.py
+++ testing/scripts/test_alibi_explain_v2.py
@@ -116,6 +116,8 @@ class TestExplainV2Server:
                     json=inference_request,
                 )
                 # note: explanation will come back in v2 as a nested json dictionary
+                print(r.text)
+                print(r.json())
                 explanation = json.loads(r.json()["outputs"][0]["data"][0])
 
         assert explanation["meta"]["name"] == "AnchorImage"
