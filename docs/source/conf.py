0262b56eee8a5fe831b3042c2890f5fc718fbbbd
RafalSkolasinski <r.j.skolasinski@gmail.com>
2022-12-02 11:45:13 +0000

    Remove versions dropdown and align top bar with SCv1 docs (#665)
    * remove versions dropdown and align top bar with V1 docs
    
    * Use consistent capitalisation of v2 in SCv2 name
    
    * Update author to be company not individual
    
    * Remove extraneous config
    
    * Set nav title explicitly
    
    * Set base URL for docs as per SCv1
    
    * Update title header on landing page
    
    Co-authored-by: Alex Rakowski <20504869+agrski@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,10 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Seldon Core V2'
+project = 'Seldon Core v2'
 copyright = '2021-2022 Seldon Technologies Ltd.'
-author = 'smv'
-
+author = "Seldon Technologies Ltd"
 
 # -- General configuration ---------------------------------------------------
 
@@ -52,12 +51,6 @@ myst_enable_extensions = [
     "tasklist",
 ]
 
-myst_substitutions = {
-    'seldon-deploy-version': '1.4.1',
-    'seldon-core-artifact-version': '1.13.1',
-}
-
-
 source_suffix = ['.rst', '.md']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -69,7 +62,7 @@ exclude_patterns = []
 
 notfound_context = {
     'title': 'Not found',
-    'body': '<h1>Sorry. Seldon Core V2 page was not found!</h1><br><h4>Please use the search bar above to find what you were looking for.</h4>',
+    'body': '<h1>Sorry. Seldon Core v2 page was not found!</h1><br><h4>Please use the search bar above to find what you were looking for.</h4>',
 }
 # -- Options for HTML output -------------------------------------------------
 
@@ -79,28 +72,28 @@ notfound_context = {
 html_theme = 'sphinx_material'
 
 # Set link name generated in the top bar.
-html_title = 'Seldon Core V2'
+html_title = 'Seldon Core v2'
 
 # Material theme options (see theme.conf for more information)
 html_theme_options = {
 
     # Set the name of the project to appear in the navigation.
-    # 'nav_title': '',
+    'nav_title': 'Seldon Core v2',
 
     # Set you GA account ID to enable tracking
     'google_analytics_account': 'UA-54780881-6',
 
     # Specify a base_url used to generate sitemap.xml. If not
     # specified, then no sitemap will be built.
-    # 'base_url': 'https://project.github.io/project',
+    "base_url": "https://docs.seldon.io/projects/seldon-core/",
 
     # Set the color and the accent color
     'color_primary': 'indigo',
     'color_accent': 'teal',
 
     # Set the repo location to get a badge with stats
-    # 'repo_url': 'https://github.com/project/project/',
-    # 'repo_name': 'Project',
+    "repo_url": "https://github.com/SeldonIO/seldon-core/",
+    'repo_name': 'Seldon Core',
 
     # Visible levels of the global TOC; -1 means unlimited
     'globaltoc_depth': 4,
@@ -112,15 +105,49 @@ html_theme_options = {
     # "logo_icon": "&#xe869",
 
     "nav_links": [
-        {"href": "contents/getting-started/index",
-            "internal": True, "title": "Getting Started"},
+        {
+            "href": "/",
+            "internal": False,
+            "title": "🚀 Our Other Projects & Products:",
+        },
+        {
+            "href": "https://docs.seldon.io/projects/alibi/en/stable/",
+            "internal": False,
+            "title": "Alibi Explain",
+        },
+        {
+            "href": "https://docs.seldon.io/projects/alibi-detect/en/stable/",
+            "internal": False,
+            "title": "Alibi Detect",
+        },
+        {
+            "href": "https://mlserver.readthedocs.io/en/latest/",
+            "internal": False,
+            "title": "MLServer",
+        },
+        {
+            "href": "https://tempo.readthedocs.io/en/latest/",
+            "internal": False,
+            "title": "Tempo SDK",
+        },
+        {
+            "href": "https://deploy.seldon.io",
+            "internal": False,
+            "title": "Seldon Deploy (Enterprise)",
+        },
+        {
+            "href": "https://github.com/SeldonIO/seldon-deploy-sdk#seldon-deploy-sdk",
+            "internal": False,
+            "title": "Seldon Deploy SDK (Enterprise)",
+        },
     ],
 
     "heroes": {
-        "index": "Documentation master"
+        "index": "Seldon Core v2 Documentation"
     },
 
-    "version_dropdown": True,
+    "version_dropdown": False,
+    "master_doc": False,
     "version_json": "_static/versions.json"
 }
 
@@ -138,19 +165,15 @@ html_static_path = ['_static']
 
 # Hack to make substitutions work in code-blocks https://github.com/sphinx-doc/sphinx/issues/4054#issuecomment-329097229
 
-
 def ultimateReplace(app, docname, source):
     result = source[0]
     for key in app.config.ultimate_replacements:
         result = result.replace(key, app.config.ultimate_replacements[key])
     source[0] = result
 
-
 ultimate_replacements = {
-    "{{ seldon-deploy-version }}": myst_substitutions["seldon-deploy-version"],
-    "{{ seldon-core-artifact-version }}": myst_substitutions["seldon-core-artifact-version"],
+    # "{{ seldon-core-artifact-version }}": myst_substitutions["seldon-core-artifact-version"],
 }
-
 
 def setup(app):
     app.add_config_value('ultimate_replacements', {}, True)
