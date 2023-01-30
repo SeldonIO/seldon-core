82329de8bb934624d24939df3ab25bc550dc798e
Sherif Akoush <sa@seldon.io>
2023-01-30 14:34:58 +0000

    fix example

--- tests/k6/notebooks/agent/agent_test_runner.sh
+++ tests/k6/notebooks/agent/agent_test_runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NUM_MODELS=1
-NUM_ITERS=5
+NUM_MODELS="1,100"
+NUM_ITERS=500
 NUM_VUS=5
-MODEL_NAME="mlflow_wine" # check model.js
+MODEL_NAME="mlflow_wine,tfsimple" # check model.js
 EXTRA="--out influxdb=http://localhost:8086/k6db ../../scenarios/infer_constant_vu.js"
 DIR="results"
 
@@ -13,17 +13,19 @@ mkdir -p $DIR
 export SCHEDULER_ENDPOINT=0.0.0.0:9004 
 export INFER_GRPC_ENDPOINT=0.0.0.0:9000
 export INFER_HTTP_ENDPOINT=http://0.0.0.0:9000
-export INFER_HTTP_ITERATIONS=10 
-export INFER_GRPC_ITERATIONS=10 
+export INFER_HTTP_ITERATIONS=1 
+export INFER_GRPC_ITERATIONS=1 
 export MODEL_TYPE=$MODEL_NAME
 export MAX_NUM_MODELS=$NUM_MODELS
 export SCHEDULER_PROXY="false"
 export ENVOY="true"
-export INFER_BATCH_SIZE=1
+export INFER_BATCH_SIZE="1,1"
+export MODELNAME_PREFIX="mlflow_winea,tfsimplea"
+export MODEL_MEMORY_BYTES="400,500"
 #export DATAFLOW_TAG="pipeline"  # "model" or "pipeline" or "", "pipeline" would trigger kstreams
 #export SKIP_UNLOAD_MODEL=1
 
-k6 run -u $NUM_VUS -i $NUM_ITERS --summary-export $DIR/base.json --out csv=$DIR/base.gz $EXTRA
+k6 run --http-debug="full" -u $NUM_VUS -i $NUM_ITERS --summary-export $DIR/base.json --out csv=$DIR/base.gz $EXTRA
 
 
 #export INFER_GRPC_ENDPOINT=0.0.0.0:9998 
