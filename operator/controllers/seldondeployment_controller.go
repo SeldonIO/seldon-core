e77b628d87da6e0c23016cc263e27f1fe4f3d9f6
Clive Cox <cc@seldon.io>
2023-02-08 18:33:21 +0000

    Remove HPA updates and fix webhooks

--- operator/controllers/seldondeployment_controller.go
+++ operator/controllers/seldondeployment_controller.go
@@ -59,7 +59,7 @@ import (
 	istio_networking "istio.io/api/networking/v1alpha3"
 	istio "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	appsv1 "k8s.io/api/apps/v1"
-	autoscaling "k8s.io/api/autoscaling/v2"
+	autoscaling "k8s.io/api/autoscaling/v2beta1"
 	corev1 "k8s.io/api/core/v1"
 	policy "k8s.io/api/policy/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
--- operator/controllers/seldondeployment_controller_test.go
+++ operator/controllers/seldondeployment_controller_test.go
@@ -29,7 +29,7 @@ import (
 	"github.com/seldonio/seldon-core/operator/constants"
 	testutils "github.com/seldonio/seldon-core/operator/controllers/testing"
 	appsv1 "k8s.io/api/apps/v1"
-	autoscaling "k8s.io/api/autoscaling/v2"
+	autoscaling "k8s.io/api/autoscaling/v2beta1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -667,11 +667,8 @@ var _ = Describe("Create a Seldon Deployment with hpa", func() {
 										{
 											Type: autoscaling.ResourceMetricSourceType,
 											Resource: &autoscaling.ResourceMetricSource{
-												Name: v1.ResourceCPU,
-												Target: autoscaling.MetricTarget{
-													Type:               autoscaling.UtilizationMetricType,
-													AverageUtilization: &utilization,
-												},
+												Name:                     v1.ResourceCPU,
+												TargetAverageUtilization: &utilization,
 											},
 										},
 									},
