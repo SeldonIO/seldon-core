c78b57b6ff97016adea4013cfc20efa75267bcb2
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-04-05 22:14:54 +0100

    Refactor per-data type conversion to bytes to extension methods
    This has two key benefits.
    Firstly, the same conversion logic was used for both requests and responses.
    Thus, we are removing duplication and minimising the risk of differences creeping in.
    Secondly, the business is easier to read, as the handling is moved outside.

--- scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/kafka/protocol.kt
+++ scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/kafka/protocol.kt
@@ -18,6 +18,7 @@ package io.seldon.dataflow.kafka
 
 import com.google.protobuf.kotlin.toByteString
 import io.seldon.mlops.inference.v2.V2Dataplane
+import io.seldon.mlops.inference.v2.V2Dataplane.InferTensorContents
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -25,129 +26,152 @@ enum class DataType {
     BOOL, UINT8, UINT16, UINT32, UINT64, INT8, INT16, INT32, INT64, FP16, FP32, FP64, BYTES
 }
 
+fun InferTensorContents.toUint8Bytes(): ByteArray = this.uintContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(1)
+            .put(it.toByte())
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toUint16Bytes(): ByteArray = this.uintContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(UShort.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putShort(it.toShort())
+            .array()
+            .toList()
+    }.toByteArray()
+
+fun InferTensorContents.toUint32Bytes(): ByteArray = this.uintContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(UInt.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putInt(it)
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toUint64Bytes(): ByteArray = this.uint64ContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(ULong.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putLong(it)
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toInt8Bytes(): ByteArray = this.intContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(1)
+            .put(it.toByte())
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toInt16Bytes(): ByteArray = this.intContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(Short.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putShort(it.toShort())
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toInt32Bytes(): ByteArray = this.intContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(Int.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putInt(it)
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toInt64Bytes(): ByteArray = this.int64ContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(Long.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putLong(it)
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toFp32Bytes(): ByteArray = this.fp32ContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(Float.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putFloat(it)
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toFp64Bytes(): ByteArray = this.fp64ContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(Double.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putDouble(it)
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toBoolBytes(): ByteArray = this.boolContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(1)
+            .put(if (it) 1 else 0)
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
+fun InferTensorContents.toRawBytes(): ByteArray = this.bytesContentsList
+    .flatMap {
+        ByteBuffer
+            .allocate(it.size() + Int.SIZE_BYTES)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putInt(it.size())
+            .put(it.toByteArray())
+            .array()
+            .toList()
+    }
+    .toByteArray()
+
 fun convertRequestToRawInputContents(request: V2Dataplane.ModelInferRequest): V2Dataplane.ModelInferRequest {
     val builder = request.toBuilder()
     request.inputsList.forEachIndexed { idx, input ->
         val v = when (DataType.valueOf(input.datatype)) {
-            DataType.UINT8 -> {
-                input.contents.uintContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(1)
-                        .put(it.toByte())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.UINT16 -> {
-                input.contents.uintContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(UShort.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putShort(it.toShort())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.UINT32 -> {
-                input.contents.uintContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(UInt.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putInt(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.UINT64 -> {
-                input.contents.uint64ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(ULong.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putLong(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT8 -> {
-                input.contents.intContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(1)
-                        .put(it.toByte())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT16 -> {
-                input.contents.intContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Short.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putShort(it.toShort())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT32 -> {
-                input.contents.intContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Int.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putInt(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT64 -> {
-                input.contents.int64ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Long.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putLong(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.BOOL -> {
-                input.contents.boolContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(1)
-                        .put(if (it) 1 else 0)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
+            DataType.UINT8 -> input.contents.toUint8Bytes()
+            DataType.UINT16 -> input.contents.toUint16Bytes()
+            DataType.UINT32 -> input.contents.toUint32Bytes()
+            DataType.UINT64 -> input.contents.toUint64Bytes()
+            DataType.INT8 -> input.contents.toInt8Bytes()
+            DataType.INT16 -> input.contents.toInt16Bytes()
+            DataType.INT32 -> input.contents.toInt32Bytes()
+            DataType.INT64 -> input.contents.toInt64Bytes()
+            DataType.BOOL -> input.contents.toBoolBytes()
             DataType.FP16, // may need to handle this separately in future
-            DataType.FP32 -> {
-                input.contents.fp32ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Float.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putFloat(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.FP64 -> {
-                input.contents.fp64ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Double.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putDouble(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.BYTES -> {
-                input.contents.bytesContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(it.size() + Int.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putInt(it.size())
-                        .put(it.toByteArray())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
+            DataType.FP32 -> input.contents.toFp32Bytes()
+            DataType.FP64 -> input.contents.toFp64Bytes()
+            DataType.BYTES -> input.contents.toRawBytes()
         }
         // Add raw contents
         builder.addRawInputContents(v.toByteString())
@@ -161,123 +185,19 @@ fun convertResponseToRawOutputContents(request: V2Dataplane.ModelInferResponse):
     val builder = request.toBuilder()
     request.outputsList.forEachIndexed { idx, output ->
         val v = when (DataType.valueOf(output.datatype)) {
-            DataType.UINT8 -> {
-                output.contents.uintContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(1)
-                        .put(it.toByte())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.UINT16 -> {
-                output.contents.uintContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(UShort.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putShort(it.toShort())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.UINT32 -> {
-                output.contents.uintContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(UInt.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putInt(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.UINT64 -> {
-                output.contents.uint64ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(ULong.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putLong(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT8 -> {
-                output.contents.intContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(1)
-                        .put(it.toByte())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT16 -> {
-                output.contents.intContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Short.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putShort(it.toShort())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT32 -> {
-                output.contents.intContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Int.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putInt(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.INT64 -> {
-                output.contents.int64ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Long.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putLong(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.BOOL -> {
-                output.contents.boolContentsList.flatMap {
-                    ByteBuffer.allocate(1)
-                        .put(if (it) 1 else 0)
-                        .array().toList()
-                }.toByteArray()
-            }
+            DataType.UINT8 -> output.contents.toUint8Bytes()
+            DataType.UINT16 -> output.contents.toUint16Bytes()
+            DataType.UINT32 -> output.contents.toUint32Bytes()
+            DataType.UINT64 -> output.contents.toUint64Bytes()
+            DataType.INT8 -> output.contents.toInt8Bytes()
+            DataType.INT16 -> output.contents.toInt16Bytes()
+            DataType.INT32 -> output.contents.toInt32Bytes()
+            DataType.INT64 -> output.contents.toInt64Bytes()
+            DataType.BOOL -> output.contents.toBoolBytes()
             DataType.FP16, // may need to handle this separately in future
-            DataType.FP32 -> {
-                output.contents.fp32ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Float.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putFloat(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.FP64 -> {
-                output.contents.fp64ContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(Double.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putDouble(it)
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
-            DataType.BYTES -> {
-                output.contents.bytesContentsList.flatMap {
-                    ByteBuffer
-                        .allocate(it.size() + Int.SIZE_BYTES)
-                        .order(ByteOrder.LITTLE_ENDIAN)
-                        .putInt(it.size())
-                        .put(it.toByteArray())
-                        .array()
-                        .toList()
-                }.toByteArray()
-            }
+            DataType.FP32 -> output.contents.toFp32Bytes()
+            DataType.FP64 -> output.contents.toFp64Bytes()
+            DataType.BYTES -> output.contents.toRawBytes()
         }
         // Add raw contents
         builder.addRawOutputContents(v.toByteString())
