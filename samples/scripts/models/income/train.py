3de894a7338b1aeab86c3239793318c8407294ee
Michael Cheah <20640150+michaelcheah@users.noreply.github.com>
2023-04-11 17:32:54 +0100

    feat(samples): Add kernel shap explainer for income model (#4772)
    * feat(samples): Refactor and add kernel shap explainer for income training scripts
    
    * chore(samples): Ignore artifacts generated in model folders
    
    * fix(samples): recursively copy artifacts
    
    * fix(samples): fix gs bucket destination

--- samples/scripts/models/income/train.py
+++ samples/scripts/models/income/train.py
@@ -1,6 +1,7 @@
 import joblib
 import numpy as np
 from alibi.datasets import fetch_adult
+from alibi.explainers import AnchorTabular, KernelShap
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
@@ -8,10 +9,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
-def main():
-    np.random.seed(0)
-
-    # prepare data
+def fetch_data():
     adult = fetch_adult()
     data = adult.data
     target = adult.target
@@ -25,8 +23,11 @@ def main():
     # build model
     idx = 30000
     X_train, Y_train = data[:idx, :], target[:idx]
-    X_test, Y_test = data[idx + 1 :, :], target[idx + 1 :]
+    X_test, Y_test = data[idx + 1:, :], target[idx + 1:]
+    return X_train, Y_train, X_test, Y_test, feature_names, category_map
 
+
+def train_classifier(X_train, Y_train, feature_names, category_map) -> Pipeline:
     ordinal_features = [
         x for x in range(len(feature_names)) if x not in list(category_map.keys())
     ]
@@ -63,13 +64,41 @@ def main():
 
     model_pipeline.fit(X_train, Y_train)
     print("Model trained!")
+    return model_pipeline
 
-    filename_p = "model.joblib"
-    print("Saving model in %s" % filename_p)
-    joblib.dump(model_pipeline, filename_p)
-    print("Model saved!")
+
+def train_anchor_explainer(model_pipeline, X_train, feature_names, category_map) -> AnchorTabular:
+    explainer = AnchorTabular(
+        model_pipeline.predict, feature_names, categorical_names=category_map, seed=1
+    )
+
+    explainer.fit(X_train, disc_perc=(25, 50, 75))
+    print("Explainer trained!")
+    return explainer
+
+
+def train_kernel_shap_explainer(model_pipeline, X_train, feature_names, category_map) -> KernelShap:
+    explainer = KernelShap(
+        model_pipeline.predict_proba,
+        "logit",
+        feature_names,
+        categorical_names=category_map,
+        seed=1
+    )
+    explainer.fit(X_train[:100, :])
+    print("Explainer trained!")
+    return explainer
 
 
 if __name__ == "__main__":
-    print("Building income model...")
-    main()
+    print("Building income model and explainers...")
+    X_train, Y_train, X_test, Y_test, feature_names, category_map = fetch_data()
+    model_pipeline = train_classifier(X_train, Y_train, feature_names, category_map)
+    anchor_explainer = train_anchor_explainer(model_pipeline, X_train, feature_names, category_map)
+    kernel_shap_explainer = train_kernel_shap_explainer(model_pipeline, X_train, feature_names, category_map)
+
+    print("Saving models...")
+    joblib.dump(model_pipeline, "classifier/model.joblib")
+    anchor_explainer.save("explainers/anchor-explainer/data")
+    kernel_shap_explainer.save("explainers/kernel-shap-explainer/data")
+
