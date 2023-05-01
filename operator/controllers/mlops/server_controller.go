aab3f0382784596ada56500a49b8d873da90020f
Clive Cox <cc@seldon.io>
2023-05-01 16:50:53 +0100

    Surface errors during reconcilliation for Servers

--- operator/controllers/mlops/server_controller.go
+++ operator/controllers/mlops/server_controller.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/go-logr/logr"
 	"github.com/seldonio/seldon-core/operator/v2/controllers/reconcilers"
 	"github.com/seldonio/seldon-core/operator/v2/controllers/reconcilers/common"
 	"github.com/seldonio/seldon-core/operator/v2/pkg/constants"
@@ -118,11 +119,15 @@ func (r *ServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 
 	stop, err := r.handleFinalizer(ctx, server)
 	if stop {
+		if err != nil {
+			r.updateStatusFromError(ctx, logger, server, err)
+		}
 		return reconcile.Result{}, err
 	}
 
 	err = r.Scheduler.ServerNotify(ctx, server)
 	if err != nil {
+		r.updateStatusFromError(ctx, logger, server, err)
 		return reconcile.Result{}, err
 	}
 
@@ -132,17 +137,20 @@ func (r *ServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		Client: r.Client,
 	})
 	if err != nil {
+		r.updateStatusFromError(ctx, logger, server, err)
 		return reconcile.Result{}, err
 	}
 
 	// Set Controller References
 	err = setControllerReferences(server, sr.GetResources(), r.Scheme)
 	if err != nil {
+		r.updateStatusFromError(ctx, logger, server, err)
 		return reconcile.Result{}, err
 	}
 
 	err = sr.Reconcile()
 	if err != nil {
+		r.updateStatusFromError(ctx, logger, server, err)
 		return reconcile.Result{}, err
 	}
 
@@ -169,6 +177,13 @@ func serverReady(status mlopsv1alpha1.ServerStatus) bool {
 	return status.Conditions != nil &&
 		status.GetCondition(apis.ConditionReady) != nil &&
 		status.GetCondition(apis.ConditionReady).Status == v1.ConditionTrue
+}
+
+func (r *ServerReconciler) updateStatusFromError(ctx context.Context, logger logr.Logger, server *mlopsv1alpha1.Server, err error) {
+	server.Status.CreateAndSetCondition(mlopsv1alpha1.StatefulSetReady, false, err.Error())
+	if errSet := r.Status().Update(ctx, server); errSet != nil {
+		logger.Error(errSet, "Failed to set status for server on error", "server", server.Name, "error", err.Error())
+	}
 }
 
 func (r *ServerReconciler) updateStatus(server *mlopsv1alpha1.Server) error {
