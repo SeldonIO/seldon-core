1d0fc7b99736f5aec04816d55eea6ad2ac25135b
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-05-09 19:08:21 +0100

    Add enum class for supported Kafka SASL mechanisms

---
+++ scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/kafka/security/SaslMechanisms.kt
@@ -0,0 +1,12 @@
+package io.seldon.dataflow.kafka.security
+
+enum class KafkaSaslMechanisms(val mechanism: String) {
+    PLAIN("PLAIN"),
+    OAUTH_BEARER("OAUTHBEARER"),
+    SCRAM_SHA_256("SCRAM-SHA-256"),
+    SCRAM_SHA_512("SCRAM-SHA-512");
+
+    override fun toString(): String {
+        return this.mechanism
+    }
+}
