8fbe9434290073203214a9b31559d79c04838984
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-05-09 19:23:42 +0100

    Move SASL config type to Kafka security package
    We are only using SASL (and mTLS) in the context of Kafka, so should reflect this dependency and specialisation in the package structure.

--- scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/kafka/security/Sasl.kt
+++ scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/kafka/security/Sasl.kt
@@ -1,5 +1,14 @@
 package io.seldon.dataflow.kafka.security
 
+typealias FilePath = String
+
+data class SaslConfig(
+    val mechanism: String,
+    val username: String,
+    val secret: String,
+    val passwordPath: FilePath
+)
+
 enum class KafkaSaslMechanisms(val mechanism: String) {
     PLAIN("PLAIN"),
     SCRAM_SHA_256("SCRAM-SHA-256"),
