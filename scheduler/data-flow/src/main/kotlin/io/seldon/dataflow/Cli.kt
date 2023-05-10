6f85dad2ccad3c7a0557e2866501a180f4e4c917
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-05-10 15:18:45 +0100

    Change max message size CLI param to int from long

--- scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/Cli.kt
+++ scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/Cli.kt
@@ -41,7 +41,7 @@ object Cli {
     val kafkaReplicationFactor = Key("kafka.replication.factor", intType)
     val kafkaUseCleanState = Key("kafka.state.clean", booleanType)
     val kafkaJoinWindowMillis = Key("kafka.join.window.millis", longType)
-    val kafkaMaxMessageSizeBytes = Key("kafka.max.message.size.bytes", longType)
+    val kafkaMaxMessageSizeBytes = Key("kafka.max.message.size.bytes", intType)
 
     // Kafka (m)TLS
     val tlsCACertPath = Key("kafka.tls.client.ca.path", stringType)
