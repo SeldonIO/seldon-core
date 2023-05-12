5f5ea6e0cd7997f3b030ee6444e58d8ff69be70b
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-05-12 21:40:33 +0100

    Make parameter/field private

--- scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/kafka/security/Sasl.kt
+++ scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/kafka/security/Sasl.kt
@@ -9,7 +9,7 @@ data class SaslConfig(
     val passwordPath: FilePath
 )
 
-enum class KafkaSaslMechanisms(val mechanism: String) {
+enum class KafkaSaslMechanisms(private val mechanism: String) {
     PLAIN("PLAIN"),
     SCRAM_SHA_256("SCRAM-SHA-256"),
     SCRAM_SHA_512("SCRAM-SHA-512");
