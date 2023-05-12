8955b54b14a0417cd0133eda0306b279ffb7b099
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-05-12 20:56:36 +0100

    Use parameterised testing for CLI SASL mechanism parsing tests

--- scheduler/data-flow/src/test/kotlin/io/seldon/dataflow/CliTest.kt
+++ scheduler/data-flow/src/test/kotlin/io/seldon/dataflow/CliTest.kt
@@ -1,17 +1,21 @@
 package io.seldon.dataflow
 
 import io.seldon.dataflow.kafka.security.KafkaSaslMechanisms
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 import strikt.api.expectCatching
 import strikt.assertions.isEqualTo
 import strikt.assertions.isSuccess
+import java.util.stream.Stream
 
 internal class CliTest {
 
-    @Test
-    fun getSaslMechanism() {
-        val expectedMechanism = KafkaSaslMechanisms.SCRAM_SHA_512
-        val args = arrayOf("--kafka-sasl-mechanism", "SCRAM-SHA-512")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("saslMechanisms")
+    fun getSaslMechanism(input: String, expectedMechanism: KafkaSaslMechanisms) {
+        val args = arrayOf("--kafka-sasl-mechanism", input)
         val cli = Cli.configWith(args)
 
         expectCatching { cli[Cli.saslMechanism] }
@@ -19,6 +23,14 @@ internal class CliTest {
             .isEqualTo(expectedMechanism)
     }
 
-        assertEquals(expectedMechanism, actualMechanism)
+    companion object {
+        @JvmStatic
+        private fun saslMechanisms(): Stream<Arguments> {
+            return Stream.of(
+                arguments("SCRAM-SHA-512", KafkaSaslMechanisms.SCRAM_SHA_512),
+                arguments("SCRAM-SHA-256", KafkaSaslMechanisms.SCRAM_SHA_256),
+                arguments("PLAIN", KafkaSaslMechanisms.PLAIN),
+            )
+        }
     }
 }
