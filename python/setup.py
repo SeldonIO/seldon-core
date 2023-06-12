4ce9e7518a751ed215fd365d308abf5b239a6eeb
Michael Meredith <michael.meredith@capitalone.com>
2023-06-12 15:52:14 -0400

    update python microservice dependencies

--- python/setup.py
+++ python/setup.py
@@ -3,8 +3,9 @@ from itertools import chain
 
 from setuptools import find_packages, setup
 
-# Extra dependencies, with special 'all' key
-extras = {"tensorflow": ["tensorflow"]}
+# Extra dependencies, with special 'tensorflow' key. TF requires a specific versio of 
+# proto to be installed to use their protos.
+extras = {"tensorflow": ["tensorflow", "protobuf>=3.20.2,<4.0.0"]}
 all_extra_deps = chain.from_iterable(extras.values())
 extras["all"] = list(set(all_extra_deps))
 
@@ -26,7 +27,7 @@ setup(
         "Flask-cors<4.0.0",
         "requests<3.0.0",
         "numpy<2.0.0",
-        "protobuf>=3.20.2,<4.0.0",
+        "protobuf<5.0.0",
         "grpcio<2.0.0",
         "Flask-OpenTracing >= 1.1.0, < 1.2.0",
         "opentracing >= 2.2.0, < 2.5.0",
@@ -38,13 +39,13 @@ setup(
         "prometheus_client >= 0.7.1, < 0.9.0",
         "werkzeug >= 2.1.1, < 2.3",
         # Addresses CVE SNYK-PYTHON-CRYPTOGRAPHY-3315328
-        "cryptography >= 39.0.1, < 40.1",
+        "cryptography >= 39.0.1,<41.0",
         # Addresses CVE SNYK-PYTHON-PYYAML-590151
         "PyYAML >= 5.4, <= 6.0.1",
         # Addresses CVE PRISMA-2021-0020
         "click >= 8.0.0a1, < 8.1",
         # Addresses CVE CVE-2019-11236 and CVE-2020-26137 and SNYK-PYTHON-URLLIB3-1533435
-        "urllib3 >= 1.26.5, < 1.27",
+        "urllib3 >= 1.26.5,<3.0.0",
         "importlib-metadata<5; python_version < '3.8'",
     ],
     extras_require=extras,
