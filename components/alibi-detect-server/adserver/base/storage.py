99ffda257b63fc3a527eef3db47f9ca151f614d2
Adrian Gonzalez-Martin <agm@seldon.io>
2023-07-04 12:38:37 +0100

    Fix Alibi CVEs (#4960)

--- components/alibi-detect-server/adserver/base/storage.py
+++ components/alibi-detect-server/adserver/base/storage.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import kfserving
 import logging
 import tempfile
 from distutils.util import strtobool
@@ -15,23 +14,6 @@ except ImportError:
         "rclone-based storage funcionality not available without rclone binary"
     )
     rclone = None
-
-
-def _rclone_enabled():
-    # IF RCLONE_ENABLED variable set explicitly we read from it
-    enabled = os.environ.get("RCLONE_ENABLED", None)
-    if enabled is not None:
-        return strtobool(enabled)
-
-    # Otherwise we determine if Rclone config is provided
-    for key in os.environ.keys():
-        if "RCLONE_CONFIG" in key:
-            return True
-    else:
-        return False
-
-
-RCLONE_ENABLED = _rclone_enabled()
 
 
 class Rclone:
@@ -57,7 +39,4 @@ class Rclone:
 
 
 def download_model(storage_uri) -> str:
-    if RCLONE_ENABLED:
-        return Rclone().copy(storage_uri)
-    else:
-        return kfserving.Storage.download(storage_uri)
+    return Rclone().copy(storage_uri)
---
+++ components/alibi-detect-server/adserver/tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+@pytest.fixture(autouse=True)
+def rclone(monkeypatch):
+    monkeypatch.setenv("RCLONE_CONFIG_GS_TYPE", "google cloud storage")
+    monkeypatch.setenv("RCLONE_CONFIG_GS_ANONYMOUS", "true")
+    yield
