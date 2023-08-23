aa0507e41a2cc5e3b315a4d92c1450e2c1631095
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-08-23 12:36:07 +0100

    Fix server requirements test

--- scheduler/pkg/scheduler/filters/replicarequirements_test.go
+++ scheduler/pkg/scheduler/filters/replicarequirements_test.go
@@ -26,10 +26,10 @@ import (
 	"github.com/seldonio/seldon-core/scheduler/v2/pkg/store"
 )
 
-func TestReplicaRequirementsFilter(t *testing.T) {
+func TestServerRequirementFilter(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	getTestModelWithRequirements := func(requirements []string) *store.ModelVersion {
+	makeModel := func(requirements []string) *store.ModelVersion {
 		return store.NewModelVersion(
 			&pb.Model{ModelSpec: &pb.ModelSpec{Requirements: requirements}, DeploymentSpec: &pb.DeploymentSpec{Replicas: 1}},
 			1,
@@ -39,28 +39,40 @@ func TestReplicaRequirementsFilter(t *testing.T) {
 			store.ModelProgressing)
 	}
 
-	getTestServerReplicaWithCaps := func(capabilities []string) *store.ServerReplica {
+	makeServerReplica := func(server *store.Server, capabilities []string) *store.ServerReplica {
 		return store.NewServerReplica("svc", 8080, 5001, 1, store.NewServer("server", true), capabilities, 100, 100, 0, nil, 100)
+	}
+
+	makeServer := func(replicas int, capabilities []string) *store.ServerSnapshot {
+		server := store.NewServer("server", true)
+		snapshot := server.CreateSnapshot(false, false)
+
+		for i := 0; i < replicas; i++ {
+			replica := makeServerReplica(server, capabilities)
+			snapshot.Replicas[i] = replica
+		}
+
+		return snapshot
 	}
 
 	type test struct {
 		name     string
 		model    *store.ModelVersion
-		server   *store.ServerReplica
+		server   *store.ServerSnapshot
 		expected bool
 	}
 
 	tests := []test{
-		{name: "Match", model: getTestModelWithRequirements([]string{"sklearn"}), server: getTestServerReplicaWithCaps([]string{"sklearn"}), expected: true},
-		{name: "Mismatch", model: getTestModelWithRequirements([]string{"sklearn"}), server: getTestServerReplicaWithCaps([]string{"xgboost"}), expected: false},
-		{name: "PartialMatch", model: getTestModelWithRequirements([]string{"sklearn", "xgboost"}), server: getTestServerReplicaWithCaps([]string{"xgboost"}), expected: false},
-		{name: "MultiMatch", model: getTestModelWithRequirements([]string{"sklearn", "xgboost"}), server: getTestServerReplicaWithCaps([]string{"xgboost", "sklearn", "tensorflow"}), expected: true},
-		{name: "Duplicates", model: getTestModelWithRequirements([]string{"sklearn", "xgboost", "sklearn"}), server: getTestServerReplicaWithCaps([]string{"xgboost", "sklearn", "tensorflow"}), expected: true},
+		{name: "Match", model: makeModel([]string{"sklearn"}), server: makeServer(1, []string{"sklearn"}), expected: true},
+		{name: "Mismatch", model: makeModel([]string{"sklearn"}), server: makeServer(1, []string{"xgboost"}), expected: false},
+		{name: "PartialMatch", model: makeModel([]string{"sklearn", "xgboost"}), server: makeServer(1, []string{"xgboost"}), expected: false},
+		{name: "MultiMatch", model: makeModel([]string{"sklearn", "xgboost"}), server: makeServer(1, []string{"xgboost", "sklearn", "tensorflow"}), expected: true},
+		{name: "Duplicates", model: makeModel([]string{"sklearn", "xgboost", "sklearn"}), server: makeServer(1, []string{"xgboost", "sklearn", "tensorflow"}), expected: true},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			filter := RequirementsReplicaFilter{}
+			filter := ServerRequirementFilter{}
 			ok := filter.Filter(test.model, test.server)
 			g.Expect(ok).To(Equal(test.expected))
 		})
