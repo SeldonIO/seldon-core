ae23b5ffaedf2c40302ffece6bee142bf16712f7
Viktor Taskov <viktor.taskov@seldon.io>
2023-08-24 10:36:23 +0100

    feat(docs): [SCv2] Automatically create and upload a custom HF model to seldon-models in GCS on every new MLServer version (#5103)
    * V2D-1257 Create and upload a custom HF model to seldon-models in GCS
    
    * Change GCS location to make it obvious it is a custom hf model

---
+++ samples/scripts/models/text-generation-huggingface/train.py
@@ -0,0 +1,19 @@
+from transformers import (
+    GPT2Tokenizer,
+    TFGPT2LMHeadModel,
+    pipeline,
+)
+
+
+def main() -> None:
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    model = TFGPT2LMHeadModel.from_pretrained("gpt2")
+
+    p = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+
+    p.save_pretrained("text-generation-model-artefacts")
+
+
+if __name__ == "__main__":
+    print("Building a custom HuggingFace model...")
+    main()
