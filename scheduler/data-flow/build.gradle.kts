dafa8e25509b6aa68eed2058171bae182865f656
Adrian Gonzalez-Martin <agm@seldon.io>
2023-08-29 17:34:09 +0100

    build(dataflow): bump grpc-stub and grpc-protobuf to 1.57.2 (#5110)

--- scheduler/data-flow/build.gradle.kts
+++ scheduler/data-flow/build.gradle.kts
@@ -27,8 +27,8 @@ dependencies {
 
     // gRPC
     implementation("io.grpc:grpc-kotlin-stub:1.2.1")
-    implementation("io.grpc:grpc-stub:1.45.0")
-    implementation("io.grpc:grpc-protobuf:1.45.0")
+    implementation("io.grpc:grpc-stub:1.57.2")
+    implementation("io.grpc:grpc-protobuf:1.57.2")
     runtimeOnly("io.grpc:grpc-netty-shaded:1.44.1")
     implementation("com.google.protobuf:protobuf-java:3.21.7")
     implementation("com.google.protobuf:protobuf-kotlin:3.21.7")
