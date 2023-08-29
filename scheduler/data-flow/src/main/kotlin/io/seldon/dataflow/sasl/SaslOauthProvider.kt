f23cc454751b1c277d30d1fadc99336041ca0660
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-08-29 18:58:06 +0100

    Add SASL OAuth config provider

---
+++ scheduler/data-flow/src/main/kotlin/io/seldon/dataflow/sasl/SaslOauthProvider.kt
@@ -0,0 +1,55 @@
+package io.seldon.dataflow.sasl
+
+import io.klogging.noCoLogger
+import io.seldon.dataflow.kafka.security.SaslConfig
+
+data class SaslOauthConfig(
+    val tokenUrl: String,
+    val clientId: String,
+    val clientSecret: String,
+    val scope: String?,
+    val extensions: List<String>?,
+)
+
+class SaslOauthProvider(private val secretsProvider: SecretsProvider) {
+    fun getOauthConfig(config: SaslConfig): SaslOauthConfig {
+        logger.info("retrieving OAuth config")
+
+        val secret = secretsProvider.getSecret(config.secret).withDefault { byteArrayOf() }
+
+        val clientId = secret.getValue(clientIdKey).toString()
+        val clientSecret = secret.getValue(clientSecretKey).toString()
+        val tokenUrl = secret.getValue(tokenUrlKey).toString()
+        val scope = secret.getValue(scopeKey).toString()
+        val extensions = secret.getValue(extensionsKey).toString().toExtensions()
+
+        return SaslOauthConfig(
+            tokenUrl = tokenUrl,
+            clientId = clientId,
+            clientSecret = clientSecret,
+            scope = scope,
+            extensions = extensions,
+        )
+    }
+
+    private fun String.toExtensions(): List<String>? {
+        if (this.isBlank()) {
+            return null
+        }
+
+        return this
+            .split(",")
+            .map { it.trim() }
+            .map { "extension_$it" }
+    }
+
+    companion object {
+        private const val clientIdKey = "client_id"
+        private const val clientSecretKey = "client_secret"
+        private const val tokenUrlKey = "token_endpoint_url"
+        private const val scopeKey = "scope"
+        private const val extensionsKey = "extension"
+
+        private val logger = noCoLogger(SaslOauthProvider::class)
+    }
+}
