621f48378b3c388ff510e1e31af9a1715782b967
Sherif Akoush <sa@seldon.io>
2023-10-02 09:44:14 +0100

    lock model before updating status

--- scheduler/pkg/store/memory.go
+++ scheduler/pkg/store/memory.go
@@ -678,9 +678,11 @@ func (m *MemoryStore) removeModelfromServerReplica(lModels map[ModelVersionID]bo
 						modelVersionID.Name, replicaIdx,
 					)
 					modelVersion.SetReplicaState(replicaIdx, Unloaded, "model is being deleted and server replica was removed")
+					m.LockModel(modelVersionID.Name)
 					m.updateModelStatus(
 						model.Latest().GetVersion() == modelVersion.GetVersion(),
 						model.IsDeleted(), modelVersion, model.GetLastAvailableModelVersion())
+						m.UnlockModel(modelVersionID.Name)
 					// send an event to progress the deletion
 					evts = append(
 						evts,
