869e683d2e76edefaf3a0ef8a9106df87b36e4ae
Sherif Akoush <sa@seldon.io>
2024-02-08 06:15:56 +0000

    update case when we can remove  pipeline finaliser

--- operator/scheduler/pipeline.go
+++ operator/scheduler/pipeline.go
@@ -132,7 +132,7 @@ func (s *SchedulerClient) SubscribePipelineEvents(ctx context.Context, conn *grp
 		}
 
 		if !pipeline.ObjectMeta.DeletionTimestamp.IsZero() {
-			logger.Info("Pipeline is pending deletion", "pipeline", pipeline.Name)
+			logger.Info("Pipeline is pending deletion", "pipeline", pipeline.Name, "state", pv.State.Status.String())
 			if canRemovePipelineFinalizer(pv.State.Status) {
 				retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
 					latestPipeline := &v1alpha1.Pipeline{}
@@ -246,7 +246,7 @@ func (s *SchedulerClient) updatePipelineStatusImpl(pipeline *v1alpha1.Pipeline) 
 
 func canRemovePipelineFinalizer(state scheduler.PipelineVersionState_PipelineStatus) bool {
 	switch state {
-	case scheduler.PipelineVersionState_PipelineTerminating:
+	case scheduler.PipelineVersionState_PipelineTerminating, scheduler.PipelineVersionState_PipelineTerminate:
 		return false
 	default:
 		return true
