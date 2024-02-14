43305a9e42ea2f609e83baf5de52d470e4cd865d
kengo <20113339+KengoA@users.noreply.github.com>
2024-02-14 12:47:54 +0000

    add tests to check override values

--- operator/controllers/reconcilers/seldon/deployment_reconciler_test.go
+++ operator/controllers/reconcilers/seldon/deployment_reconciler_test.go
@@ -23,6 +23,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/pointer"
 	client2 "sigs.k8s.io/controller-runtime/pkg/client"
 
 	mlopsv1alpha1 "github.com/seldonio/seldon-core/operator/v2/apis/mlops/v1alpha1"
@@ -138,6 +139,114 @@ func TestDeploymentReconcile(t *testing.T) {
 					Namespace: meta.Namespace,
 				}, dep)
 				g.Expect(err).To(BeNil())
+			}
+		})
+	}
+}
+
+func TestDeploymentOverride(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	type test struct {
+		name             string
+		deploymentName   string
+		podSpec          *v1.PodSpec
+		override         *mlopsv1alpha1.OverrideSpec
+		expectedReplicas int32
+		expectedCPU      resource.Quantity
+		expectedMemory   resource.Quantity
+	}
+
+	tests := []test{
+		{
+			name:           "scheduler: ReplicasOverride",
+			deploymentName: mlopsv1alpha1.SchedulerName,
+			podSpec:        &v1.PodSpec{},
+			override: &mlopsv1alpha1.OverrideSpec{
+				Replicas: pointer.Int32Ptr(3),
+			},
+			expectedReplicas: 3,
+		},
+		{
+			name:           "scheduler: ResourcesOverride",
+			deploymentName: mlopsv1alpha1.SchedulerName,
+			podSpec: &v1.PodSpec{
+				Containers: []v1.Container{
+					{
+						Resources: v1.ResourceRequirements{
+							Limits: v1.ResourceList{
+								v1.ResourceCPU:    resource.MustParse("1"),
+								v1.ResourceMemory: resource.MustParse("1Gi"),
+							},
+						},
+					},
+				},
+			},
+			override: &mlopsv1alpha1.OverrideSpec{
+				PodSpec: &mlopsv1alpha1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Resources: v1.ResourceRequirements{
+								Limits: v1.ResourceList{
+									v1.ResourceCPU:    resource.MustParse("2"),
+									v1.ResourceMemory: resource.MustParse("2Gi"),
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCPU:    *resource.NewQuantity(2, resource.DecimalSI),
+			expectedMemory: *resource.NewQuantity(2*1024*1024*1024, resource.BinarySI),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			logger := logrtest.New(t)
+			var client client2.Client
+			scheme := runtime.NewScheme()
+			err := mlopsv1alpha1.AddToScheme(scheme)
+			g.Expect(err).To(BeNil())
+			err = v1.AddToScheme(scheme)
+			g.Expect(err).To(BeNil())
+			err = auth.AddToScheme(scheme)
+			g.Expect(err).To(BeNil())
+			err = appsv1.AddToScheme(scheme)
+			g.Expect(err).To(BeNil())
+			annotator := patch.NewAnnotator(constants.LastAppliedConfig)
+			meta := metav1.ObjectMeta{
+				Name:      "test",
+				Namespace: "default",
+			}
+			client = testing2.NewFakeClient(scheme)
+			sr, err := NewComponentDeploymentReconciler(
+				test.deploymentName,
+				common.ReconcilerConfig{Ctx: context.TODO(), Logger: logger, Client: client},
+				meta,
+				test.podSpec,
+				test.override,
+				metav1.ObjectMeta{},
+				annotator)
+			g.Expect(err).To(BeNil())
+			err = sr.Reconcile()
+			g.Expect(err).To(BeNil())
+
+			dep := &appsv1.Deployment{}
+			err = client.Get(context.TODO(), types.NamespacedName{
+				Name:      test.deploymentName,
+				Namespace: meta.Namespace,
+			}, dep)
+			g.Expect(err).To(BeNil())
+
+			if test.expectedReplicas != 0 {
+				g.Expect(*dep.Spec.Replicas).To(Equal(test.expectedReplicas))
+			}
+			if test.expectedCPU.Value() != 0 {
+				g.Expect(dep.Spec.Template.Spec.Containers[0].Resources.Limits.Cpu().Value()).To(Equal(test.expectedCPU.Value()))
+			}
+			if test.expectedMemory.Value() != 0 {
+				g.Expect(dep.Spec.Template.Spec.Containers[0].Resources.Limits.Memory().Value()).To(Equal(test.expectedMemory.Value()))
 			}
 		})
 	}
