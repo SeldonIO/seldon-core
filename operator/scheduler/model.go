f4c394e05e28332f4adcdfb12c80393581fe0a0d
Sherif Akoush <sa@seldon.io>
2024-03-11 09:13:48 +0000

    add docstring

--- operator/scheduler/model.go
+++ operator/scheduler/model.go
@@ -31,6 +31,10 @@ import (
 	"github.com/seldonio/seldon-core/operator/v2/pkg/utils"
 )
 
+// LoadModel loads a model to the scheduler
+// If the connection is not provided, get a new one
+// In the case of errors we check if the error is retryable and return a boolean to indicate if the error is retryable
+// For the cases we think we should retry, check logic in `checkErrorRetryable`
 func (s *SchedulerClient) LoadModel(ctx context.Context, model *v1alpha1.Model, conn *grpc.ClientConn) (bool, error) {
 	logger := s.logger.WithName("LoadModel")
 	retryableError := false
@@ -331,6 +335,55 @@ func (s *SchedulerClient) handlePendingDeleteModels(
 			if retryUnload, err := s.UnloadModel(ctx, &model, conn); err != nil {
 				if retryUnload {
 					s.logger.Info("Failed to call unload model", "model", model.Name)
+					continue
+				} else {
+					// this is essentially a failed pre-condition (model does not exist in scheduler)
+					// we can remove
+					// note that there is still the chance the model is not updated from the different model servers
+					// upon reconnection of the scheduler
+					retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+						model.ObjectMeta.Finalizers = utils.RemoveStr(model.ObjectMeta.Finalizers, constants.ModelFinalizerName)
+						if errUpdate := s.Update(ctx, &model); errUpdate != nil {
+							s.logger.Error(err, "Failed to remove finalizer", "model", model.Name)
+							return errUpdate
+						}
+						s.logger.Info("Removed finalizer", "model", model.Name)
+						return nil
+					})
+					if retryErr != nil {
+						s.logger.Error(err, "Failed to remove finalizer after retries", "model", model.Name)
+					}
+				}
+			} else {
+				// if the model exists in the scheduler so we wait until we get the event from the subscription stream
+				s.logger.Info("Unload model called successfully, not removing finalizer", "model", model.Name)
+			}
+			break
+		}
+	}
+}
+
+// when need to reload the models that are marked in k8s as loaded, this is because there could be a
+// case where the scheduler has load the models state (if the scheduler and the model server restart at the same time)
+func (s *SchedulerClient) handleLoadedModels(
+	ctx context.Context, namespace string, conn *grpc.ClientConn) {
+	modelList := &v1alpha1.ModelList{}
+	// Get all models in the namespace
+	err := s.List(
+		ctx,
+		modelList,
+		client.InNamespace(namespace),
+	)
+	if err != nil {
+		return
+	}
+
+	for _, model := range modelList.Items {
+		// models that are not in the process of being deleted has DeletionTimestamp as zero
+		if model.ObjectMeta.DeletionTimestamp.IsZero() {
+			if retryLoad, err := s.LoadModel(ctx, &model, conn); err != nil {
+				if retryLoad {
+					s.logger.Info("Failed to call load model", "model", model.Name)
 					continue
 				} else {
 					// this is essentially a failed pre-condition (model does not exist in scheduler)
