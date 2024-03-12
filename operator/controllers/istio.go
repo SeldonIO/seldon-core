1b5eb181392b2c5f0ae44c20fa84708b8b7de56b
oona <maojiangyun@163.com>
2024-03-12 23:11:52 +0800

    Update istio.go
    add env ANNOTATION_ISTIO_HOST_SEPARATOR to support multi hosts

--- operator/controllers/istio.go
+++ operator/controllers/istio.go
@@ -10,4 +10,5 @@ const (
 	ANNOTATION_ISTIO_RETRIES         = "seldon.io/istio-retries"
 	ANNOTATION_ISTIO_RETRIES_TIMEOUT = "seldon.io/istio-retries-timeout"
 	ANNOTATION_ISTIO_HOST            = "seldon.io/istio-host"
+	ANNOTATION_ISTIO_HOST_SEPARATOR  = ","
 )
