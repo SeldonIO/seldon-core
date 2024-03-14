1785c9fea2c15723bab925d691bb12c1938661a1
Sherif Akoush <sa@seldon.io>
2024-03-14 13:53:35 +0000

    add unit test for sendwithtimeout

---
+++ scheduler/pkg/server/utils_test.go
@@ -0,0 +1,73 @@
+/*
+Copyright (c) 2024 Seldon Technologies Ltd.
+
+Use of this software is governed by
+(1) the license included in the LICENSE file or
+(2) if the license included in the LICENSE file is the Business Source License 1.1,
+the Change License after the Change Date as each is defined in accordance with the LICENSE file.
+*/
+
+package server
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestSendWithTimeout(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	type test struct {
+		name      string
+		sleepTime time.Duration
+		err       error
+		isErr     bool
+		isExpired bool
+	}
+
+	fn := func(err error) error {
+		time.Sleep(5 * time.Millisecond)
+		return err
+	}
+
+	tests := []test{
+		{
+			name:      "simple",
+			sleepTime: 10 * time.Millisecond,
+			err:       nil,
+			isErr:     false,
+			isExpired: false,
+		},
+		{
+			name:      "timeout",
+			sleepTime: 1 * time.Millisecond,
+			err:       nil,
+			isErr:     true,
+			isExpired: true,
+		},
+		{
+			name:      "error",
+			sleepTime: 10 * time.Millisecond,
+			err:       fmt.Errorf("error"),
+			isErr:     true,
+			isExpired: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hasExpired, err := sentWithTimeout(func() error {
+				return fn(test.err)
+			}, test.sleepTime)
+			g.Expect(hasExpired).To(Equal(test.isExpired))
+			if test.isErr {
+				g.Expect(err).ToNot(BeNil())
+			} else {
+				g.Expect(err).To(BeNil())
+			}
+		})
+	}
+}
