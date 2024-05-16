26d2e77894a412ec56124e69491728af199d18fa
Sherif Akoush <sa@seldon.io>
2024-05-16 14:48:30 +0100

    fix typo in package version

--- scheduler/data-flow/build.gradle.kts
+++ scheduler/data-flow/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
     // Kafka
     implementation("org.apache.kafka:kafka-streams:7.6.1-ccs")
-    testImplementation("org.apache.kafka:kafka-streams-test-utils:7.6.1-css")
+    testImplementation("org.apache.kafka:kafka-streams-test-utils:7.6.1-ccs")
 
     // gRPC
     implementation("io.grpc:grpc-kotlin-stub:1.4.1")
