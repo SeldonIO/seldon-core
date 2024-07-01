2bc7b18901a2596e0e5254f4491d7be7d574284d
Sherif Akoush <sa@seldon.io>
2024-07-01 17:01:45 +0100

    add pipeline get from db helper (for testing)

--- scheduler/pkg/store/pipeline/db_test.go
+++ scheduler/pkg/store/pipeline/db_test.go
@@ -138,3 +138,80 @@ func TestSaveAndRestore(t *testing.T) {
 		})
 	}
 }
+
+func TestGetPipelineFromDB(t *testing.T) {
+	g := NewGomegaWithT(t)
+	type test struct {
+		name         string
+		pipelines    []*Pipeline
+		pipelineName string
+		isErr        bool
+	}
+
+	tests := []test{
+		{
+			name: "test single pipeline",
+			pipelines: []*Pipeline{
+				{
+					Name:        "test",
+					LastVersion: 0,
+					Versions: []*PipelineVersion{
+						{
+							Name:    "p1",
+							Version: 0,
+							UID:     "x",
+							Steps: map[string]*PipelineStep{
+								"a": {Name: "a"},
+							},
+							State: &PipelineState{
+								Status:    PipelineReady,
+								Reason:    "deployed",
+								Timestamp: time.Now(),
+							},
+							Output: &PipelineOutput{},
+							KubernetesMeta: &KubernetesMeta{
+								Namespace: "default",
+							},
+						},
+					},
+					Deleted: false,
+				},
+			},
+			pipelineName: "test",
+			isErr:        false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			path := fmt.Sprintf("%s/db", t.TempDir())
+			logger := log.New()
+			db, err := newPipelineDbManager(getPipelineDbFolder(path), logger)
+			g.Expect(err).To(BeNil())
+			for _, p := range test.pipelines {
+				err := db.save(p)
+				g.Expect(err).To(BeNil())
+			}
+			g.Expect(err).To(BeNil())
+
+			actualPipeline, err := db.get(test.pipelineName)
+			if test.isErr {
+				g.Expect(err).To(BeNil())
+				g.Expect(actualPipeline).To(BeNil())
+			} else {
+				g.Expect(err).To(BeNil())
+				g.Expect(actualPipeline).ToNot(BeNil())
+				pipeFound := false
+				for _, pipe := range test.pipelines {
+					if pipe.Name == test.pipelineName {
+						g.Expect(cmp.Equal(pipe, actualPipeline)).To(BeTrue())
+						pipeFound = true
+					}
+				}
+				g.Expect(pipeFound).To(BeTrue())
+			}
+			err = db.Stop()
+			g.Expect(err).To(BeNil())
+		})
+	}
+}
