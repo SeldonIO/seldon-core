2a218353ae2a267efacd862ef1159e3b982a1230
Sherif Akoush <sa@seldon.io>
2024-07-02 09:36:28 +0100

    introduce ExperimentSnapshot proto

--- apis/go/mlops/scheduler/storage.pb.go
+++ apis/go/mlops/scheduler/storage.pb.go
@@ -100,6 +100,64 @@ func (x *PipelineSnapshot) GetDeleted() bool {
 	return false
 }
 
+type ExperimentSnapshot struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Experiment *Experiment `protobuf:"bytes,1,opt,name=experiment,proto3" json:"experiment,omitempty"`
+	// to mark the experiment as deleted, this is currently required as we persist all
+	// experiments in the local scheduler state (badgerdb) so that events can be replayed
+	// on restart, which would guard against lost events in communication.
+	Deleted bool `protobuf:"varint,2,opt,name=deleted,proto3" json:"deleted,omitempty"`
+}
+
+func (x *ExperimentSnapshot) Reset() {
+	*x = ExperimentSnapshot{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_mlops_scheduler_storage_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ExperimentSnapshot) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ExperimentSnapshot) ProtoMessage() {}
+
+func (x *ExperimentSnapshot) ProtoReflect() protoreflect.Message {
+	mi := &file_mlops_scheduler_storage_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ExperimentSnapshot.ProtoReflect.Descriptor instead.
+func (*ExperimentSnapshot) Descriptor() ([]byte, []int) {
+	return file_mlops_scheduler_storage_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *ExperimentSnapshot) GetExperiment() *Experiment {
+	if x != nil {
+		return x.Experiment
+	}
+	return nil
+}
+
+func (x *ExperimentSnapshot) GetDeleted() bool {
+	if x != nil {
+		return x.Deleted
+	}
+	return false
+}
+
 var File_mlops_scheduler_storage_proto protoreflect.FileDescriptor
 
 var file_mlops_scheduler_storage_proto_rawDesc = []byte{
@@ -119,11 +177,18 @@ var file_mlops_scheduler_storage_proto_rawDesc = []byte{
 	0x69, 0x70, 0x65, 0x6c, 0x69, 0x6e, 0x65, 0x57, 0x69, 0x74, 0x68, 0x53, 0x74, 0x61, 0x74, 0x65,
 	0x52, 0x08, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x73, 0x12, 0x18, 0x0a, 0x07, 0x64, 0x65,
 	0x6c, 0x65, 0x74, 0x65, 0x64, 0x18, 0x04, 0x20, 0x01, 0x28, 0x08, 0x52, 0x07, 0x64, 0x65, 0x6c,
-	0x65, 0x74, 0x65, 0x64, 0x42, 0x3c, 0x5a, 0x3a, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63,
-	0x6f, 0x6d, 0x2f, 0x73, 0x65, 0x6c, 0x64, 0x6f, 0x6e, 0x69, 0x6f, 0x2f, 0x73, 0x65, 0x6c, 0x64,
-	0x6f, 0x6e, 0x2d, 0x63, 0x6f, 0x72, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x73, 0x2f, 0x67, 0x6f, 0x2f,
-	0x76, 0x32, 0x2f, 0x6d, 0x6c, 0x6f, 0x70, 0x73, 0x2f, 0x73, 0x63, 0x68, 0x65, 0x64, 0x75, 0x6c,
-	0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x65, 0x74, 0x65, 0x64, 0x22, 0x72, 0x0a, 0x12, 0x45, 0x78, 0x70, 0x65, 0x72, 0x69, 0x6d, 0x65,
+	0x6e, 0x74, 0x53, 0x6e, 0x61, 0x70, 0x73, 0x68, 0x6f, 0x74, 0x12, 0x42, 0x0a, 0x0a, 0x65, 0x78,
+	0x70, 0x65, 0x72, 0x69, 0x6d, 0x65, 0x6e, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x22,
+	0x2e, 0x73, 0x65, 0x6c, 0x64, 0x6f, 0x6e, 0x2e, 0x6d, 0x6c, 0x6f, 0x70, 0x73, 0x2e, 0x73, 0x63,
+	0x68, 0x65, 0x64, 0x75, 0x6c, 0x65, 0x72, 0x2e, 0x45, 0x78, 0x70, 0x65, 0x72, 0x69, 0x6d, 0x65,
+	0x6e, 0x74, 0x52, 0x0a, 0x65, 0x78, 0x70, 0x65, 0x72, 0x69, 0x6d, 0x65, 0x6e, 0x74, 0x12, 0x18,
+	0x0a, 0x07, 0x64, 0x65, 0x6c, 0x65, 0x74, 0x65, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x08, 0x52,
+	0x07, 0x64, 0x65, 0x6c, 0x65, 0x74, 0x65, 0x64, 0x42, 0x3c, 0x5a, 0x3a, 0x67, 0x69, 0x74, 0x68,
+	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x65, 0x6c, 0x64, 0x6f, 0x6e, 0x69, 0x6f, 0x2f,
+	0x73, 0x65, 0x6c, 0x64, 0x6f, 0x6e, 0x2d, 0x63, 0x6f, 0x72, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x73,
+	0x2f, 0x67, 0x6f, 0x2f, 0x76, 0x32, 0x2f, 0x6d, 0x6c, 0x6f, 0x70, 0x73, 0x2f, 0x73, 0x63, 0x68,
+	0x65, 0x64, 0x75, 0x6c, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -138,18 +203,21 @@ func file_mlops_scheduler_storage_proto_rawDescGZIP() []byte {
 	return file_mlops_scheduler_storage_proto_rawDescData
 }
 
-var file_mlops_scheduler_storage_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_mlops_scheduler_storage_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_mlops_scheduler_storage_proto_goTypes = []any{
-	(*PipelineSnapshot)(nil),  // 0: seldon.mlops.scheduler.PipelineSnapshot
-	(*PipelineWithState)(nil), // 1: seldon.mlops.scheduler.PipelineWithState
+	(*PipelineSnapshot)(nil),   // 0: seldon.mlops.scheduler.PipelineSnapshot
+	(*ExperimentSnapshot)(nil), // 1: seldon.mlops.scheduler.ExperimentSnapshot
+	(*PipelineWithState)(nil),  // 2: seldon.mlops.scheduler.PipelineWithState
+	(*Experiment)(nil),         // 3: seldon.mlops.scheduler.Experiment
 }
 var file_mlops_scheduler_storage_proto_depIdxs = []int32{
-	1, // 0: seldon.mlops.scheduler.PipelineSnapshot.versions:type_name -> seldon.mlops.scheduler.PipelineWithState
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	2, // 0: seldon.mlops.scheduler.PipelineSnapshot.versions:type_name -> seldon.mlops.scheduler.PipelineWithState
+	3, // 1: seldon.mlops.scheduler.ExperimentSnapshot.experiment:type_name -> seldon.mlops.scheduler.Experiment
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_mlops_scheduler_storage_proto_init() }
@@ -171,6 +239,18 @@ func file_mlops_scheduler_storage_proto_init() {
 				return nil
 			}
 		}
+		file_mlops_scheduler_storage_proto_msgTypes[1].Exporter = func(v any, i int) any {
+			switch v := v.(*ExperimentSnapshot); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -178,7 +258,7 @@ func file_mlops_scheduler_storage_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_mlops_scheduler_storage_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
