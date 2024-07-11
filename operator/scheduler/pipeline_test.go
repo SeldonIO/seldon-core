c49adc1ce6c545d4827f56af1576c7229628b164
Sherif Akoush <sa@seldon.io>
2024-07-11 12:12:08 +0100

    add a test for pipeline termination

--- operator/scheduler/pipeline_test.go
+++ operator/scheduler/pipeline_test.go
@@ -94,6 +94,42 @@ func TestSubscribePipelineEvents(t *testing.T) {
 			noSchedulerState: false,
 		},
 		{
+			name: "pipeline terminated - with scheduler state",
+			resources: []client.Object{
+				&mlopsv1alpha1.Pipeline{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:       "foo",
+						Namespace:  "default",
+						Generation: 1,
+						DeletionTimestamp: &now,
+						Finalizers:        []string{constants.PipelineFinalizerName},
+					},
+					Spec: mlopsv1alpha1.PipelineSpec{},
+				},
+			},
+			results: []*scheduler.PipelineStatusResponse{
+				{
+					PipelineName: "foo",
+					Versions: []*scheduler.PipelineWithState{
+						{
+							Pipeline: &scheduler.Pipeline{
+								Name: "foo",
+								KubernetesMeta: &scheduler.KubernetesMeta{
+									Namespace:  "default",
+									Generation: 1,
+								},
+							},
+							State: &scheduler.PipelineVersionState{
+								Status:      scheduler.PipelineVersionState_PipelineTerminated,
+								ModelsReady: false,
+							},
+						},
+					},
+				},
+			},
+			noSchedulerState: false,
+		},
+		{
 			name: "model not ready and pipeline ready",
 			resources: []client.Object{
 				&mlopsv1alpha1.Pipeline{
