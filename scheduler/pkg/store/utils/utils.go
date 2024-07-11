ae0f70de51b988d263b2077c0eeae26bcf7f1703
Sherif Akoush <sa@seldon.io>
2024-07-11 16:15:06 +0100

    update copyright

--- scheduler/pkg/store/utils/utils.go
+++ scheduler/pkg/store/utils/utils.go
@@ -1,3 +1,12 @@
+/*
+Copyright (c) 2024 Seldon Technologies Ltd.
+
+Use of this software is governed BY
+(1) the license included in the LICENSE file or
+(2) if the license included in the LICENSE file is the Business Source License 1.1,
+the Change License after the Change Date as each is defined in accordance with the LICENSE file.
+*/
+
 package utils
 
 import (
