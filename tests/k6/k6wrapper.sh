a2e48c395d606d766e4bc209a4eb355b55d6568b
Lucian Carata <lcr@bitrunes.org>
2024-08-21 14:12:51 +0100

    WIP.

--- tests/k6/k6wrapper.sh
+++ tests/k6/k6wrapper.sh
@@ -5,20 +5,52 @@ METADATA="metadata.txt"
 NOW=$(date +"%Y_%m_%d_%H_%M_%S")
 UUID=$(uuidgen)
 LABELS=$(cat /info/labels)
-# extracts controller id from the labels: 
+# extracts controller id from the labels:
 # eg: controller-uid="95a4c449-5cda-45a0-93e1-177caacc3639" job-name="k6"
 JOBID=$(echo $LABELS | sed -n 's/.*controller-uid="\([a-zA-Z0-9-]\+\)".*/\1/p')
-if [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
-    gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+
+DEFAULT_OUTPUT_TYPE=csv
+
+if [ -z "$OUTPUT_TYPE" ]; then
+    OUTPUT_TYPE=${DEFAULT_OUTPUT_TYPE}
 fi
-echo "start:"$(date) > $DIR/$METADATA
-# use local k6 that is built with xk6 to include kube apis
-./k6 $@
-echo "end:"$(date) >> $DIR/$METADATA
-echo "args:"$@ >> $DIR/$METADATA
-echo "envs:"$(printenv) >> $DIR/$METADATA
-echo "metadata:"$TEST_METADATA >> $DIR/$METADATA
-echo "labels:"$LABELS >> $DIR/$METADATA 
-if [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
-    gsutil cp -r $DIR ${GS_BUCKET_NAME}/${TEST_METADATA}_${JOBID}_${NOW}_${UUID}
-fi
+
+# k6 pre-test
+case $OUTPUT_TYPE in
+    "csv")
+        K6_ARGS="--out csv=$DIR/k6-raw-test-output.gz"
+        ;;
+    "gs_bucket")
+        if [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+            gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+        fi
+        echo "start:"$(date) > $DIR/$METADATA
+        ;;
+    "clickhouse")
+        if [ -z "${K6_CLICKHOUSE_DB}" ]; then
+            echo "K6_CLICKHOUSE_DB is not set"
+            exit 1
+        fi
+        K6_ARGS="--out clickhouse=${K6_CLICKHOUSE_DB}"
+        ;;
+    *)
+        K6_ARGS=""
+        ;;
+esac
+
+./k6 $@ ${K6_ARGS}
+
+# k6 post-test
+case $OUTPUT_TYPE in
+    "gs_bucket")
+        echo "end:"$(date) >> $DIR/$METADATA
+        echo "args:"$@ >> $DIR/$METADATA
+        echo "envs:"$(printenv) >> $DIR/$METADATA
+        echo "metadata:"$TEST_METADATA >> $DIR/$METADATA
+        echo "labels:"$LABELS >> $DIR/$METADATA
+        if [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+            gsutil cp -r $DIR ${GS_BUCKET_NAME}/${TEST_METADATA}_${JOBID}_${NOW}_${UUID}
+        fi
+        exit 0
+        ;;
+esac
