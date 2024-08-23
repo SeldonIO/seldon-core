b9cc24b796d85da7c560c5cd3c3911913bbd529a
Lucian Carata <lcr@bitrunes.org>
2024-08-23 02:56:29 +0100

    WIP.

--- tests/k6/k6wrapper.sh
+++ tests/k6/k6wrapper.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 DIR="results"
-METADATA="metadata.txt"
 NOW=$(date +"%Y_%m_%d_%H_%M_%S")
+METADATA_FILE="metadata-${TEST_ID}-${NOW}.txt"
+SUMMARY_FILE="summary-${TEST_ID}-${NOW}.json"
+RAW_FILE="rawTestData-${TEST_ID}-${NOW}.json"
+
 UUID=$(uuidgen)
 LABELS=$(cat /info/labels)
 # extracts controller id from the labels:
@@ -10,44 +13,52 @@ LABELS=$(cat /info/labels)
 JOBID=$(echo $LABELS | sed -n 's/.*controller-uid="\([a-zA-Z0-9-]\+\)".*/\1/p')
 
 DEFAULT_OUTPUT_TYPE=none
+K6_ARGS=""
 
 if [ -z "$OUTPUT_TYPE" ]; then
     OUTPUT_TYPE=${DEFAULT_OUTPUT_TYPE}
 fi
 
 # k6 pre-test
+echo "start:"$(date) > $DIR/$METADATA_FILE
 case $OUTPUT_TYPE in
     "csv")
-        K6_ARGS="--out csv=$DIR/k6-raw-test-output.gz"
+        K6_ARGS="--summary-export $DIR/${SUMMARY_FILE}"
+        K6_ARGS="${K6_ARGS} --out csv=$DIR/${RAW_FILE}"
         ;;
     "gs_bucket")
         if [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
             gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
         fi
-        echo "start:"$(date) > $DIR/$METADATA
+        K6_ARGS="--summary-export $DIR/${SUMMARY_FILE}"
+        K6_ARGS="${K6_ARGS} --out csv=$DIR/${RAW_FILE}"
         ;;
     "clickhouse")
         if [ -z "${K6_CLICKHOUSE_DSN}" ]; then
             echo "Dataset Name (DSN) is not set in K6_CLICKHOUSE_DSN. The format needs to be clickhouse://user:pass@host:port/[DBName]"
             exit 1
         fi
-        K6_ARGS="--out clickhouse=${K6_CLICKHOUSE_DSN}"
+        K6_ARGS="--summary-export $DIR/${SUMMARY_FILE}"
+        K6_ARGS="${K6_ARGS} --out clickhouse=${K6_CLICKHOUSE_DSN}"
         ;;
     *)
         K6_ARGS=""
         ;;
 esac
 
-./k6 $@ ${K6_ARGS}
+K6_CMD=$1
+shift
+
+./k6 ${K6_CMD} ${K6_ARGS} $@
 
 # k6 post-test
+echo "end:"$(date) >> $DIR/$METADATA_FILE
+echo "args:"$@ >> $DIR/$METADATA_FILE
+echo -e "envs:"$(printenv) >> $DIR/$METADATA_FILE
+echo "metadata:"$TEST_METADATA >> $DIR/$METADATA_FILE
+echo "labels:"$LABELS >> $DIR/$METADATA_FILE
 case $OUTPUT_TYPE in
     "gs_bucket")
-        echo "end:"$(date) >> $DIR/$METADATA
-        echo "args:"$@ >> $DIR/$METADATA
-        echo "envs:"$(printenv) >> $DIR/$METADATA
-        echo "metadata:"$TEST_METADATA >> $DIR/$METADATA
-        echo "labels:"$LABELS >> $DIR/$METADATA
         if [ -f "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
             gsutil cp -r $DIR ${GS_BUCKET_NAME}/${TEST_METADATA}_${JOBID}_${NOW}_${UUID}
         fi
