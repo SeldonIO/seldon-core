5994b9a0602e093a10b6e4e38bd1ff7689bc65b0
Sherif Akoush <sa@seldon.io>
2024-09-17 09:39:24 +0100

    add testing for servers (and other events) in hub

--- scheduler/pkg/coordinator/model_test.go
+++ scheduler/pkg/coordinator/model_test.go
@@ -19,24 +19,89 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type eventHubTestOp int
+
+const (
+	modelEventHub eventHubTestOp = iota
+	serverEventHub
+	pipelineEventHub
+	experimentEventHub
+)
+
 func TestRegisterHandler(t *testing.T) {
 	type test struct {
 		name        string
 		numHandlers int
+		op          eventHubTestOp
 	}
 
 	tests := []test{
+		// models
 		{
-			name:        "0 handlers do not block publishing",
+			name:        "0 handlers do not block publishing - models",
 			numHandlers: 0,
+			op:          modelEventHub,
 		},
 		{
-			name:        "1 handler receives events",
+			name:        "1 handler receives events - models",
 			numHandlers: 1,
+			op:          modelEventHub,
 		},
 		{
-			name:        "n handlers all receive all events",
+			name:        "n handlers all receive all events - models",
 			numHandlers: 20,
+			op:          modelEventHub,
+		},
+
+		// servers
+		{
+			name:        "0 handlers do not block publishing - servers",
+			numHandlers: 0,
+			op:          serverEventHub,
+		},
+		{
+			name:        "1 handler receives events - servers",
+			numHandlers: 1,
+			op:          serverEventHub,
+		},
+		{
+			name:        "n handlers all receive all events - servers",
+			numHandlers: 20,
+			op:          serverEventHub,
+		},
+
+		// pipelines
+		{
+			name:        "0 handlers do not block publishing - pipelines",
+			numHandlers: 0,
+			op:          pipelineEventHub,
+		},
+		{
+			name:        "1 handler receives events - pipelines",
+			numHandlers: 1,
+			op:          pipelineEventHub,
+		},
+		{
+			name:        "n handlers all receive all events - pipelines",
+			numHandlers: 20,
+			op:          pipelineEventHub,
+		},
+
+		// experiments
+		{
+			name:        "0 handlers do not block publishing - experiments",
+			numHandlers: 0,
+			op:          experimentEventHub,
+		},
+		{
+			name:        "1 handler receives events - experiments",
+			numHandlers: 1,
+			op:          experimentEventHub,
+		},
+		{
+			name:        "n handlers all receive all events - experiments",
+			numHandlers: 20,
+			op:          experimentEventHub,
 		},
 	}
 
@@ -53,19 +118,59 @@ func TestRegisterHandler(t *testing.T) {
 			var counter int64
 
 			for i := 0; i < tt.numHandlers; i++ {
-				h.RegisterModelEventHandler(
-					fmt.Sprintf("handler-%d", i),
-					queueSize,
-					l,
-					func(event ModelEventMsg) { atomic.AddInt64(&counter, 1) },
-				)
+				if tt.op == modelEventHub {
+					h.RegisterModelEventHandler(
+						fmt.Sprintf("handler-%d", i),
+						queueSize,
+						l,
+						func(event ModelEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				} else if tt.op == serverEventHub {
+					h.RegisterServerEventHandler(
+						fmt.Sprintf("handler-%d", i),
+						queueSize,
+						l,
+						func(event ServerEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				} else if tt.op == pipelineEventHub {
+					h.RegisterPipelineEventHandler(
+						fmt.Sprintf("handler-%d", i),
+						queueSize,
+						l,
+						func(event PipelineEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				} else if tt.op == experimentEventHub {
+					h.RegisterExperimentEventHandler(
+						fmt.Sprintf("handler-%d", i),
+						queueSize,
+						l,
+						func(event ExperimentEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				}
 			}
 
 			for i := 0; i < numEvents; i++ {
-				h.PublishModelEvent(
-					eventSource,
-					ModelEventMsg{},
-				)
+				if tt.op == modelEventHub {
+					h.PublishModelEvent(
+						eventSource,
+						ModelEventMsg{},
+					)
+				} else if tt.op == serverEventHub {
+					h.PublishServerEvent(
+						eventSource,
+						ServerEventMsg{},
+					)
+				} else if tt.op == pipelineEventHub {
+					h.PublishPipelineEvent(
+						eventSource,
+						PipelineEventMsg{},
+					)
+				} else if tt.op == experimentEventHub {
+					h.PublishExperimentEvent(
+						eventSource,
+						ExperimentEventMsg{},
+					)
+				}
 			}
 
 			// Handlers are async, so need a little while to run
@@ -83,32 +188,128 @@ func TestPublishModelEvent(t *testing.T) {
 		numPublishers   int
 		numEventsBefore int
 		numEventsAfter  int
+		op              eventHubTestOp
 	}
 
 	tests := []test{
+		// models
 		{
-			name:            "Events published before handler registration are ignored",
+			name:            "Events published before handler registration are ignored - models",
 			numPublishers:   1,
 			numEventsBefore: 10,
 			numEventsAfter:  0,
+			op:              modelEventHub,
 		},
 		{
-			name:            "Events published after handler registration are received",
+			name:            "Events published after handler registration are received - models",
 			numPublishers:   1,
 			numEventsBefore: 0,
 			numEventsAfter:  10,
+			op:              modelEventHub,
 		},
 		{
-			name:            "Only events published after handler registration are received",
+			name:            "Only events published after handler registration are received - models",
 			numPublishers:   1,
 			numEventsBefore: 10,
 			numEventsAfter:  10,
+			op:              modelEventHub,
 		},
 		{
-			name:            "Multiple producers can publish to the same topic",
+			name:            "Multiple producers can publish to the same topic - models",
 			numPublishers:   10,
 			numEventsBefore: 10,
 			numEventsAfter:  10,
+			op:              modelEventHub,
+		},
+
+		// servers
+		{
+			name:            "Events published before handler registration are ignored - servers",
+			numPublishers:   1,
+			numEventsBefore: 10,
+			numEventsAfter:  0,
+			op:              serverEventHub,
+		},
+		{
+			name:            "Events published after handler registration are received - servers",
+			numPublishers:   1,
+			numEventsBefore: 0,
+			numEventsAfter:  10,
+			op:              serverEventHub,
+		},
+		{
+			name:            "Only events published after handler registration are received - servers",
+			numPublishers:   1,
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			op:              serverEventHub,
+		},
+		{
+			name:            "Multiple producers can publish to the same topic - servers",
+			numPublishers:   10,
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			op:              serverEventHub,
+		},
+
+		// pipelines
+		{
+			name:            "Events published before handler registration are ignored - pipelines",
+			numPublishers:   1,
+			numEventsBefore: 10,
+			numEventsAfter:  0,
+			op:              pipelineEventHub,
+		},
+		{
+			name:            "Events published after handler registration are received - pipelines",
+			numPublishers:   1,
+			numEventsBefore: 0,
+			numEventsAfter:  10,
+			op:              pipelineEventHub,
+		},
+		{
+			name:            "Only events published after handler registration are received - pipelines",
+			numPublishers:   1,
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			op:              pipelineEventHub,
+		},
+		{
+			name:            "Multiple producers can publish to the same topic - pipelines",
+			numPublishers:   10,
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			op:              pipelineEventHub,
+		},
+
+		// experiments
+		{
+			name:            "Events published before handler registration are ignored - experiments",
+			numPublishers:   1,
+			numEventsBefore: 10,
+			numEventsAfter:  0,
+			op:              experimentEventHub,
+		},
+		{
+			name:            "Events published after handler registration are received - experiments",
+			numPublishers:   1,
+			numEventsBefore: 0,
+			numEventsAfter:  10,
+			op:              experimentEventHub,
+		},
+		{
+			name:            "Only events published after handler registration are received - experiments",
+			numPublishers:   1,
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			op:              experimentEventHub,
+		},
+		{
+			name:            "Multiple producers can publish to the same topic - experiments",
+			numPublishers:   10,
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			op:              experimentEventHub,
 		},
 	}
 
@@ -120,28 +321,85 @@ func TestPublishModelEvent(t *testing.T) {
 
 			for b := 0; b < tt.numEventsBefore; b++ {
 				for p := 0; p < tt.numPublishers; p++ {
-					h.PublishModelEvent(
-						fmt.Sprintf("publisher-%d", p),
-						ModelEventMsg{},
-					)
+					if tt.op == modelEventHub {
+						h.PublishModelEvent(
+							fmt.Sprintf("publisher-%d", p),
+							ModelEventMsg{},
+						)
+					} else if tt.op == serverEventHub {
+						h.PublishServerEvent(
+							fmt.Sprintf("publisher-%d", p),
+							ServerEventMsg{},
+						)
+					} else if tt.op == pipelineEventHub {
+						h.PublishPipelineEvent(
+							fmt.Sprintf("publisher-%d", p),
+							PipelineEventMsg{},
+						)
+					} else if tt.op == experimentEventHub {
+						h.PublishExperimentEvent(
+							fmt.Sprintf("publisher-%d", p),
+							ExperimentEventMsg{},
+						)
+					}
 				}
 			}
 
 			var counter int64
 			queueSize := tt.numPublishers * (tt.numEventsBefore + tt.numEventsAfter)
-			h.RegisterModelEventHandler(
-				"test.handler",
-				queueSize,
-				l,
-				func(event ModelEventMsg) { atomic.AddInt64(&counter, 1) },
-			)
+			if tt.op == modelEventHub {
+				h.RegisterModelEventHandler(
+					"test.handler",
+					queueSize,
+					l,
+					func(event ModelEventMsg) { atomic.AddInt64(&counter, 1) },
+				)
+			} else if tt.op == serverEventHub {
+				h.RegisterServerEventHandler(
+					"test.handler",
+					queueSize,
+					l,
+					func(event ServerEventMsg) { atomic.AddInt64(&counter, 1) },
+				)
+			} else if tt.op == pipelineEventHub {
+				h.RegisterPipelineEventHandler(
+					"test.handler",
+					queueSize,
+					l,
+					func(event PipelineEventMsg) { atomic.AddInt64(&counter, 1) },
+				)
+			} else if tt.op == experimentEventHub {
+				h.RegisterExperimentEventHandler(
+					"test.handler",
+					queueSize,
+					l,
+					func(event ExperimentEventMsg) { atomic.AddInt64(&counter, 1) },
+				)
+			}
 
 			for a := 0; a < tt.numEventsAfter; a++ {
 				for p := 0; p < tt.numPublishers; p++ {
-					h.PublishModelEvent(
-						fmt.Sprintf("publisher-%d", p),
-						ModelEventMsg{},
-					)
+					if tt.op == modelEventHub {
+						h.PublishModelEvent(
+							fmt.Sprintf("publisher-%d", p),
+							ModelEventMsg{},
+						)
+					} else if tt.op == serverEventHub {
+						h.PublishServerEvent(
+							fmt.Sprintf("publisher-%d", p),
+							ServerEventMsg{},
+						)
+					} else if tt.op == pipelineEventHub {
+						h.PublishPipelineEvent(
+							fmt.Sprintf("publisher-%d", p),
+							PipelineEventMsg{},
+						)
+					} else if tt.op == experimentEventHub {
+						h.PublishExperimentEvent(
+							fmt.Sprintf("publisher-%d", p),
+							ExperimentEventMsg{},
+						)
+					}
 				}
 			}
 
@@ -160,29 +418,112 @@ func TestClose(t *testing.T) {
 		numEventsAfter  int
 		registerBefore  bool
 		expectedCount   int
+		op              eventHubTestOp
 	}
 
 	tests := []test{
+		// models
 		{
-			name:            "Immediate close is allowed",
+			name:            "Immediate close is allowed - models",
 			numEventsBefore: 0,
 			numEventsAfter:  10,
 			registerBefore:  true,
 			expectedCount:   0,
+			op:              modelEventHub,
 		},
 		{
-			name:            "No events should be received after calling Close",
+			name:            "No events should be received after calling Close - models",
 			numEventsBefore: 10,
 			numEventsAfter:  10,
 			registerBefore:  true,
 			expectedCount:   10,
+			op:              modelEventHub,
 		},
 		{
-			name:            "No events should be received by a handler registered after calling Close",
+			name:            "No events should be received by a handler registered after calling Close - models",
 			numEventsBefore: 10,
 			numEventsAfter:  10,
 			registerBefore:  false,
 			expectedCount:   0,
+			op:              modelEventHub,
+		},
+
+		// servers
+		{
+			name:            "Immediate close is allowed - servers",
+			numEventsBefore: 0,
+			numEventsAfter:  10,
+			registerBefore:  true,
+			expectedCount:   0,
+			op:              serverEventHub,
+		},
+		{
+			name:            "No events should be received after calling Close - servers",
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			registerBefore:  true,
+			expectedCount:   10,
+			op:              serverEventHub,
+		},
+		{
+			name:            "No events should be received by a handler registered after calling Close - servers",
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			registerBefore:  false,
+			expectedCount:   0,
+			op:              serverEventHub,
+		},
+
+		// pipelines
+		{
+			name:            "Immediate close is allowed - pipelines",
+			numEventsBefore: 0,
+			numEventsAfter:  10,
+			registerBefore:  true,
+			expectedCount:   0,
+			op:              pipelineEventHub,
+		},
+		{
+			name:            "No events should be received after calling Close - pipelines",
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			registerBefore:  true,
+			expectedCount:   10,
+			op:              pipelineEventHub,
+		},
+		{
+			name:            "No events should be received by a handler registered after calling Close - pipelines",
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			registerBefore:  false,
+			expectedCount:   0,
+			op:              pipelineEventHub,
+		},
+
+		// experiments
+		{
+			name:            "Immediate close is allowed - experiments",
+			numEventsBefore: 0,
+			numEventsAfter:  10,
+			registerBefore:  true,
+			expectedCount:   0,
+			op:              experimentEventHub,
+		},
+		{
+			name:            "No events should be received after calling Close - experiments",
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			registerBefore:  true,
+			expectedCount:   10,
+			op:              experimentEventHub,
+		},
+		{
+			name:            "No events should be received by a handler registered after calling Close - experiments",
+			numEventsBefore: 10,
+			numEventsAfter:  10,
+			registerBefore:  false,
+			expectedCount:   0,
+			op:              experimentEventHub,
 		},
 	}
 
@@ -196,12 +537,35 @@ func TestClose(t *testing.T) {
 			queueSize := tt.numEventsBefore + tt.numEventsAfter
 
 			register := func() {
-				h.RegisterModelEventHandler(
-					"test.handler",
-					queueSize,
-					l,
-					func(event ModelEventMsg) { atomic.AddInt64(&counter, 1) },
-				)
+				if tt.op == modelEventHub {
+					h.RegisterModelEventHandler(
+						"test.handler",
+						queueSize,
+						l,
+						func(event ModelEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				} else if tt.op == serverEventHub {
+					h.RegisterServerEventHandler(
+						"test.handler",
+						queueSize,
+						l,
+						func(event ServerEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				} else if tt.op == pipelineEventHub {
+					h.RegisterPipelineEventHandler(
+						"test.handler",
+						queueSize,
+						l,
+						func(event PipelineEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				} else if tt.op == experimentEventHub {
+					h.RegisterExperimentEventHandler(
+						"test.handler",
+						queueSize,
+						l,
+						func(event ExperimentEventMsg) { atomic.AddInt64(&counter, 1) },
+					)
+				}
 			}
 
 			if tt.registerBefore {
@@ -209,10 +573,27 @@ func TestClose(t *testing.T) {
 			}
 
 			for b := 0; b < tt.numEventsBefore; b++ {
-				h.PublishModelEvent(
-					"test.publisher",
-					ModelEventMsg{},
-				)
+				if tt.op == modelEventHub {
+					h.PublishModelEvent(
+						"test.publisher",
+						ModelEventMsg{},
+					)
+				} else if tt.op == serverEventHub {
+					h.PublishServerEvent(
+						"test.publisher",
+						ServerEventMsg{},
+					)
+				} else if tt.op == pipelineEventHub {
+					h.PublishPipelineEvent(
+						"test.publisher",
+						PipelineEventMsg{},
+					)
+				} else if tt.op == experimentEventHub {
+					h.PublishExperimentEvent(
+						"test.publisher",
+						ExperimentEventMsg{},
+					)
+				}
 			}
 
 			h.Close()
@@ -222,10 +603,27 @@ func TestClose(t *testing.T) {
 			}
 
 			for a := 0; a < tt.numEventsAfter; a++ {
-				h.PublishModelEvent(
-					"test.publisher",
-					ModelEventMsg{},
-				)
+				if tt.op == modelEventHub {
+					h.PublishModelEvent(
+						"test.publisher",
+						ModelEventMsg{},
+					)
+				} else if tt.op == serverEventHub {
+					h.PublishServerEvent(
+						"test.publisher",
+						ServerEventMsg{},
+					)
+				} else if tt.op == pipelineEventHub {
+					h.PublishPipelineEvent(
+						"test.publisher",
+						PipelineEventMsg{},
+					)
+				} else if tt.op == experimentEventHub {
+					h.PublishExperimentEvent(
+						"test.publisher",
+						ExperimentEventMsg{},
+					)
+				}
 			}
 
 			// Handlers are async, so need a little while to run
