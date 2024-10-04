acf7f0503424edf239e7eeaf09005b2c39806895
Sherif Akoush <sa@seldon.io>
2024-10-04 16:17:06 +0100

    lint fixes

--- operator/scheduler/pipeline_test.go
+++ operator/scheduler/pipeline_test.go
@@ -34,7 +34,7 @@ func TestSubscribePipelineEvents(t *testing.T) {
 	}
 	now := metav1.Now()
 
-	// note expected state is derived in the test, maybe we should be explictly about it in the future
+	// note expected state is derived in the test, maybe we should be explicitly about it in the future
 	tests := []test{
 		{
 			name: "model and pipeline ready - no scheduler state",
