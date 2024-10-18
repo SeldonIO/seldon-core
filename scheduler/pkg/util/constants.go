0969697aec74b20ac38c145ccbc0916808977577
Sherif Akoush <sa@seldon.io>
2024-10-18 15:57:51 +0100

    add keepalive for agent client -> scheduler

--- scheduler/pkg/util/constants.go
+++ scheduler/pkg/util/constants.go
@@ -9,10 +9,12 @@ the Change License after the Change Date as each is defined in accordance with t
 
 package util
 
-import "time"
+import (
+	"time"
+)
 
+// REST
 const (
-	// REST constants
 	DefaultReverseProxyHTTPPort = 9999
 	MaxIdleConnsHTTP            = 10
 	MaxIdleConnsPerHostHTTP     = 10
@@ -22,12 +24,19 @@ const (
 	IdleConnTimeoutSeconds      = 60 * 30
 )
 
+// GRPC
 const (
 	GRPCRetryBackoff             = 100 * time.Millisecond
 	GRPCRetryMaxCount            = 5 // around 3.2s in total wait duration
 	GRPCMaxMsgSizeBytes          = 1000 * 1024 * 1024
-	EnvoyUpdateDefaultBatchWait  = 250 * time.Millisecond
 	GRPCModelServerLoadTimeout   = 60 * time.Minute // How long to wait for a model to load? think of LLM Load, maybe should be a config
 	GRPCModelServerUnloadTimeout = 2 * time.Minute
 	GRPCControlPlaneTimeout      = 1 * time.Minute // For control plane operations except load/unload
+)
+
+const (
+	EnvoyUpdateDefaultBatchWait = 250 * time.Millisecond
+	ClientKeapAliveTime         = 10 * time.Second
+	ClientKeapAliveTimeout      = time.Second
+	ClientKeapAlivePermit       = true
 )
