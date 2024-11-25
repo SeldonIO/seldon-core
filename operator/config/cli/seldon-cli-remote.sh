04e59d7673f50359fab5ab8d63bff6e44cd8019c
Sherif Akoush <sa@seldon.io>
2024-11-25 19:01:30 +0000

    add copyright

--- operator/config/cli/seldon-cli-remote.sh
+++ operator/config/cli/seldon-cli-remote.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#
+# Copyright (c) 2024 Seldon Technologies Ltd.
+
+# Use of this software is governed by
+# (1) the license included in the LICENSE file or
+# (2) if the license included in the LICENSE file is the Business Source License 1.1,
+# the Change License after the Change Date as each is defined in accordance with the LICENSE file.
+#
+
+
 # This script is used to run commands on the seldon-cli pod in the seldon-mesh namespace
 # Usage: ./seldon-cli-remote.sh <command>
 # command: the command to run on the seldon-cli pod in the format: `seldon` <args> 
