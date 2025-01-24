4f1091d35585463c2e801cb301acf3a9c7621429
Sherif Akoush <sa@seldon.io>
2025-01-24 15:08:04 +0000

    treat scheduling with min replicas as good

--- scheduler/pkg/scheduler/scheduler.go
+++ scheduler/pkg/scheduler/scheduler.go
@@ -185,25 +185,21 @@ func (s *SimpleScheduler) scheduleToServer(modelName string) error {
 	ok := s.findAndUpdateToServers(filteredServers, latestModel, desiredReplicas, desiredReplicas)
 	// Try to scheduler with min replicas if not enough replicas
 	okWithMinReplicas := false
-	if !ok {
-		if minReplicas > 0 {
-			okWithMinReplicas = s.findAndUpdateToServers(filteredServers, latestModel, desiredReplicas, int(minReplicas))
-		}
+	if !ok && minReplicas > 0 {
+		okWithMinReplicas = s.findAndUpdateToServers(filteredServers, latestModel, desiredReplicas, int(minReplicas))
+		msg := "Failed to schedule model as no matching server had enough suitable replicas, managed to schedule with min replicas"
+		logger.Debug(msg)
 	}
 
-	if !ok {
+	if !ok && !okWithMinReplicas {
 		msg := "Failed to schedule model as no matching server had enough suitable replicas"
-		if okWithMinReplicas {
-			msg = "Failed to schedule model as no matching server had enough suitable replicas, managed to schedule with min replicas"
-		}
 		logger.Debug(msg)
 		// we do not want to reset the server if it has live replicas or loading replicas
 		// in the case of loading replicas, we need to make sure that we can unload them later.
 		// for example in the case that a model is just marked as loading on a particular server replica
 		// then it gets a delete request (before it is marked as loaded or available) we need to make sure
 		// that we can unload it from the server
-		// we also do not want to reset the server if we managed to schedule with min replicas
-		s.store.FailedScheduling(latestModel, msg, !latestModel.HasLiveReplicas() && !latestModel.IsLoadingOrLoadedOnServer() && !okWithMinReplicas)
+		s.store.FailedScheduling(latestModel, msg, !latestModel.HasLiveReplicas() && !latestModel.IsLoadingOrLoadedOnServer())
 		return errors.New(msg)
 	}
 
