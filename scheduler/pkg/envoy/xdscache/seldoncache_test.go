7a68abe728ee9f8fec98388cb1c54b271bb56251
Sherif Akoush <sa@seldon.io>
2025-03-04 15:32:43 +0000

    add note

--- scheduler/pkg/envoy/xdscache/seldoncache_test.go
+++ scheduler/pkg/envoy/xdscache/seldoncache_test.go
@@ -479,6 +479,10 @@ func TestAccessLogSettings(t *testing.T) {
 										IgnoreCase: true,
 									},
 								}
+								// this is a little bit cumbersome, but we need to check the filter
+								// as it is a nested structure
+								// the actual test to check the filter is correct (i.e. only fitlering bad requests) is 
+								// done manually.
 								expectedFilter := &accesslog.OrFilter{
 									Filters: []*accesslog.AccessLogFilter{
 										// http
