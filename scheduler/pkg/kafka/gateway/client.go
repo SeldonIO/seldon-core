c9b8eafb6fc97d3d19dc2873b7d1ede68c2259d5
Robert <robert.samoilescu@gmail.com>
2025-03-24 15:38:06 +0000

    Included spec flag to delete topics

--- scheduler/pkg/kafka/gateway/client.go
+++ scheduler/pkg/kafka/gateway/client.go
@@ -188,7 +188,8 @@ func (kc *KafkaSchedulerClient) SubscribeModelEvents() error {
 			}
 		} else {
 			logger.Infof("Removing model %s", event.ModelName)
-			err := kc.consumerManager.RemoveModel(event.ModelName)
+			cleanTopicOnDeletion := latestVersionStatus.GetModelDefn().GetModelSpec().GetCleanTopicsOnDelete()
+			err := kc.consumerManager.RemoveModel(event.ModelName, cleanTopicOnDeletion)
 			if err != nil {
 				kc.logger.WithError(err).Errorf("Failed to remove model %s", event.ModelName)
 			}
