da45495eb32687d7734d996516e57fe3665d0bc5
Robert <robert.samoilescu@gmail.com>
2025-05-27 15:01:53 +0100

    Implemented dataflow validation for seldon runtime

---
+++ operator/controllers/reconcilers/common/validator.go
@@ -0,0 +1,64 @@
+package common
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	mlopsv1alpha1 "github.com/seldonio/seldon-core/operator/v2/apis/mlops/v1alpha1"
+)
+
+func ParseInt32(s string) (int32, error) {
+	i64, err := strconv.ParseInt(s, 10, 32)
+	return int32(i64), err
+}
+
+func ValidateDataflowScaleSpec(
+	ctx context.Context,
+	clt client.Client,
+	component *mlopsv1alpha1.ComponentDefn,
+	kafkaConfig *mlopsv1alpha1.KafkaConfig,
+	namespace string,
+	logger logr.Logger,
+) error {
+	logger.Info("kafkaConfig.Topics", "Topics", kafkaConfig.Topics)
+
+	// Get the number of Pipeline resources in the namespace
+	var pipelineList mlopsv1alpha1.PipelineList
+	if err := clt.List(ctx, &pipelineList, client.InNamespace(namespace)); err != nil {
+		return fmt.Errorf("failed to list Pipeline resources in namespace %s: %w", namespace, err)
+	}
+
+	pipelineCount := int32(len(pipelineList.Items))
+	logger.Info("Number of Pipeline resources", "namespace", namespace, "count", pipelineCount)
+
+	numPartitions, err := ParseInt32(kafkaConfig.Topics["numPartitions"].StrVal)
+	if err != nil {
+		return fmt.Errorf("failed to parse numPartitions from KafkaConfig: %w", err)
+	}
+
+	logger.Info("Using numPartitions from KafkaConfig", "numPartitions", numPartitions)
+
+	maxReplicas := numPartitions
+	if pipelineCount != 0 {
+		maxReplicas = numPartitions * pipelineCount
+	}
+
+	logger.Info("Maximum replicas for dataflow engine", "max_replicas", maxReplicas)
+
+	if component.Replicas == nil {
+		return fmt.Errorf("%s component must have replicas defined", mlopsv1alpha1.DataflowEngineName)
+	}
+	replicas := *component.Replicas
+	logger.Info("Seldon dataflow engine replicas", "replicas", replicas)
+
+	// Validate that the number of replicas is less than or equal to the number of partitions
+	if replicas > maxReplicas {
+		return fmt.Errorf("%s replicas %d cannot be greater than %d", mlopsv1alpha1.DataflowEngineName, replicas, maxReplicas)
+	}
+
+	return nil
+}
