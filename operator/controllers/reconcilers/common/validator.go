6ed61cbe20a08ce2b4a4800b2063f2583feb1155
Robert <robert.samoilescu@gmail.com>
2025-06-02 15:17:39 +0100

    Implemented seldon runtime reconciler to react to pipelines

--- operator/controllers/reconcilers/common/validator.go
+++ operator/controllers/reconcilers/common/validator.go
@@ -21,20 +21,10 @@ func ValidateDataflowScaleSpec(
 	clt client.Client,
 	component *mlopsv1alpha1.ComponentDefn,
 	kafkaConfig *mlopsv1alpha1.KafkaConfig,
-	namespace string,
+	namespace *string,
 	logger logr.Logger,
 ) error {
 	logger.Info("kafkaConfig.Topics", "Topics", kafkaConfig.Topics)
-
-	// Get the number of Pipeline resources in the namespace
-	var pipelineList mlopsv1alpha1.PipelineList
-	if err := clt.List(ctx, &pipelineList, client.InNamespace(namespace)); err != nil {
-		return fmt.Errorf("failed to list Pipeline resources in namespace %s: %w", namespace, err)
-	}
-
-	pipelineCount := int32(len(pipelineList.Items))
-	logger.Info("Number of Pipeline resources", "namespace", namespace, "count", pipelineCount)
-
 	numPartitions, err := ParseInt32(kafkaConfig.Topics["numPartitions"].StrVal)
 	if err != nil {
 		return fmt.Errorf("failed to parse numPartitions from KafkaConfig: %w", err)
@@ -42,16 +32,28 @@ func ValidateDataflowScaleSpec(
 
 	logger.Info("Using numPartitions from KafkaConfig", "numPartitions", numPartitions)
 
+	var pipelineCount int32 = 0
+	if namespace != nil {
+		// Get the number of Pipeline resources in the namespace
+		var pipelineList mlopsv1alpha1.PipelineList
+		if err := clt.List(ctx, &pipelineList, client.InNamespace(*namespace)); err != nil {
+			return fmt.Errorf("failed to list Pipeline resources in namespace %s: %w", *namespace, err)
+		}
+
+		pipelineCount = int32(len(pipelineList.Items))
+		logger.Info("Number of Pipeline resources", "namespace", *namespace, "count", pipelineCount)
+	}
+
 	maxReplicas := numPartitions
 	if pipelineCount != 0 {
 		maxReplicas = numPartitions * pipelineCount
 	}
 
 	logger.Info("Maximum replicas for dataflow engine", "max_replicas", maxReplicas)
+
 	if component.Replicas != nil && *component.Replicas > maxReplicas {
 		component.Replicas = &maxReplicas
 		logger.Info("Adjusted dataflow engine replicas to max", "replicas", maxReplicas)
 	}
-
 	return nil
 }
