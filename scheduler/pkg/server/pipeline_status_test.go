38e478179c198089218189b2722f3c5da2996671
Robert <robert.samoilescu@gmail.com>
2025-06-17 17:05:17 +0100

    Reverted tests

--- scheduler/pkg/server/pipeline_status_test.go
+++ scheduler/pkg/server/pipeline_status_test.go
@@ -18,6 +18,7 @@ import (
 
 	pb "github.com/seldonio/seldon-core/apis/go/v2/mlops/scheduler"
 
+	"github.com/seldonio/seldon-core/scheduler/v2/pkg/coordinator"
 	"github.com/seldonio/seldon-core/scheduler/v2/pkg/store/pipeline"
 )
 
@@ -112,100 +113,100 @@ func TestPipelineStatusStream(t *testing.T) {
 	}
 }
 
-// func TestPipelineStatusEvents(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestPipelineStatusEvents(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	type test struct {
-// 		name    string
-// 		loadReq *pb.LoadPipelineRequest
-// 		timeout time.Duration
-// 		err     bool
-// 	}
+	type test struct {
+		name    string
+		loadReq *pb.LoadPipelineRequest
+		timeout time.Duration
+		err     bool
+	}
 
-// 	tests := []test{
-// 		{
-// 			name: "pipeline ok",
-// 			loadReq: &pb.LoadPipelineRequest{
-// 				Pipeline: &pb.Pipeline{
-// 					Name:    "foo",
-// 					Version: 1,
-// 					Uid:     "x",
-// 					Steps: []*pb.PipelineStep{
-// 						{
-// 							Name: "a",
-// 						},
-// 						{
-// 							Name:   "b",
-// 							Inputs: []string{"a.outputs"},
-// 						},
-// 					},
-// 				},
-// 			},
-// 			timeout: 10 * time.Millisecond,
-// 			err:     false,
-// 		},
-// 		{
-// 			name: "timeout",
-// 			loadReq: &pb.LoadPipelineRequest{
-// 				Pipeline: &pb.Pipeline{
-// 					Name:    "foo",
-// 					Version: 1,
-// 					Uid:     "x",
-// 					Steps: []*pb.PipelineStep{
-// 						{
-// 							Name: "a",
-// 						},
-// 						{
-// 							Name:   "b",
-// 							Inputs: []string{"a.outputs"},
-// 						},
-// 					},
-// 				},
-// 			},
-// 			timeout: 1 * time.Millisecond,
-// 			err:     true,
-// 		},
-// 	}
+	tests := []test{
+		{
+			name: "pipeline ok",
+			loadReq: &pb.LoadPipelineRequest{
+				Pipeline: &pb.Pipeline{
+					Name:    "foo",
+					Version: 1,
+					Uid:     "x",
+					Steps: []*pb.PipelineStep{
+						{
+							Name: "a",
+						},
+						{
+							Name:   "b",
+							Inputs: []string{"a.outputs"},
+						},
+					},
+				},
+			},
+			timeout: 10 * time.Millisecond,
+			err:     false,
+		},
+		{
+			name: "timeout",
+			loadReq: &pb.LoadPipelineRequest{
+				Pipeline: &pb.Pipeline{
+					Name:    "foo",
+					Version: 1,
+					Uid:     "x",
+					Steps: []*pb.PipelineStep{
+						{
+							Name: "a",
+						},
+						{
+							Name:   "b",
+							Inputs: []string{"a.outputs"},
+						},
+					},
+				},
+			},
+			timeout: 1 * time.Millisecond,
+			err:     true,
+		},
+	}
 
-// 	for _, test := range tests {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			s, hub := createTestScheduler()
-// 			s.timeout = test.timeout
-// 			if test.loadReq != nil {
-// 				err := s.pipelineHandler.AddPipeline(test.loadReq.Pipeline)
-// 				g.Expect(err).To(BeNil())
-// 			}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			s, hub := createTestScheduler()
+			s.timeout = test.timeout
+			if test.loadReq != nil {
+				err := s.pipelineHandler.AddPipeline(test.loadReq.Pipeline)
+				g.Expect(err).To(BeNil())
+			}
 
-// 			stream := newStubPipelineStatusServer(1, 5*time.Millisecond)
-// 			s.pipelineEventStream.streams[stream] = &PipelineSubscription{
-// 				name:   "dummy",
-// 				stream: stream,
-// 				fin:    make(chan bool),
-// 			}
-// 			g.Expect(s.pipelineEventStream.streams[stream]).ToNot(BeNil())
-// 			hub.PublishPipelineEvent(pipelineEventHandlerName, coordinator.PipelineEventMsg{
-// 				PipelineName: "foo", PipelineVersion: 1})
+			stream := newStubPipelineStatusServer(1, 5*time.Millisecond)
+			s.pipelineEventStream.streams[stream] = &PipelineSubscription{
+				name:   "dummy",
+				stream: stream,
+				fin:    make(chan bool),
+			}
+			g.Expect(s.pipelineEventStream.streams[stream]).ToNot(BeNil())
+			hub.PublishPipelineEvent(pipelineEventHandlerName, coordinator.PipelineEventMsg{
+				PipelineName: "foo", PipelineVersion: 1})
 
-// 			// to allow events to propagate
-// 			time.Sleep(500 * time.Millisecond)
+			// to allow events to propagate
+			time.Sleep(500 * time.Millisecond)
 
-// 			if test.err {
-// 				g.Expect(s.pipelineEventStream.streams).To(HaveLen(0))
-// 			} else {
+			if test.err {
+				g.Expect(s.pipelineEventStream.streams).To(HaveLen(0))
+			} else {
 
-// 				var psr *pb.PipelineStatusResponse
-// 				select {
-// 				case next := <-stream.msgs:
-// 					psr = next
-// 				default:
-// 					t.Fail()
-// 				}
+				var psr *pb.PipelineStatusResponse
+				select {
+				case next := <-stream.msgs:
+					psr = next
+				default:
+					t.Fail()
+				}
 
-// 				g.Expect(psr).ToNot(BeNil())
-// 				g.Expect(psr.Versions).To(HaveLen(1))
-// 				g.Expect(psr.Versions[0].State.Status).To(Equal(pb.PipelineVersionState_PipelineCreate))
-// 				g.Expect(s.pipelineEventStream.streams).To(HaveLen(1))
-// 			}
-// 		})
-// 	}
-// }
+				g.Expect(psr).ToNot(BeNil())
+				g.Expect(psr.Versions).To(HaveLen(1))
+				g.Expect(psr.Versions[0].State.Status).To(Equal(pb.PipelineVersionState_PipelineCreate))
+				g.Expect(s.pipelineEventStream.streams).To(HaveLen(1))
+			}
+		})
+	}
+}
