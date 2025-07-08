bee9cfd33c67ac29a7af534dba0b296c5978cae9
Robert <robert.samoilescu@gmail.com>
2025-07-08 12:02:27 +0100

    Fixed store and load pipeline test

--- scheduler/pkg/kafka/pipeline/kafkamanager_test.go
+++ scheduler/pkg/kafka/pipeline/kafkamanager_test.go
@@ -10,9 +10,13 @@ the Change License after the Change Date as each is defined in accordance with t
 package pipeline
 
 import (
+	"sync"
 	"testing"
 
 	. "github.com/onsi/gomega"
+	kafka_config "github.com/seldonio/seldon-core/components/kafka/v2/pkg/config"
+	"github.com/seldonio/seldon-core/scheduler/v2/pkg/tracing"
+	"github.com/sirupsen/logrus"
 )
 
 func TestGetPipelineKey(t *testing.T) {
@@ -48,66 +52,68 @@ func TestGetPipelineKey(t *testing.T) {
 	}
 }
 
-// func TestLoadOrStorePipeline(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestStoreAndLoadPipeline(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	type test struct {
-// 		name              string
-// 		pipeline          *Pipeline
-// 		resourceName      string
-// 		isModel           bool
-// 		expectedPipelines int
-// 	}
-// 	logger := logrus.New()
-// 	tracer, err := tracing.NewTraceProvider("test", nil, logger)
-// 	g.Expect(err).To(BeNil())
-// 	tests := []test{
-// 		{
-// 			name:              "model",
-// 			resourceName:      "foo",
-// 			isModel:           true,
-// 			expectedPipelines: 1,
-// 		},
-// 		{
-// 			name:              "model - existing in map",
-// 			pipeline:          &Pipeline{wg: &sync.WaitGroup{}},
-// 			resourceName:      "foo",
-// 			isModel:           true,
-// 			expectedPipelines: 1,
-// 		},
-// 		{
-// 			name:              "pipeline",
-// 			resourceName:      "foo",
-// 			isModel:           false,
-// 			expectedPipelines: 1,
-// 		},
-// 		{
-// 			name:              "pipeline - existing in map",
-// 			pipeline:          &Pipeline{wg: &sync.WaitGroup{}},
-// 			resourceName:      "foo",
-// 			isModel:           false,
-// 			expectedPipelines: 1,
-// 		},
-// 	}
-// 	for _, test := range tests {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			km, err := NewKafkaManager(logrus.New(), "default", &kafka_config.KafkaConfig{}, tracer, 10, 10)
-// 			g.Expect(err).To(BeNil())
-// 			if test.pipeline != nil {
-// 				km.pipelines.Store(getPipelineKey(test.resourceName, test.isModel), test.pipeline)
-// 			}
-// 			pipeline, err := km.loadOrStorePipeline(test.resourceName, test.isModel)
-// 			g.Expect(err).To(BeNil())
-// 			g.Expect(pipeline).ToNot(BeNil())
-// 			count := 0
-// 			km.pipelines.Range(func(key interface{}, val interface{}) bool {
-// 				count = count + 1
-// 				return true
-// 			})
-// 			g.Expect(count).To(Equal(test.expectedPipelines))
-// 			if test.pipeline != nil {
-// 				g.Expect(pipeline).To(Equal(test.pipeline))
-// 			}
-// 		})
-// 	}
-// }
+	type test struct {
+		name              string
+		pipeline          *Pipeline
+		resourceName      string
+		isModel           bool
+		expectedPipelines int
+	}
+	logger := logrus.New()
+	tracer, err := tracing.NewTraceProvider("test", nil, logger)
+	g.Expect(err).To(BeNil())
+	tests := []test{
+		{
+			name:              "model",
+			resourceName:      "foo",
+			isModel:           true,
+			expectedPipelines: 1,
+		},
+		{
+			name:              "model - existing in map",
+			pipeline:          &Pipeline{wg: &sync.WaitGroup{}},
+			resourceName:      "foo",
+			isModel:           true,
+			expectedPipelines: 1,
+		},
+		{
+			name:              "pipeline",
+			resourceName:      "foo",
+			isModel:           false,
+			expectedPipelines: 1,
+		},
+		{
+			name:              "pipeline - existing in map",
+			pipeline:          &Pipeline{wg: &sync.WaitGroup{}},
+			resourceName:      "foo",
+			isModel:           false,
+			expectedPipelines: 1,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			km, err := NewKafkaManager(logrus.New(), "default", &kafka_config.KafkaConfig{}, tracer, 10)
+			g.Expect(err).To(BeNil())
+			if test.pipeline != nil {
+				km.pipelines.Store(getPipelineKey(test.resourceName, test.isModel), test.pipeline)
+			}
+			err = km.StorePipeline(test.resourceName, test.isModel)
+			g.Expect(err).To(BeNil())
+			pipeline, err := km.LoadPipeline(test.resourceName, test.isModel)
+			g.Expect(err).To(BeNil())
+			g.Expect(pipeline).ToNot(BeNil())
+			count := 0
+			km.pipelines.Range(func(key interface{}, val interface{}) bool {
+				count = count + 1
+				return true
+			})
+			g.Expect(count).To(Equal(test.expectedPipelines))
+			if test.pipeline != nil {
+				g.Expect(pipeline).To(Equal(test.pipeline))
+			}
+		})
+	}
+}
