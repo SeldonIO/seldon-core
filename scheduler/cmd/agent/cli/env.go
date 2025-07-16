712b327c2ec801d5e6682587952faaad2a13226c
Robert <robert.samoilescu@gmail.com>
2025-07-16 15:14:06 +0100

    Reverted agent cli

--- scheduler/cmd/agent/cli/env.go
+++ scheduler/cmd/agent/cli/env.go
@@ -51,17 +51,3 @@ func getEnvInt(name string) (int, bool, bool) {
 
 	return int(val), true, true
 }
-
-func getEnvBool(name string) (bool, bool, bool) {
-	fromEnv := os.Getenv(name)
-	if fromEnv == "" {
-		return false, false, false
-	}
-
-	val, err := strconv.ParseBool(fromEnv)
-	if err != nil {
-		return false, true, false
-	}
-
-	return val, true, true
-}
