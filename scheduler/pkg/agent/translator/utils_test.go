19d86139653b6800fc2d2a0d294e1eec02abb5d9
Robert <robert.samoilescu@gmail.com>
2025-07-17 13:59:41 +0100

    Included more tests for util functions

--- scheduler/pkg/agent/translator/utils_test.go
+++ scheduler/pkg/agent/translator/utils_test.go
@@ -99,3 +99,149 @@ func TestGzip(t *testing.T) {
 	g.Expect(err).To(BeNil(), "Error reading decompressed response body")
 	g.Expect(string(decompressedContent)).To(Equal(content), "Decompressed content does not match original content")
 }
+
+func TestReadRequestBody(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	tests := []struct {
+		name      string
+		req       *http.Request
+		expectErr bool
+	}{
+		{
+			name:      "Valid request body",
+			req:       &http.Request{Body: io.NopCloser(strings.NewReader("test body"))},
+			expectErr: false,
+		},
+		{
+			name:      "Empty request body",
+			req:       &http.Request{Body: io.NopCloser(strings.NewReader(""))},
+			expectErr: true,
+		},
+		{
+			name:      "Nil request body",
+			req:       &http.Request{Body: nil},
+			expectErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			body, err := ReadRequestBody(test.req)
+			if test.expectErr {
+				g.Expect(err).NotTo(BeNil(), "expected an error but got none")
+			} else {
+				g.Expect(err).To(BeNil(), "expected no error but got one")
+				g.Expect(body).NotTo(BeNil(), "expected body to be non-nil")
+			}
+		})
+	}
+}
+
+func TestReadResponseBody(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	tests := []struct {
+		name      string
+		res       *http.Response
+		expectErr bool
+	}{
+		{
+			name:      "Valid response body",
+			res:       &http.Response{Body: io.NopCloser(strings.NewReader("test response"))},
+			expectErr: false,
+		},
+		{
+			name:      "Empty response body",
+			res:       &http.Response{Body: io.NopCloser(strings.NewReader(""))},
+			expectErr: true,
+		},
+		{
+			name:      "Nil response body",
+			res:       &http.Response{Body: nil},
+			expectErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			body, err := ReadResponseBody(test.res)
+			if test.expectErr {
+				g.Expect(err).NotTo(BeNil(), "expected an error but got none")
+			} else {
+				g.Expect(err).To(BeNil(), "expected no error but got one")
+				g.Expect(body).NotTo(BeNil(), "expected body to be non-nil")
+			}
+		})
+	}
+}
+
+func TestConstructStringTensor(t *testing.T) {
+	g := NewGomegaWithT(t)
+	tests := []struct {
+		name      string
+		fieldName string
+		fieldData []string
+		expected  map[string]interface{}
+	}{
+		{
+			name:      "Valid string tensor",
+			fieldName: "testField",
+			fieldData: []string{"value1", "value2"},
+			expected: map[string]interface{}{
+				"name":     "testField",
+				"shape":    []int{2},
+				"datatype": "BYTES",
+				"data":     []string{"value1", "value2"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := ConstructStringTensor(test.fieldName, test.fieldData)
+			g.Expect(result).To(Equal(test.expected), "Constructed tensor does not match expected result")
+		})
+	}
+}
+
+func TestExtractTensorByName(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	tests := []struct {
+		name       string
+		outputs    []interface{}
+		tensorName string
+		expectErr  bool
+		expected   map[string]interface{}
+	}{
+		{
+			name: "Extract existing tensor",
+			outputs: []interface{}{
+				map[string]interface{}{"name": "tensor1", "data": []string{"a", "b"}},
+				map[string]interface{}{"name": "tensor2", "data": []string{"c", "d"}},
+			},
+			tensorName: "tensor1",
+			expectErr:  false,
+			expected:   map[string]interface{}{"name": "tensor1", "data": []string{"a", "b"}},
+		},
+		{
+			name:       "Extract non-existing tensor",
+			outputs:    []interface{}{},
+			tensorName: "tensor3",
+			expectErr:  true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := ExtractTensorByName(test.outputs, test.tensorName)
+			if test.expectErr {
+				g.Expect(err).NotTo(BeNil(), "expected an error but got none")
+			} else {
+				g.Expect(err).To(BeNil(), "expected no error but got one")
+				g.Expect(result).To(Equal(test.expected), "Extracted tensor does not match expected result")
+			}
+		})
+	}
+}
