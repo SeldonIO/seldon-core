0e1d475efc954d7a5f06aa3bd24035e52b44778b
Robert <robert.samoilescu@gmail.com>
2025-07-17 15:16:24 +0100

    Implemented BaseTranslator to avoid code duplication

--- scheduler/pkg/agent/translator/openai/embeddings.go
+++ scheduler/pkg/agent/translator/openai/embeddings.go
@@ -12,7 +12,9 @@ import (
 	"github.com/seldonio/seldon-core/scheduler/v2/pkg/agent/translator"
 )
 
-type OpenAIEmbeddingsTranslator struct{}
+type OpenAIEmbeddingsTranslator struct {
+	translator.BaseTranslator
+}
 
 func getEmbeddingParameters(jsonBody map[string]interface{}) map[string]interface{} {
 	llmParameters := make(map[string]interface{})
@@ -114,74 +116,4 @@ func (t *OpenAIEmbeddingsTranslator) TranslateToOIP(req *http.Request, logger lo
 		return nil, err
 	}
 	return newReq, nil
-}
-
-func (t *OpenAIEmbeddingsTranslator) TranslateFromOIP(res *http.Response, logger log.FieldLogger) (*http.Response, error) {
-	// Decompress the response if needed - gzip
-	var isGzipped bool
-	var err error
-
-	if isGzipped, err = translator.DecompressIfNeeded(res); err != nil {
-		logger.WithError(err).Error("Failed to decompress OpenAI API response")
-		return nil, err
-	}
-
-	// Read the response body
-	body, err := translator.ReadResponseBody(res)
-	if err != nil {
-		logger.WithError(err).Error("Failed to read OpenAI API response body")
-		return nil, err
-	}
-
-	jsonBody, err := translator.GetJsonBody(body)
-	if err != nil {
-		logger.WithError(err).Error("Failed to parse OpenAI API response body")
-		return nil, err
-	}
-
-	// Parse the response body
-	logger.Info("Parsing OpenAI API response body", jsonBody)
-	outputs, ok := jsonBody["outputs"].([]interface{})
-	if !ok {
-		logger.Error("`outputs` field not found or not an array in OpenAI API response")
-		return nil, fmt.Errorf("`outputs` field not found or not an array in OpenAI API response")
-	}
-
-	// Extract the output_all tensor form the inference response. This contains the full response
-	// OpenAI API response - only works for OpenAI runtime, since we return the original OpenAI API response
-	tensorName := "output_all"
-	outputAll, err := translator.ExtractTensorByName(outputs, tensorName)
-	if err != nil {
-		logger.WithError(err).Errorf("Failed to extract '%s' tensor from OpenAI API response", tensorName)
-		return nil, err
-	}
-
-	data, ok := outputAll["data"].([]interface{})
-	if !ok {
-		logger.Errorf("`data` field not found or not an array of strings in output tensor %s", tensorName)
-		return nil, fmt.Errorf("`data` field not found or not an array of strings in output tensor %s", tensorName)
-	}
-
-	content, ok := data[0].(string)
-	if !ok {
-		logger.Errorf("`data` field in output tensor %s is not a byte array", tensorName)
-		return nil, fmt.Errorf("`data` field in output tensor %s is not a byte array", tensorName)
-	}
-
-	// Create a new response with the translated body
-	newBody := io.NopCloser(bytes.NewBuffer([]byte(content)))
-	newRes := http.Response{
-		StatusCode: res.StatusCode,
-		Header:     res.Header.Clone(),
-		Body:       newBody,
-	}
-
-	// compress the response body if needed
-	if isGzipped {
-		if err := translator.Compress(&newRes); err != nil {
-			logger.WithError(err).Error("Failed to compress OpenAI API response")
-			return nil, err
-		}
-	}
-	return &newRes, nil
 }
