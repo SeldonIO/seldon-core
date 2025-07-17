e930d2adfeb0ab2c93c3d4d0c690afd77516d4a3
Robert <robert.samoilescu@gmail.com>
2025-07-17 15:16:24 +0100

    Implemented BaseTranslator to avoid code duplication

---
+++ scheduler/pkg/agent/translator/openai/embeddings_test.go
@@ -0,0 +1,75 @@
+package openai
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	log "github.com/sirupsen/logrus"
+)
+
+func TestEmbeddingsRequest(t *testing.T) {
+	g := NewGomegaWithT(t)
+	type test struct {
+		name               string
+		openAIContent      map[string]interface{}
+		expectedOipContent map[string]interface{}
+	}
+
+	tests := []test{
+		{
+			name: "default",
+			openAIContent: map[string]interface{}{
+				"model": "text-embedding-ada-002",
+				"input": "This is a test",
+			},
+			expectedOipContent: map[string]interface{}{
+				"inputs": []map[string]interface{}{
+					{
+						"name":     "input",
+						"shape":    []int{1},
+						"datatype": "BYTES",
+						"data":     []string{"This is a test"},
+					},
+				},
+				"parameters": map[string]interface{}{
+					"llm_parameters": map[string]interface{}{},
+				},
+			},
+		},
+	}
+
+	logger := log.New().WithField("Source", "HTTPProxy")
+	openAITranslator := &OpenAIEmbeddingsTranslator{}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			openAIReqBody, err := json.Marshal(test.openAIContent)
+			g.Expect(err).To(BeNil(), "Error marshalling OpenAI request content")
+
+			openAIReq := http.Request{
+				Method: http.MethodPost,
+				Body:   io.NopCloser(strings.NewReader(string(openAIReqBody))),
+				URL: &url.URL{
+					Scheme: "http",
+					Host:   "localhost:9000",
+					Path:   "/v2/models/chatgpt/infer/embeddings",
+				},
+			}
+			oipReq, err := openAITranslator.TranslateToOIP(&openAIReq, logger)
+			g.Expect(err).To(BeNil(), "Error translating OpenAI request to OIP format")
+
+			oipReqBody, err := io.ReadAll(oipReq.Body)
+			g.Expect(err).To(BeNil(), "Error reading OIP request body")
+
+			expectedOipBody, err := json.Marshal(test.expectedOipContent)
+			g.Expect(err).To(BeNil(), "Error marshalling expected OIP request content")
+
+			g.Expect(oipReqBody).To(Equal(expectedOipBody), "OIP request body does not match expected format")
+		})
+	}
+}
