d3bf54fc1ba1b0835ad6f5b16f842c74732c2fa2
Robert <robert.samoilescu@gmail.com>
2025-09-12 11:20:32 +0100

    Fixed operator scheduler pipeline test

--- operator/scheduler/pipeline_test.go
+++ operator/scheduler/pipeline_test.go
@@ -24,10 +24,6 @@ import (
 )
 
 func TestSubscribePipelineEvents(t *testing.T) {
-	t.Parallel()
-
-	g := NewGomegaWithT(t)
-
 	type test struct {
 		name               string
 		existing_resources []client.Object
@@ -39,7 +35,7 @@ func TestSubscribePipelineEvents(t *testing.T) {
 	// note expected state is derived in the test, maybe we should be explicitly about it in the future
 	tests := []test{
 		{
-			name: "model and pipeline ready - no scheduler state",
+			name: "models ready, dataflow ready, pipelinegw ready - no scheduler state",
 			existing_resources: []client.Object{
 				&mlopsv1alpha1.Pipeline{
 					ObjectMeta: metav1.ObjectMeta{
@@ -74,7 +70,7 @@ func TestSubscribePipelineEvents(t *testing.T) {
 			noSchedulerState: true,
 		},
 		{
-			name: "model and pipeline ready - with scheduler state",
+			name: "models ready, dataflow ready, pipelinegw ready - with scheduler state",
 			existing_resources: []client.Object{
 				&mlopsv1alpha1.Pipeline{
 					ObjectMeta: metav1.ObjectMeta{
@@ -146,7 +142,7 @@ func TestSubscribePipelineEvents(t *testing.T) {
 			noSchedulerState: false,
 		},
 		{
-			name: "model not ready and pipeline ready",
+			name: "model not ready, dataflow ready, pipelinegw ready",
 			existing_resources: []client.Object{
 				&mlopsv1alpha1.Pipeline{
 					ObjectMeta: metav1.ObjectMeta{
@@ -181,7 +177,42 @@ func TestSubscribePipelineEvents(t *testing.T) {
 			noSchedulerState: false,
 		},
 		{
-			name: "model and pipeline not ready",
+			name: "model ready, dataflow ready, pipelinegw not ready - with scheduler state",
+			existing_resources: []client.Object{
+				&mlopsv1alpha1.Pipeline{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:       "foo",
+						Namespace:  "default",
+						Generation: 1,
+					},
+					Spec: mlopsv1alpha1.PipelineSpec{},
+				},
+			},
+			results: []*scheduler.PipelineStatusResponse{
+				{
+					PipelineName: "foo",
+					Versions: []*scheduler.PipelineWithState{
+						{
+							Pipeline: &scheduler.Pipeline{
+								Name: "foo",
+								KubernetesMeta: &scheduler.KubernetesMeta{
+									Namespace:  "default",
+									Generation: 1,
+								},
+							},
+							State: &scheduler.PipelineVersionState{
+								Status:           scheduler.PipelineVersionState_PipelineReady,
+								PipelineGwStatus: scheduler.PipelineVersionState_PipelineFailed,
+								ModelsReady:      true,
+							},
+						},
+					},
+				},
+			},
+			noSchedulerState: false,
+		},
+		{
+			name: "model, dataflow, pipelinegw not ready",
 			existing_resources: []client.Object{
 				&mlopsv1alpha1.Pipeline{
 					ObjectMeta: metav1.ObjectMeta{
@@ -374,6 +405,7 @@ func TestSubscribePipelineEvents(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
+			g := NewGomegaWithT(t)
 
 			// note that if responses_pipelines is nil -> scheduler state is not existing
 			var grpcClient mockSchedulerGrpcClient
@@ -434,15 +466,22 @@ func TestSubscribePipelineEvents(t *testing.T) {
 					} else {
 						g.Expect(err).To(BeNil())
 					}
-					if r.Versions[0].State.Status == scheduler.PipelineVersionState_PipelineReady && r.Versions[0].State.ModelsReady {
-						g.Expect(pipeline.Status.IsReady()).To(BeTrueBecause("Pipeline and Models are ready"))
+					if r.Versions[0].State.Status == scheduler.PipelineVersionState_PipelineReady &&
+						r.Versions[0].State.PipelineGwStatus == scheduler.PipelineVersionState_PipelineReady &&
+						r.Versions[0].State.ModelsReady {
+						g.Expect(pipeline.Status.IsReady()).To(BeTrueBecause("Dataflow, Pipeline Gateway and Models are ready"))
 					} else {
-						g.Expect(pipeline.Status.IsReady()).To(BeFalseBecause("Either Pipline or Models are not ready"))
+						g.Expect(pipeline.Status.IsReady()).To(BeFalseBecause("Either Dataflow or Pipeline Gateway or Models are not ready"))
 					}
 					if r.Versions[0].State.Status == scheduler.PipelineVersionState_PipelineReady {
 						g.Expect(pipeline.Status.IsConditionReady(mlopsv1alpha1.PipelineReady)).To(BeTrueBecause("Pipeline is ready"))
 					} else {
 						g.Expect(pipeline.Status.IsConditionReady(mlopsv1alpha1.PipelineReady)).To(BeFalseBecause("Pipeline is not ready"))
+					}
+					if r.Versions[0].State.PipelineGwStatus == scheduler.PipelineVersionState_PipelineReady {
+						g.Expect(pipeline.Status.IsConditionReady(mlopsv1alpha1.PipelineGwReady)).To(BeTrueBecause("Pipeline Gateway is ready"))
+					} else {
+						g.Expect(pipeline.Status.IsConditionReady(mlopsv1alpha1.PipelineGwReady)).To(BeFalseBecause("Pipeline Gateway is not ready"))
 					}
 					if r.Versions[0].State.ModelsReady {
 						g.Expect(pipeline.Status.IsConditionReady(mlopsv1alpha1.ModelsReady)).To(BeTrueBecause("Models are ready"))
