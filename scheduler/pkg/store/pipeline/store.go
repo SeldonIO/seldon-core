f2df63bde761f6d2c0ac10c46485db23f95a2792
Robert <robert.samoilescu@gmail.com>
2025-09-17 15:58:39 +0100

    Implemented model ready condition as a conjuction betwen status and modelgw status ready

--- scheduler/pkg/store/pipeline/store.go
+++ scheduler/pkg/store/pipeline/store.go
@@ -541,7 +541,9 @@ func (ps *PipelineStore) handleModelEvents(event coordinator.ModelEventMsg) {
 				return
 			}
 			ps.mu.Lock()
-			evts := updatePipelinesFromModelAvailability(refs, event.ModelName, model != nil && model.GetLastAvailableModel() != nil, ps.pipelines, ps.logger)
+			modelVersion := model.GetLastAvailableModel()
+			modelAvailable := model != nil && modelVersion != nil && modelVersion.ModelState().ModelGWState == store.ModelAvailable
+			evts := updatePipelinesFromModelAvailability(refs, event.ModelName, modelAvailable, ps.pipelines, ps.logger)
 			ps.mu.Unlock()
 			// Publish events for modified pipelines
 			if ps.eventHub != nil {
