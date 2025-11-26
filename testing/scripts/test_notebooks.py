abcc21d69258b562f7e1551c6d5ed07cd2d943d2
Antanina Vertsinskaya <antanina.vertsinskaya@seldon.io>
2025-11-26 14:26:07 +0000

    Uncomment fixed notebooks

--- testing/scripts/test_notebooks.py
+++ testing/scripts/test_notebooks.py
@@ -31,36 +31,33 @@ class TestNotebooks(object):
     # def test_server_examples(self):
     #     create_and_run_script("../../notebooks", "server_examples")
 
-    def test_rolling_updates(self):
-        create_and_run_script("../../notebooks", "rolling_updates")
-
     # #
     # # Ambassador
     # #
 
-    # def test_ambassador_canary(self):
-    #     create_and_run_script("../../examples/ambassador/canary", "ambassador_canary")
+    def test_ambassador_canary(self):
+        create_and_run_script("../../examples/ambassador/canary", "ambassador_canary")
 
-    # def test_ambassador_headers(self):
-    #     create_and_run_script("../../examples/ambassador/headers", "ambassador_headers")
+    def test_ambassador_headers(self):
+        create_and_run_script("../../examples/ambassador/headers", "ambassador_headers")
 
-    # def test_ambassador_shadow(self):
-    #     create_and_run_script("../../examples/ambassador/shadow", "ambassador_shadow")
+    def test_ambassador_shadow(self):
+        create_and_run_script("../../examples/ambassador/shadow", "ambassador_shadow")
 
-    # #
-    # # KEDA Examples
-    # #
+    
+    # KEDA Examples
+    
 
-    # def test_keda_prom_auto_scale(self):
-    #     try:
-    #         create_and_run_script("../../examples/keda", "keda_prom_auto_scale")
-    #     except CalledProcessError as e:
-    #         run(
-    #             "helm delete seldon-core-analytics --namespace seldon-system",
-    #             shell=True,
-    #             check=False,
-    #         )
-    #         raise e
+    def test_keda_prom_auto_scale(self):
+        try:
+            create_and_run_script("../../examples/keda", "keda_prom_auto_scale")
+        except CalledProcessError as e:
+            run(
+                "helm delete seldon-core-analytics --namespace seldon-system",
+                shell=True,
+                check=False,
+            )
+            raise e
 
     # #
     # # Misc
@@ -88,65 +85,65 @@ class TestNotebooks(object):
     def test_graph_metadata(self):
         create_and_run_script("../../examples/models/metadata", "graph_metadata")
 
-    # def test_grpc_metadata(self):
-    #     create_and_run_script("../../examples/models/metadata", "metadata_grpc")
+    def test_grpc_metadata(self):
+        create_and_run_script("../../examples/models/metadata", "metadata_grpc")
 
-    # def test_autoscaling(self):
-    #     try:
-    #         create_and_run_script(
-    #             "../../examples/models/autoscaling", "autoscaling_example"
-    #         )
-    #     except CalledProcessError as e:
-    #         run("helm delete loadtester --namespace seldon", shell=True, check=False)
-    #         raise e
+    def test_autoscaling(self):
+        try:
+            create_and_run_script(
+                "../../examples/models/autoscaling", "autoscaling_example"
+            )
+        except CalledProcessError as e:
+            run("helm delete loadtester --namespace seldon", shell=True, check=False)
+            raise e
 
-    # def test_scaling(self):
-    #     create_and_run_script("../../notebooks", "scale")
+    def test_scaling(self):
+        create_and_run_script("../../notebooks", "scale")
 
-    # #
-    # # Payloads
-    # #
+    #
+    # Payloads
+    #
 
-    # def test_jsondata(self):
-    #     create_and_run_script(
-    #         "../../examples/models/sklearn_iris_jsondata", "sklearn_iris_jsondata"
-    #     )
+    def test_jsondata(self):
+        create_and_run_script(
+            "../../examples/models/sklearn_iris_jsondata", "sklearn_iris_jsondata"
+        )
 
-    # #
-    # # SKLearn
-    # #
+    #
+    # SKLearn
+    #
 
-    # def test_sklearn_iris(self):
-    #     create_and_run_script("../../examples/models/sklearn_iris", "sklearn_iris")
+    def test_sklearn_iris(self):
+        create_and_run_script("../../examples/models/sklearn_iris", "sklearn_iris")
 
-    # #
-    # # OpenVino
-    # #
+    #
+    # OpenVino
+    #
 
-    # # def test_openvino_squeezenet(self):
-    # #    create_and_run_script("../../examples/models/openvino", "openvino-squeezenet")
+    def test_openvino_squeezenet(self):
+       create_and_run_script("../../examples/models/openvino", "openvino-squeezenet")
 
-    # # def test_openvino_imagenet_ensemble(self):
-    # #    create_and_run_script(
-    # #        "../../examples/models/openvino_imagenet_ensemble",
-    # #        "openvino_imagenet_ensemble",
-    # #    )
+    def test_openvino_imagenet_ensemble(self):
+       create_and_run_script(
+           "../../examples/models/openvino_imagenet_ensemble",
+           "openvino_imagenet_ensemble",
+       )
 
-    # def test_custom_metrics_server(self):
-    #     create_and_run_script("../../examples/feedback/metrics-server", "README")
+    def test_custom_metrics_server(self):
+        create_and_run_script("../../examples/feedback/metrics-server", "README")
 
-    # #
-    # # Upgrade
-    # #
+    #
+    # Upgrade
+    #
 
-    # def test_upgrade(self):
-    #     try:
-    #         create_and_run_script("../../notebooks", "operator_upgrade")
-    #     except:
-    #         run("make install_seldon", shell=True, check=False)
-    #         raise
+    def test_upgrade(self):
+        try:
+            create_and_run_script("../../notebooks", "operator_upgrade")
+        except:
+            run("make install_seldon", shell=True, check=False)
+            raise
 
-    # def test_disruption_budgets(self):
-    #     create_and_run_script(
-    #         "../../examples/models/disruption_budgets", "pdbs_example"
-    #     )
+    def test_disruption_budgets(self):
+        create_and_run_script(
+            "../../examples/models/disruption_budgets", "pdbs_example"
+        )
