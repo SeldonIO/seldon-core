49511e16aaec89c18c90673c7af5607b4398a8f9
Viktor Taskov <viktor.taskov@seldon.io>
2025-12-09 12:56:21 +0000

    replace chart path in e2e utils to test the upgrade path for newer versions - older ones are present in the legacy path

--- testing/scripts/kind_test_setup.sh
+++ testing/scripts/kind_test_setup.sh
@@ -5,9 +5,9 @@ set -o errexit
 set -o pipefail
 
 # Run s2i build to create base images
-# make s2i_build_base_images
+make s2i_build_base_images
 
-# make kind_create_cluster
-#make kind_build_images
-#make kind_build_test_images
+make kind_create_cluster
+make kind_build_images
+make kind_build_test_images
 make kind_setup
