d6aaff1c5308fb79265ba20e8c441cc761d6b5a2
Miguel Angel <miguel.abad@seldon.io>
2025-12-17 19:29:15 +0100

    do not clean resources of scenarios that fail

--- tests/integration/godog/suite/suite.go
+++ tests/integration/godog/suite/suite.go
@@ -148,6 +148,10 @@ func InitializeScenario(scenarioCtx *godog.ScenarioContext) {
 
 	// After: optional cleanup / rollback
 	scenarioCtx.After(func(ctx context.Context, scenario *godog.Scenario, err error) (context.Context, error) {
+		if err != nil {
+			// don't clean up of resources of scenarios that fail
+			return ctx, nil
+		}
 		if suiteDeps.Config.SkipCleanup {
 			log.WithField("scenario", scenario.Name).Debug("Skipping cleanup")
 			return ctx, nil
