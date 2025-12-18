5b610a4f50c912d16af0ad1e7927fa54004280e4
Antanina Vertsinskaya <antanina.vertsinskaya@seldon.io>
2025-12-18 14:02:13 +0000

    Deprecate the rest of notebooks where `seldon-core-s2i-python3*.:1.19`. is used

--- examples/kubeflow/pipeline/pipeline_steps/clean_text/build_image.sh
+++ examples/kubeflow/pipeline/pipeline_steps/clean_text/build_image.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-s2i build . seldonio/seldon-core-s2i-python37:1.2.3 clean_text_transformer:0.1
+PYTHON_VERSION=""
+TAG=""
+
+s2i build . seldonio/seldon-core-s2i-python${PYTHON_VERSION}:${TAG} clean_text_transformer:0.1
 
