766027c73aa30df0b12f70459ddc554cfb9c2f87
Miguel Angel <miguel.abad@seldon.io>
2025-12-19 14:11:24 +0100

    bootstrap of components

---
+++ tests/integration/godog/components/bootstrap.go
@@ -0,0 +1,38 @@
+package components
+
+import (
+	"fmt"
+
+	"github.com/seldonio/seldon-core/tests/integration/godog/k8sclient"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+const (
+	KafkaNodePool ComponentName = "KafkaNodePool"
+)
+
+func StartComponents(k8sClient *k8sclient.K8sClient, namespace string) (*EnvManager, error) {
+	var kafkaNodePoolGVK = schema.GroupVersionKind{
+		Group:   "kafka.strimzi.io",
+		Version: "v1beta2",
+		Kind:    "KafkaNodePool",
+	}
+
+	kafkaNodePool := NewK8sObjectComponent(
+		k8sClient,
+		KafkaNodePool,
+		kafkaNodePoolGVK,
+		types.NamespacedName{Namespace: namespace, Name: "kafka"},
+		UnavailableByDeleting,
+	)
+
+	kafka := NewKafkaComponent(k8sClient, namespace)
+
+	env, err := NewEnvManager(kafkaNodePool, kafka)
+	if err != nil {
+		return nil, fmt.Errorf("failed to bootstrap components: %v", err)
+	}
+
+	return env, nil
+}
